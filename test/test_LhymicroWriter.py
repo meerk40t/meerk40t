bedbd21e6ad4ee2944629cc2cbea125984dfba81
tatarize <tatarize@gmail.com>
2019-09-09 08:28:44 -0700

    Fix test for python 3, too

--- test/test_LhymicroWriter.py
+++ test/test_LhymicroWriter.py
@@ -7,25 +7,25 @@ from LhymicroWriter import LhymicroWriter
 class TestWriter(unittest.TestCase):
 
     def test_write_home(self):
-        writer = LhymicroWriter(controller="")
+        writer = LhymicroWriter(controller=b'')
         writer.home()
-        self.assertEquals(writer.controller, "IPP\n")
+        self.assertEqual(writer.controller, b"IPP\n")
 
     def test_write_square(self):
-        writer = LhymicroWriter(controller="")
+        writer = LhymicroWriter(controller=b'')
         writer.move(100,0)
         writer.move(0, 100)
         writer.move(-100, 0)
         writer.move(0, -100)
-        self.assertEquals(writer.controller, "IB100S1P\nIR100S1P\nIT100S1P\nIL100S1P\n")
+        self.assertEqual(writer.controller, b"IB100S1P\nIR100S1P\nIT100S1P\nIL100S1P\n")
 
     def test_write_lock_rail(self):
-        writer = LhymicroWriter(controller="")
+        writer = LhymicroWriter(controller=b'')
         writer.lock_rail()
-        self.assertEquals(writer.controller, "IS1P\n")
+        self.assertEqual(writer.controller, b'IS1P\n')
 
     def test_write_compact_mode_M2(self):
-        writer = LhymicroWriter(board="M2", controller="")
+        writer = LhymicroWriter(board="M2", controller=b'')
         writer.speed = 10
         writer.to_compact_mode()
         writer.move(100, 0)
@@ -33,10 +33,10 @@ class TestWriter(unittest.TestCase):
         writer.move(-100, 0)
         writer.move(0, -100)
         writer.to_default_mode()
-        self.assertEquals(writer.controller, "ICV1151911011002218NRBS1EB100R100T100L100FNSE-\n")
+        self.assertEqual(writer.controller, b"ICV1151911011002218NRBS1EB100R100T100L100FNSE-\n")
 
     def test_write_compact_mode_B2(self):
-        writer = LhymicroWriter(board="B2", controller="")
+        writer = LhymicroWriter(board="B2", controller=b'')
         writer.speed = 10
         writer.to_compact_mode()
         writer.move(100, 0)
@@ -44,4 +44,4 @@ class TestWriter(unittest.TestCase):
         writer.move(-100, 0)
         writer.move(0, -100)
         writer.to_default_mode()
-        self.assertEquals(writer.controller, "ICV0121101011005181NRBS1EB100R100T100L100FNSE-\n")
+        self.assertEqual(writer.controller, b"ICV0121101011005181NRBS1EB100R100T100L100FNSE-\n")
--- test/test_zingl.py
+++ test/test_zingl.py
@@ -1,44 +1,51 @@
 from __future__ import print_function
 
 import unittest
-import os
+
 import ZinglPlotter
 
+from math import ceil
 
 class TestZingl(unittest.TestCase):
 
     def test_line(self):
         for x, y, on in ZinglPlotter.plot_line(0, 0, 100, 100):
-            self.assertEquals(x,y)
+            self.assertEqual(x, y)
+
         for i, v in enumerate(ZinglPlotter.plot_line(0, 0, 100, 0)):
             x, y, on = v
-            self.assertEquals(y,0)
+            self.assertEqual(y, 0)
             self.assertEqual(x, i)
+
         for i, v in enumerate(ZinglPlotter.plot_line(0, 0, 0, 100)):
             x, y, on = v
             self.assertEqual(x, 0)
-            self.assertEquals(y,i)
-        for i, v in enumerate(ZinglPlotter.plot_line(0, 0, 100, 200)):
-            x, y, on = v
-            self.assertEqual(x,round(i/2.0))
-            self.assertEquals(y, i)
+            self.assertEqual(y, i)
+
         for i, v in enumerate(ZinglPlotter.plot_line(0, 0, 100, 300)):
             x, y, on = v
-            self.assertEqual(x,round(i/3.0))
-            self.assertEquals(y, i)
+            self.assertEqual(x, int(round(i / 3.0)))
+            self.assertEqual(y, i)
+
         for i, v in enumerate(ZinglPlotter.plot_line(0, 0, 300, 100)):
             x, y, on = v
-            self.assertEqual(y, round(i / 3.0))
-            self.assertEquals(x, i)
+            self.assertEqual(y, int(round(i / 3.0)))
+            self.assertEqual(x, i)
+
+        for i, v in enumerate(ZinglPlotter.plot_line(0, 0, 100, 200)):
+            x, y, on = v
+            self.assertEqual(x, int(ceil(i / 2.0)))
+            self.assertEqual(y, i)
+
 
     def test_quad_bezier(self):
         for x, y, on in ZinglPlotter.plot_quad_bezier(0, 0, 50, 50, 100, 100):
-            self.assertEquals(x,y)
+            self.assertEqual(x, y)
         for x, y, on in ZinglPlotter.plot_quad_bezier(0, 0, 50, 50, 0, 0):
-            self.assertEquals(x, y)
+            self.assertEqual(x, y)
 
     def test_cubic_bezier(self):
         for x, y, on in ZinglPlotter.plot_cubic_bezier(0, 0, 50, 50, 100, 100, 150, 150):
-            self.assertEquals(x,y)
+            self.assertEqual(x, y)
         for x, y, on in ZinglPlotter.plot_cubic_bezier(0, 0, 100, 100, 100, 100, 0, 0):
-            self.assertEquals(x, y)
+            self.assertEqual(x, y)
