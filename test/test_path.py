fb713ed6bda97527ba4f77fa3eae04742b00ebc9
tatarize <tatarize@gmail.com>
2019-09-20 06:01:29 -0700

    +Some tests

--- test/test_path.py
+++ test/test_path.py
@@ -9,11 +9,25 @@ from path import *
 class TestPath(unittest.TestCase):
 
     def test_subpaths(self):
-        object_path = Path()
-        svg_parser.parse_svg_path(object_path, "M0,0 50,50 100,100z M0,100 50,50, 100,0")
-        for i, p in enumerate(object_path.as_subpaths()):
+        path = Path()
+        svg_parser.parse_svg_path(path, "M0,0 50,50 100,100z M0,100 50,50, 100,0")
+        for i, p in enumerate(path.as_subpaths()):
             if i == 0:
                 self.assertEqual(p.d(), "M 0,0 L 50,50 L 100,100 Z")
             elif i == 1:
                 self.assertEqual(p.d(), "M 0,100 L 50,50 L 100,0")
             self.assertLessEqual(i, 1)
+
+    def test_move_quad_smooth(self):
+        path = Path()
+        path.move((4, 4), (20, 20), (25, 25), 6 + 3j)
+        path.quad((20, 33), (100, 100))
+        path.smooth_quad((13, 45), (16, 16), (34, 56), "z")
+        self.assertEqual(path.d(), "M 4,4 L 20,20 L 25,25 L 6,3 Q 20,33 100,100 T 13,45 T 16,16 T 34,56 T 4,4 Z")
+
+    def test_move_cubic_smooth(self):
+        path = Path()
+        path.move((4, 4), (20, 20), (25, 25), 6 + 3j)
+        path.cubic((20, 33), (25, 25), (100, 100))
+        path.smooth_cubic((13, 45), (16, 16), (34, 56), "z")
+        self.assertEqual(path.d(), "M 4,4 L 20,20 L 25,25 L 6,3 C 20,33 25,25 100,100 S 13,45 16,16 S 34,56 4,4 Z")
