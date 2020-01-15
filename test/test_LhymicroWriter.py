32054931dd2ca41ba24fd3d134bb5cf3cec6a100
tatarize <tatarize@gmail.com>
2020-01-15 09:27:29 +0000

    Kernel Imports

--- test/test_LhymicroWriter.py
+++ test/test_LhymicroWriter.py
@@ -2,18 +2,18 @@ from __future__ import print_function
 
 import unittest
 from LhymicroWriter import LhymicroWriter
-from LaserProject import LaserProject
+from Kernel import Kernel
 
 
 class TestWriter(unittest.TestCase):
 
     def test_write_home(self):
-        writer = LhymicroWriter(LaserProject(), controller=b'')
+        writer = LhymicroWriter(Kernel(), controller=b'')
         writer.home()
         self.assertEqual(writer.controller, b"IPP\n")
 
     def test_write_square(self):
-        writer = LhymicroWriter(LaserProject(), controller=b'')
+        writer = LhymicroWriter(Kernel(), controller=b'')
         writer.move_relative(100, 0)
         writer.move_relative(0, 100)
         writer.move_relative(-100, 0)
@@ -21,12 +21,12 @@ class TestWriter(unittest.TestCase):
         self.assertEqual(writer.controller, b"IB100S1P\nIR100S1P\nIT100S1P\nIL100S1P\n")
 
     def test_write_lock_rail(self):
-        writer = LhymicroWriter(LaserProject(), controller=b'')
+        writer = LhymicroWriter(Kernel(), controller=b'')
         writer.lock_rail()
         self.assertEqual(writer.controller, b'IS1P\n')
 
     def test_write_compact_mode_M2(self):
-        writer = LhymicroWriter(LaserProject(), board="M2", controller=b'')
+        writer = LhymicroWriter(Kernel(), board="M2", controller=b'')
         writer.speed = 10
         writer.to_compact_mode()
         writer.move_relative(100, 0)
@@ -37,7 +37,7 @@ class TestWriter(unittest.TestCase):
         self.assertEqual(writer.controller, b"ICV1151911011002218NRBS1EB100R100T100L100FNSE-\n")
 
     def test_write_compact_mode_B2(self):
-        writer = LhymicroWriter(LaserProject(), board="B2", controller=b'')
+        writer = LhymicroWriter(Kernel(), board="B2", controller=b'')
         writer.speed = 10
         writer.to_compact_mode()
         writer.move_relative(100, 0)
