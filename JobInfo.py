66dc26a3c65fcaf0ff493d27743fc00ef8fefd8b
tatarize <tatarize@gmail.com>
2020-02-20 19:09:48 -0800

    Jobs Dialog Labels
    The switch in 0.5.0 changed operations to primary and elements to graphical objects alone. The required commands in jobs was called Operations but that name got used for the operations like cut and raster, elements were called operations and operations into commands for consistency with other labeling.

--- JobInfo.py
+++ JobInfo.py
@@ -19,8 +19,8 @@ class JobInfo(wx.Frame):
         wx.Frame.__init__(self, *args, **kwds)
 
         self.SetSize((659, 612))
-        self.elements_listbox = wx.ListBox(self, wx.ID_ANY, choices=[], style=wx.LB_ALWAYS_SB | wx.LB_SINGLE)
         self.operations_listbox = wx.ListBox(self, wx.ID_ANY, choices=[], style=wx.LB_ALWAYS_SB | wx.LB_SINGLE)
+        self.commands_listbox = wx.ListBox(self, wx.ID_ANY, choices=[], style=wx.LB_ALWAYS_SB | wx.LB_SINGLE)
         self.button_job_spooler = wx.BitmapButton(self, wx.ID_ANY, icons8_route_50.GetBitmap())
         self.button_writer_control = wx.Button(self, wx.ID_ANY, _("Start Job"))
         self.button_writer_control.SetBitmap(icons8_laser_beam_52.GetBitmap())
@@ -62,10 +62,10 @@ class JobInfo(wx.Frame):
         self.__set_properties()
         self.__do_layout()
 
-        self.Bind(wx.EVT_LISTBOX, self.on_listbox_element_click, self.elements_listbox)
-        self.Bind(wx.EVT_LISTBOX_DCLICK, self.on_listbox_element_dclick, self.elements_listbox)
-        self.Bind(wx.EVT_LISTBOX, self.on_listbox_operations_click, self.operations_listbox)
-        self.Bind(wx.EVT_LISTBOX_DCLICK, self.on_listbox_operations_dclick, self.operations_listbox)
+        self.Bind(wx.EVT_LISTBOX, self.on_listbox_operation_click, self.operations_listbox)
+        self.Bind(wx.EVT_LISTBOX_DCLICK, self.on_listbox_operation_dclick, self.operations_listbox)
+        self.Bind(wx.EVT_LISTBOX, self.on_listbox_commands_click, self.commands_listbox)
+        self.Bind(wx.EVT_LISTBOX_DCLICK, self.on_listbox_commands_dclick, self.commands_listbox)
         self.Bind(wx.EVT_BUTTON, self.on_button_start_job, self.button_writer_control)
         self.Bind(wx.EVT_BUTTON, self.on_button_job_spooler, self.button_job_spooler)
         # end wxGlade
@@ -75,7 +75,7 @@ class JobInfo(wx.Frame):
 
         self.device = None
         self.job_items = None
-        self.required_preprocessing_operations = None
+        self.required_preprocessing_commands = None
 
     def spool_trace_simple(self, event):
         print("Spool Simple.")
@@ -120,7 +120,7 @@ class JobInfo(wx.Frame):
                     self.job_items = [e for e in self.job_items if not hasattr(e, 'type') or e.type != 'text']
                     self.update_gui()
 
-                self.required_preprocessing_operations.append(remove_text)
+                self.required_preprocessing_commands.append(remove_text)
                 break
 
     def conditional_jobadd_make_raster(self):
@@ -153,7 +153,7 @@ class JobInfo(wx.Frame):
                     op.clear()
                     op.append(image_element)
 
-        self.required_preprocessing_operations.append(make_image)
+        self.required_preprocessing_commands.append(make_image)
 
     def conditional_jobadd_actualize_image(self):
         for op in self.job_items:
@@ -170,7 +170,7 @@ class JobInfo(wx.Frame):
                     for elem in op:
                         if ElementFunctions.needs_actualization(elem, op.raster_step):
                             ElementFunctions.make_actual(elem,op.raster_step)
-        self.required_preprocessing_operations.append(actualize)
+        self.required_preprocessing_commands.append(actualize)
 
     def conditional_jobadd_scale_rotary(self):
         if self.device.scale_x != 1.0 or self.device.scale_y != 1.0:
@@ -189,15 +189,15 @@ class JobInfo(wx.Frame):
                             pass
             self.conditional_jobadd_actualize_image()
 
-        self.required_preprocessing_operations.append(scale_for_rotary)
+        self.required_preprocessing_commands.append(scale_for_rotary)
 
     def set_operations(self, operations):
         if not isinstance(operations, list):
             operations = [operations]
-        if self.required_preprocessing_operations is None:
-            self.required_preprocessing_operations = []
+        if self.required_preprocessing_commands is None:
+            self.required_preprocessing_commands = []
         else:
-            self.required_preprocessing_operations.clear()
+            self.required_preprocessing_commands.clear()
         self.job_items.clear()
         for op in operations:
             self.job_items.append(copy(op))
@@ -234,7 +234,7 @@ class JobInfo(wx.Frame):
     def set_kernel(self, kernel):
         self.kernel = kernel
         self.set_device(kernel.device)
-        self.required_preprocessing_operations = []
+        self.required_preprocessing_commands = []
         self.job_items = []
         self.kernel.listen("element_property_update", self.on_element_property_update)
 
@@ -247,8 +247,8 @@ class JobInfo(wx.Frame):
     def __set_properties(self):
         # begin wxGlade: JobInfo.__set_properties
         self.SetTitle("Job")
-        self.elements_listbox.SetToolTip(_("Element List"))
-        self.operations_listbox.SetToolTip(_("Operation List"))
+        self.operations_listbox.SetToolTip(_("operation List"))
+        self.commands_listbox.SetToolTip(_("Command List"))
         self.button_writer_control.SetToolTip(_("Start the Job"))
 
         self.button_job_spooler.SetMinSize((50, 50))
@@ -259,9 +259,9 @@ class JobInfo(wx.Frame):
         # begin wxGlade: JobInfo.__do_layout
         sizer_2 = wx.BoxSizer(wx.VERTICAL)
         sizer_3 = wx.BoxSizer(wx.HORIZONTAL)
-        sizer_1 = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, _("Elements and Operations")), wx.HORIZONTAL)
-        sizer_1.Add(self.elements_listbox, 10, wx.EXPAND, 0)
-        sizer_1.Add(self.operations_listbox, 3, wx.EXPAND, 0)
+        sizer_1 = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, _("Operations and Commands")), wx.HORIZONTAL)
+        sizer_1.Add(self.operations_listbox, 10, wx.EXPAND, 0)
+        sizer_1.Add(self.commands_listbox, 3, wx.EXPAND, 0)
         sizer_2.Add(sizer_1, 10, wx.EXPAND, 0)
         sizer_3.Add(self.button_writer_control, 1, wx.EXPAND, 0)
         sizer_3.Add(self.button_job_spooler, 0, 0, 0)
@@ -284,23 +284,23 @@ class JobInfo(wx.Frame):
         self.kernel.open_window("JobSpooler")
 
     def on_button_start_job(self, event):  # wxGlade: JobInfo.<event_handler>
-        if len(self.required_preprocessing_operations) == 0:
+        if len(self.required_preprocessing_commands) == 0:
             self.device.send_job(self.job_items)
             self.on_button_job_spooler()
             self.kernel.close_old_window("JobInfo")
         else:
-            # Using copy of operations, so operations can add ops.
-            ops = self.required_preprocessing_operations[:]
-            self.required_preprocessing_operations = []
+            # Using copy of commands, so commands can add ops.
+            ops = self.required_preprocessing_commands[:]
+            self.required_preprocessing_commands = []
             for op in ops:
                 op()
             self.update_gui()
 
-    def on_listbox_element_click(self, event):  # wxGlade: JobInfo.<event_handler>
+    def on_listbox_operation_click(self, event):  # wxGlade: JobInfo.<event_handler>
         event.Skip()
 
-    def on_listbox_element_dclick(self, event):  # wxGlade: JobInfo.<event_handler>
-        node_index = self.elements_listbox.GetSelection()
+    def on_listbox_operation_dclick(self, event):  # wxGlade: JobInfo.<event_handler>
+        node_index = self.operations_listbox.GetSelection()
         if node_index == -1:
             return
         obj = self.job_items[node_index]
@@ -311,12 +311,12 @@ class JobInfo(wx.Frame):
             self.kernel.open_window("EngraveProperty").set_operation(obj)
         event.Skip()
 
-    def on_listbox_operations_click(self, event):  # wxGlade: JobInfo.<event_handler>
-        print("Event handler 'on_listbox_operations_click' not implemented!")
+    def on_listbox_commands_click(self, event):  # wxGlade: JobInfo.<event_handler>
+        print("Event handler 'on_listbox_commands_click' not implemented!")
         event.Skip()
 
-    def on_listbox_operations_dclick(self, event):  # wxGlade: JobInfo.<event_handler>
-        print("Event handler 'on_listbox_operations_dclick' not implemented!")
+    def on_listbox_commands_dclick(self, event):  # wxGlade: JobInfo.<event_handler>
+        print("Event handler 'on_listbox_commands_dclick' not implemented!")
         event.Skip()
 
     def on_element_property_update(self, *args):
@@ -330,16 +330,16 @@ class JobInfo(wx.Frame):
             except AttributeError:
                 return str(e)
 
+        self.commands_listbox.Clear()
         self.operations_listbox.Clear()
-        self.elements_listbox.Clear()
-        elements = self.job_items
-        operations = self.required_preprocessing_operations
-        if elements is not None and len(elements) != 0:
-            self.elements_listbox.InsertItems([name_str(e) for e in self.job_items], 0)
+        operations = self.job_items
+        commands = self.required_preprocessing_commands
         if operations is not None and len(operations) != 0:
-            self.operations_listbox.InsertItems([name_str(e) for e in self.required_preprocessing_operations], 0)
+            self.operations_listbox.InsertItems([name_str(e) for e in self.job_items], 0)
+        if commands is not None and len(commands) != 0:
+            self.commands_listbox.InsertItems([name_str(e) for e in self.required_preprocessing_commands], 0)
 
-            self.button_writer_control.SetLabelText(_("Execute Operations"))
+            self.button_writer_control.SetLabelText(_("Execute Commands"))
             self.button_writer_control.SetBackgroundColour(wx.Colour(255, 255, 102))
         else:
             self.button_writer_control.SetLabelText(_("Start Job"))
