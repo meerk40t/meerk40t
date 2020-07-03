5c88ceb68291a0cad8acb33a74b830cf2f555145
tatarize <tatarize@gmail.com>
2020-07-02 15:38:16 -0900

    Specific Black For Rasters
    Allows black rasters to claim all black stroked objects.

--- Kernel.py
+++ Kernel.py
@@ -1264,16 +1264,18 @@ class Elemental(Module):
         for element in elements:
             found_color = False
             for op in self.ops():
-                if op.operation in ("Engrave", "Cut") and op.color == element.stroke:
+                if op.operation in ("Engrave", "Cut", "Raster") and op.color == element.stroke:
                     op.append(element)
                     found_color = True
                 elif op.operation == 'Image' and isinstance(element, SVGImage):
                     op.append(element)
+                    found_color = True
                 elif op.operation == 'Raster' and \
                         not isinstance(element, SVGImage) and \
                         element.fill is not None and \
                         element.fill.value is not None:
                     op.append(element)
+                    found_color = True
             if not found_color:
                 if element.stroke is not None and element.stroke.value is not None:
                     op = LaserOperation(operation="Engrave", color=element.stroke, speed=35.0)
