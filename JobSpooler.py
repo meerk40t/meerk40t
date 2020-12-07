3c4406a0231c1f36f92d3337ed4e37938efd7739
tatarize <tatarize@gmail.com>
2020-12-07 02:16:32 -0800

    Job Spooler - Truncate.

--- JobSpooler.py
+++ JobSpooler.py
@@ -12,9 +12,8 @@ class JobSpooler(wx.Frame, Module):
         wx.Frame.__init__(self, parent, -1, "",
                           style=wx.DEFAULT_FRAME_STYLE | wx.FRAME_FLOAT_ON_PARENT | wx.TAB_TRAVERSAL)
         Module.__init__(self, context, path)
-        self.SetSize((643, 633))
-        self.spooler = context._kernel.active.spooler
-        self.panel_simulation = wx.Panel(self, wx.ID_ANY)
+        self.SetSize((673, 456))
+        self.combo_device = wx.ComboBox(self, wx.ID_ANY, choices=[], style=wx.CB_DROPDOWN)
         self.text_time_laser = wx.TextCtrl(self, wx.ID_ANY, "", style=wx.TE_READONLY)
         self.text_time_travel = wx.TextCtrl(self, wx.ID_ANY, "", style=wx.TE_READONLY)
         self.text_time_total = wx.TextCtrl(self, wx.ID_ANY, "", style=wx.TE_READONLY)
@@ -24,6 +23,7 @@ class JobSpooler(wx.Frame, Module):
         self.__set_properties()
         self.__do_layout()
 
+        self.Bind(wx.EVT_COMBOBOX, self.on_combo_device, self.combo_device)
         self.Bind(wx.EVT_LIST_BEGIN_DRAG, self.on_list_drag, self.list_job_spool)
         self.Bind(wx.EVT_LIST_ITEM_RIGHT_CLICK, self.on_item_rightclick, self.list_job_spool)
         # end wxGlade
@@ -39,23 +39,24 @@ class JobSpooler(wx.Frame, Module):
         self.list_lookup = {}
         self.Bind(wx.EVT_CLOSE, self.on_close, self)
 
+
     def __set_properties(self):
+        # begin wxGlade: Spooler.__set_properties
         _icon = wx.NullIcon
         _icon.CopyFromBitmap(icons8_route_50.GetBitmap())
         self.SetIcon(_icon)
-        # begin wxGlade: Spooler.__set_properties
         self.SetTitle("Job Spooler")
-        self.panel_simulation.SetToolTip("Job Simulation")
+        self.combo_device.SetToolTip("Select the device")
         self.text_time_laser.SetToolTip("Time Estimate: Lasering Time")
         self.text_time_travel.SetToolTip("Time Estimate: Traveling Time")
         self.text_time_total.SetToolTip("Time Estimate: Total Time")
         self.text_time_total_copy.SetToolTip("Time Estimate: Total Time")
         self.list_job_spool.SetToolTip("List and modify the queued operations")
-        self.list_job_spool.AppendColumn("#", format=wx.LIST_FORMAT_LEFT, width=35)
+        self.list_job_spool.AppendColumn("#", format=wx.LIST_FORMAT_LEFT, width=78)
         self.list_job_spool.AppendColumn("Name", format=wx.LIST_FORMAT_LEFT, width=143)
         self.list_job_spool.AppendColumn("Status", format=wx.LIST_FORMAT_LEFT, width=73)
         self.list_job_spool.AppendColumn("Type", format=wx.LIST_FORMAT_LEFT, width=53)
-        self.list_job_spool.AppendColumn("Speed", format=wx.LIST_FORMAT_LEFT, width=77)
+        self.list_job_spool.AppendColumn("Speed", format=wx.LIST_FORMAT_LEFT, width=83)
         self.list_job_spool.AppendColumn("Settings", format=wx.LIST_FORMAT_LEFT, width=223)
         # end wxGlade
 
@@ -67,7 +68,7 @@ class JobSpooler(wx.Frame, Module):
         sizer_total_time = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, "Total Time"), wx.VERTICAL)
         sizer_travel_time = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, "Travel Time"), wx.VERTICAL)
         sizer_laser_time = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, "Laser Time"), wx.VERTICAL)
-        sizer_frame.Add(self.panel_simulation, 7, wx.EXPAND, 0)
+        sizer_frame.Add(self.combo_device, 0, wx.EXPAND, 0)
         sizer_laser_time.Add(self.text_time_laser, 0, wx.EXPAND, 0)
         sizer_time.Add(sizer_laser_time, 1, wx.EXPAND, 0)
         sizer_travel_time.Add(self.text_time_travel, 0, wx.EXPAND, 0)
@@ -77,10 +78,31 @@ class JobSpooler(wx.Frame, Module):
         sizer_total_remaining.Add(self.text_time_total_copy, 0, wx.EXPAND, 0)
         sizer_time.Add(sizer_total_remaining, 1, wx.EXPAND, 0)
         sizer_frame.Add(sizer_time, 0, wx.EXPAND, 0)
-        sizer_frame.Add(self.list_job_spool, 0, wx.EXPAND, 0)
+        sizer_frame.Add(self.list_job_spool, 1, wx.EXPAND, 0)
         self.SetSizer(sizer_frame)
         self.Layout()
         # end wxGlade
+
+    def on_combo_device(self, event):  # wxGlade: Spooler.<event_handler>
+        event.Skip()
+
+    def on_list_drag(self, event):  # wxGlade: JobSpooler.<event_handler>
+        event.Skip()
+
+    def on_item_rightclick(self, event):  # wxGlade: JobSpooler.<event_handler>
+        index = event.Index
+        spooler = self.context.active.spooler
+        try:
+            element = spooler._queue[index]
+        except IndexError:
+            return
+        menu = wx.Menu()
+        convert = menu.Append(wx.ID_ANY, _("Remove %s") % str(element)[:16], "", wx.ITEM_NORMAL)
+        self.Bind(wx.EVT_MENU, self.on_tree_popup_delete(element), convert)
+        convert = menu.Append(wx.ID_ANY, _("Clear All"), "", wx.ITEM_NORMAL)
+        self.Bind(wx.EVT_MENU, self.on_tree_popup_clear(element), convert)
+        self.PopupMenu(menu)
+        menu.Destroy()
 
     def on_close(self, event):
         if self.state == 5:
@@ -94,11 +116,11 @@ class JobSpooler(wx.Frame, Module):
         self.context.close(self.name)
         self.Show()
 
-        self.context._kernel.active.listen('spooler;queue', self.on_spooler_update)
+        self.context.active.listen('spooler;queue', self.on_spooler_update)
         self.refresh_spooler_list()
 
     def finalize(self, channel=None):
-        self.context._kernel.active.unlisten('spooler;queue', self.on_spooler_update)
+        self.context.active.unlisten('spooler;queue', self.on_spooler_update)
         try:
             self.Close()
         except RuntimeError:
@@ -109,23 +131,6 @@ class JobSpooler(wx.Frame, Module):
             self.Close()
         except RuntimeError:
             pass
-
-    def on_list_drag(self, event):  # wxGlade: JobSpooler.<event_handler>
-        event.Skip()
-
-    def on_item_rightclick(self, event):  # wxGlade: JobSpooler.<event_handler>
-        index = event.Index
-        try:
-            element = self.spooler._queue[index]
-        except IndexError:
-            return
-        menu = wx.Menu()
-        convert = menu.Append(wx.ID_ANY, _("Remove %s") % str(element)[:16], "", wx.ITEM_NORMAL)
-        self.Bind(wx.EVT_MENU, self.on_tree_popup_delete(element), convert)
-        convert = menu.Append(wx.ID_ANY, _("Clear All"), "", wx.ITEM_NORMAL)
-        self.Bind(wx.EVT_MENU, self.on_tree_popup_clear(element), convert)
-        self.PopupMenu(menu)
-        menu.Destroy()
 
     def refresh_spooler_list(self):
         if not self.update_spooler:
@@ -141,9 +146,10 @@ class JobSpooler(wx.Frame, Module):
         except RuntimeError:
             return
 
-        if len(self.spooler._queue) > 0:
+        spooler = self.context.active.spooler
+        if len(spooler._queue) > 0:
             # This should actually process and update the queue items.
-            for i, e in enumerate(self.spooler._queue):
+            for i, e in enumerate(spooler._queue):
                 m = self.list_job_spool.InsertItem(i, "#%d" % i)
                 if m != -1:
                     self.list_job_spool.SetItem(m, 1, name_str(e))
@@ -176,14 +182,16 @@ class JobSpooler(wx.Frame, Module):
 
     def on_tree_popup_clear(self, element):
         def delete(event):
-            self.spooler.clear_queue()
+            spooler = self.context.active.spooler
+            spooler.clear_queue()
             self.refresh_spooler_list()
 
         return delete
 
     def on_tree_popup_delete(self, element):
         def delete(event):
-            self.spooler.remove(element)
+            spooler = self.context.active.spooler
+            spooler.remove(element)
             self.refresh_spooler_list()
 
         return delete
