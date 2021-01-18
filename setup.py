22fef901ece1297a8c3ee20694a614f75279084b
tatarize <tatarize@gmail.com>
2021-01-18 06:20:21 -0800

    Updated Range

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ setup(
     install_requires=[
         "pyusb>=1.0.0",
         "Pillow>=8.0.0",
-        "wxPython>=4.1.0",
-        "opencv-python-headless>=3.5.0.0",
+        "wxPython>=4.0.0",
+        "opencv-python-headless>=3.4.0.0",
         "ezdxf>=0.13.1"
     ],
 )
