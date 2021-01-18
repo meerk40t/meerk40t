3d13272317cd557f6b32ebe8952ae822e05b4df7
tatarize <tatarize@gmail.com>
2021-01-18 06:12:33 -0800

    OpenCV 3.x works.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
         "pyusb>=1.0.0",
         "Pillow>=8.0.0",
         "wxPython>=4.0.0",
-        "opencv-python-headless>=4.0.0.0",
+        "opencv-python-headless>=3.0.0.0",
         "ezdxf>=0.13.1"
     ],
 )
