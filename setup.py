d510fa5c022a9772ac7081b36b7a9f8e646d2cc1
tatarize <tatarize@gmail.com>
2021-01-22 20:48:46 -0800

    Correct extra:all ezdxf

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
         "pyusb>=1.0.0",
     ],
     extras_require={
-        'all': ['wxPython>=4.0.0', "Pillow>=7.0.0", "meerk40t_camera", "opencv-python-headless>=3.4.0.0", "ezdxf>=13.0.0"],
+        'all': ['wxPython>=4.0.0', "Pillow>=7.0.0", "meerk40t_camera", "opencv-python-headless>=3.4.0.0", "ezdxf>=0.13.0"],
         'gui': ['wxPython>=4.0.0', "Pillow>=7.0.0"],
         'cam': ["meerk40t_camera", "opencv-python-headless>=3.4.0.0"],
         'dxf': ["ezdxf>=0.13.0"],
