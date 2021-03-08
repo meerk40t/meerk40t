f95eb3136123e71a79af402a75a4d07ee36a3444
tatarize <tatarize@gmail.com>
2021-03-08 11:00:22 -0800

    Correct LeftDown potential crash.

--- Widget.py
+++ Widget.py
@@ -890,6 +890,10 @@ class RectSelectWidget(Widget):
             self.end_location = space_pos
             return RESPONSE_CONSUME
         elif event_type == 'leftup' or event_type == "leftclick":
+            if self.start_location is None:
+                self.start_location = space_pos
+            if self.end_location is None:
+                self.end_location = space_pos
             elements.validate_bounds()
             for obj in elements.elems():
                 sx = self.start_location[0]
