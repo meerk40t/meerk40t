3c55d6d79033658beb515bcb953610e63b11df1a
Tatarize <tatarize@gmail.com>
2021-03-10 11:20:06 -0800

    Raster Wizard Apply, Correction

--- meerk40t/core/elements.py
+++ meerk40t/core/elements.py
@@ -3184,14 +3184,14 @@ class Elemental(Modifier):
 
         @self.tree_conditional(lambda node: isinstance(node.object, SVGImage))
         @self.tree_submenu(_("RasterWizard"))
-        @self.tree_values("script", values=self.context.match("raster_script"))
+        @self.tree_values("script", values=self.context.match("raster_script", suffix=True))
         @self.tree_operation(_("RasterWizard: {script}"), node_type="elem", help="")
         def image_rasterwizard_open(node, script=None, **kwargs):
             self.context("window open RasterWizard %s" % script)
 
         @self.tree_conditional(lambda node: isinstance(node.object, SVGImage))
         @self.tree_submenu(_("Apply Raster Script"))
-        @self.tree_values("script", values=self.context.match("raster_script"))
+        @self.tree_values("script", values=self.context.match("raster_script", suffix=True))
         @self.tree_operation(_("Apply: {script}"), node_type="elem", help="")
         def image_rasterwizard_apply(node, script=None, **kwargs):
             self.context("image wizard %s\n" % script)
