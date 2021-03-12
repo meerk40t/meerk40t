0298970f44196614be6883d757ea8e6aad1104d5
Tatarize <tatarize@gmail.com>
2021-03-12 11:09:06 -0800

    Version Bump

--- meerk40t/gui/wxmeerk40t.py
+++ meerk40t/gui/wxmeerk40t.py
@@ -3522,7 +3522,7 @@ def handleGUIException(exc_type, exc_value, exc_traceback):
     :return:
     """
     error_log = "MeerK40t crash log. Version: %s on %s\n" % (
-        "0.7.0 Beta-16",
+        "0.7.0 Beta-17",
         sys.platform,
     )
     error_log += "".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
--- meerk40t/main.py
+++ meerk40t/main.py
@@ -22,7 +22,7 @@ for full details.
 
 """
 
-MEERK40T_VERSION = "0.7.0 Beta-16"
+MEERK40T_VERSION = "0.7.0 Beta-17"
 
 
 def pair(value):
