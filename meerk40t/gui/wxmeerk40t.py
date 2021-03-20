f5f2723e6ae1e9cc39d8551df02c8fdeb768c664
Tatarize <tatarize@gmail.com>
2021-03-19 22:16:16 -0700

    Update Version.

--- meerk40t/gui/wxmeerk40t.py
+++ meerk40t/gui/wxmeerk40t.py
@@ -3547,7 +3547,7 @@ def handleGUIException(exc_type, exc_value, exc_traceback):
     :return:
     """
     error_log = "MeerK40t crash log. Version: %s on %s\n" % (
-        "0.7.0 Beta-17",
+        "0.7.0 Beta-18",
         sys.platform,
     )
     error_log += "".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
