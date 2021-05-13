4135b4948fc7e383afc03cdfe520851c97b11dcc
tatarize <tatarize@gmail.com>
2021-05-13 09:03:10 -0700

    Patch #413

--- DefaultModules.py
+++ DefaultModules.py
@@ -26,7 +26,8 @@ class SVGWriter:
         root.set(SVG_ATTR_XMLNS_LINK, SVG_VALUE_XLINK)
         root.set(SVG_ATTR_XMLNS_EV, SVG_VALUE_XMLNS_EV)
         root.set(
-            "xmlns:meerK40t", "https://github.com/meerk40t/meerk40t/wiki/Namespace"
+            "xmlns:meerK40t",
+            "https://htmlpreview.github.io/?https://github.com/meerk40t/meerk40t/blob/master/svg-namespace.html"
         )
         # Native unit is mils, these must convert to mm and to px
         mils_per_mm = 39.3701
