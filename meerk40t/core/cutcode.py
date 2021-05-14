73acb68f80ffb6221708ebb3b83c3327db51cb1d
Tatarize <tatarize@gmail.com>
2021-05-14 00:04:03 -0700

    Fix Issue #420, Arc Reverse Crash. Transparency Palette Image.

--- meerk40t/core/cutcode.py
+++ meerk40t/core/cutcode.py
@@ -303,7 +303,8 @@ class ArcCut(CutObject):
         return self.arc.end
 
     def reverse(self):
-        self.arc = copy(self.arc).reversed()
+        self.arc = copy(self.arc)
+        self.arc.reverse()
 
     def generator(self):
         return ZinglPlotter.plot_arc(self.arc)
@@ -339,7 +340,8 @@ class RasterCut(CutObject):
         width, height = image.size
         mode = image.mode
 
-        if mode not in ("1", "P", "L", "RGB", "RGBA"):
+        if mode not in ("1", "P", "L", "RGB", "RGBA") or \
+                mode is "P" and 'transparency' in image.info:
             # Any mode without a filter should get converted.
             image = image.convert("RGBA")
             mode = image.mode
