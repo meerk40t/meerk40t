12ad5275ff590a124ba89602301d4e2e032692c2
Tatarize <tatarize@gmail.com>
2021-05-14 00:07:22 -0700

    Fix mode 'P' with transparency

--- meerk40t/core/cutcode.py
+++ meerk40t/core/cutcode.py
@@ -341,7 +341,8 @@ class RasterCut(CutObject):
         width, height = image.size
         mode = image.mode
 
-        if mode not in ("1", "P", "L", "RGB", "RGBA"):
+        if mode not in ("1", "P", "L", "RGB", "RGBA") or \
+                mode is "P" and 'transparency' in image.info:
             # Any mode without a filter should get converted.
             image = image.convert("RGBA")
             mode = image.mode
