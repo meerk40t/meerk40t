448b3f6c946b219fae19ef13fc011f2382cf7e71
Tatarize <tatarize@gmail.com>
2021-06-06 14:01:12 -0700

    Toolbar Control includes cameras

--- meerk40t/gui/panes/toolbarcontrol.py
+++ meerk40t/gui/panes/toolbarcontrol.py
@@ -1,42 +1,22 @@
 import wx
 from wx import aui
-
-ID_MAIN_TOOLBAR = wx.NewId()
-ID_ADD_FILE = wx.NewId()
-ID_OPEN = wx.NewId()
-
-ID_SAVE = wx.NewId()
-ID_NAV = wx.NewId()
-ID_USB = wx.NewId()
-ID_CONTROLLER = wx.NewId()
-ID_CONFIGURATION = wx.NewId()
-ID_DEVICES = wx.NewId()
-ID_CAMERA = wx.NewId()
-ID_CAMERA1 = wx.NewId()
-ID_CAMERA2 = wx.NewId()
-ID_CAMERA3 = wx.NewId()
-ID_CAMERA4 = wx.NewId()
-ID_CAMERA5 = wx.NewId()
-ID_JOB = wx.NewId()
-ID_SIM = wx.NewId()
-ID_PAUSE = wx.NewId()
-ID_STOP = wx.NewId()
-
-ID_SPOOLER = wx.NewId()
-ID_KEYMAP = wx.NewId()
-ID_SETTING = wx.NewId()
-ID_NOTES = wx.NewId()
-ID_OPERATIONS = wx.NewId()
-ID_CONSOLE = wx.NewId()
-ID_ROTARY = wx.NewId()
-ID_RASTER = wx.NewId()
-
+from wx.aui import EVT_AUITOOLBAR_TOOL_DROPDOWN
 from ..icons import (
     icons8_camera_50,
     icons8_connected_50,
     icons8_move_50,
     icons8_route_50,
 )
+
+ID_NAV = wx.NewId()
+ID_CONTROLLER = wx.NewId()
+ID_CAMERA = wx.NewId()
+ID_CAMERA1 = wx.NewId()
+ID_CAMERA2 = wx.NewId()
+ID_CAMERA3 = wx.NewId()
+ID_CAMERA4 = wx.NewId()
+ID_CAMERA5 = wx.NewId()
+ID_SPOOLER = wx.NewId()
 
 _ = wx.GetTranslation
 
@@ -64,17 +44,45 @@ def register_control_tools(context, gui):
             kind=wx.ITEM_NORMAL,
             short_help_string=_("Opens Camera Window"),
         )
-        toolbar.Bind(wx.EVT_TOOL, gui.on_camera_click, id=ID_CAMERA)
-        # self.Bind(
-        #     RB.EVT_RIBBONBUTTONBAR_DROPDOWN_CLICKED,
-        #     self.on_camera_dropdown,
-        #     id=ID_CAMERA,
-        # )
-        # self.Bind(wx.EVT_MENU, self.on_camera_click, id=ID_CAMERA1)
-        # self.Bind(wx.EVT_MENU, self.on_camera_click, id=ID_CAMERA2)
-        # self.Bind(wx.EVT_MENU, self.on_camera_click, id=ID_CAMERA3)
-        # self.Bind(wx.EVT_MENU, self.on_camera_click, id=ID_CAMERA4)
-        # self.Bind(wx.EVT_MENU, self.on_camera_click, id=ID_CAMERA5)
+
+        def on_camera_click(event=None):
+            eid = event.GetId()
+            context.setting(int, "camera_default", 1)
+            if eid == ID_CAMERA1:
+                context.camera_default = 1
+            elif eid == ID_CAMERA2:
+                context.camera_default = 2
+            elif eid == ID_CAMERA3:
+                context.camera_default = 3
+            elif eid == ID_CAMERA4:
+                context.camera_default = 4
+            elif eid == ID_CAMERA5:
+                context.camera_default = 5
+
+            v = context.camera_default
+            context("camwin %d\n" % v)
+
+        def on_camera_dropdown(event=None):
+            menu = wx.Menu()
+            menu.Append(ID_CAMERA1, _("Camera %d") % 1)
+            menu.Append(ID_CAMERA2, _("Camera %d") % 2)
+            menu.Append(ID_CAMERA3, _("Camera %d") % 3)
+            menu.Append(ID_CAMERA4, _("Camera %d") % 4)
+            menu.Append(ID_CAMERA5, _("Camera %d") % 5)
+            menu.Bind(wx.EVT_MENU, on_camera_click, id=ID_CAMERA1)
+            menu.Bind(wx.EVT_MENU, on_camera_click, id=ID_CAMERA2)
+            menu.Bind(wx.EVT_MENU, on_camera_click, id=ID_CAMERA3)
+            menu.Bind(wx.EVT_MENU, on_camera_click, id=ID_CAMERA4)
+            menu.Bind(wx.EVT_MENU, on_camera_click, id=ID_CAMERA5)
+            gui.PopupMenu(menu)
+
+        toolbar.SetToolDropDown(ID_CAMERA, True)
+        toolbar.Bind(
+            EVT_AUITOOLBAR_TOOL_DROPDOWN,
+            on_camera_dropdown,
+            id=ID_CAMERA,
+        )
+        toolbar.Bind(wx.EVT_TOOL, on_camera_click, id=ID_CAMERA)
 
     toolbar.AddTool(
         ID_SPOOLER,
@@ -100,7 +108,6 @@ def register_control_tools(context, gui):
         lambda v: context("window toggle -o Controller\n"),
         id=ID_CONTROLLER,
     )
-
     toolbar.Create(gui)
 
     pane = (
