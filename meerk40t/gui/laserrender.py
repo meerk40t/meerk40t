365e377b3c65718fd259e6e1eb5e125d9bc80e4c
Tatarize <tatarize@gmail.com>
2021-07-11 01:22:55 -0700

    Correct 2 billion width pen crash.

--- meerk40t/gui/laserrender.py
+++ meerk40t/gui/laserrender.py
@@ -151,7 +151,10 @@ class LaserRender:
             swizzle_color = swizzlecolor(c)
             self.color.SetRGBA(swizzle_color | c.alpha << 24)  # wx has BBGGRR
             self.pen.SetColour(self.color)
-            self.pen.SetWidth(width)
+            try:
+                self.pen.SetWidth(width)
+            except OverflowError:
+                pass  # Exceeds 32 bit signed integer.
             gc.SetPen(self.pen)
         else:
             gc.SetPen(wx.TRANSPARENT_PEN)
