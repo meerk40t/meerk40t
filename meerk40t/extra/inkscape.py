9a748b156152563bdb6c1d8464905e5af121e138
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2021-08-24 12:56:11 +0100

    Various inkscape improvements

--- meerk40t/extra/inkscape.py
+++ meerk40t/extra/inkscape.py
@@ -15,6 +15,9 @@ def plugin(kernel, lifecycle):
         )
         def inscape_load(channel, _, data=None, **kwargs):
             inkscape_path, filename = data
+            if not os.path.exists(inkscape_path):
+                channel(_("Inkscape not found. Try 'inkscape locate'"))
+                return
             channel(_("Loading..."))
             e = kernel.root
             e.load(filename)
@@ -29,6 +32,9 @@ def plugin(kernel, lifecycle):
         )
         def inkscape_simplify(channel, _, data=None, **kwargs):
             inkscape_path, filename = data
+            if not os.path.exists(inkscape_path):
+                channel(_("Inkscape not found. Try 'inkscape locate'"))
+                return
             channel(_("Making plain_svg with Inkscape."))
             c = run(
                 [
@@ -50,6 +56,9 @@ def plugin(kernel, lifecycle):
         )
         def inkscape_text2path(channel, _, data=None, **kwargs):
             inkscape_path, filename = data
+            if not os.path.exists(inkscape_path):
+                channel(_("Inkscape not found. Try 'inkscape locate'"))
+                return
             channel(_("Making plain_svg with Inkscape."))
             c = run(
                 [
@@ -74,6 +83,9 @@ def plugin(kernel, lifecycle):
             if step is not None and step > 0:
                 dpi = 1000 / step
             inkscape_path, filename = data
+            if not os.path.exists(inkscape_path):
+                channel(_("Inkscape not found. Try 'inkscape locate'"))
+                return
             channel(_("Making PNG with Inkscape."))
             c = run(
                 [
@@ -120,10 +132,10 @@ def plugin(kernel, lifecycle):
         def inkscape_version(channel, _, data, **kwargs):
             inkscape_path, filename = data
             if not os.path.exists(inkscape_path):
-                channel(_("Inkscape not found."))
+                channel(_("Inkscape not found. Try 'inkscape locate'"))
                 return
             c = run([inkscape_path, "-V"], stdout=PIPE)
-            channel(c.stdout)
+            channel('Inkscape executable at "%s" is: %s' % (inkscape_path, c.stdout.decode("utf-8")))
             return "inkscape", data
 
         @kernel.console_command(
@@ -133,6 +145,7 @@ def plugin(kernel, lifecycle):
             output_type="inkscape",
         )
         def inkscape_locate(channel, _, data, **kwargs):
+            print("platform",platform)
             if "darwin" in platform:
                 inkscape = [
                     "/Applications/Inkscape.app/Contents/MacOS/Inkscape",
@@ -140,10 +153,10 @@ def plugin(kernel, lifecycle):
                 ]
             elif "win" in platform:
                 inkscape = [
-                    "C:/Program Files/Inkscape/inkscape.exe",
                     "C:/Program Files (x86)/Inkscape/inkscape.exe",
-                    "C:/Program Files/Inkscape/bin/inkscape.exe",
                     "C:/Program Files (x86)/Inkscape/bin/inkscape.exe",
+                    "C:/Program Files/Inkscape/inkscape.exe",
+                    "C:/Program Files/Inkscape/bin/inkscape.exe",
                 ]
             elif "linux" in platform:
                 inkscape = [
@@ -151,7 +164,7 @@ def plugin(kernel, lifecycle):
                     "/usr/bin/inkscape",
                 ]
             else:
-                channel(_("Platform inkscape locations unknown."))
+                channel(_("Inkscape location: Platform '%s' unknown so no idea where to look") % platform)
                 return
             inkscape_path, filename = data
             channel(_("----------"))
@@ -165,10 +178,12 @@ def plugin(kernel, lifecycle):
                     result = _("Fail")
                 channel(_("Searching: %s -- Result: %s") % (ink, result))
             channel(_("----------"))
-            if match is None:
-                raise ModuleNotFoundError
             root_context = kernel.root
             root_context.setting(str, "inkscape_path", "inkscape.exe")
+            if match is None:
+                root_context.inkscape_path = ""
+                channel(_("Inkscape location: Inkscape not found"))
+                return
             root_context.inkscape_path = match
             return "inkscape", (match, filename)
 
