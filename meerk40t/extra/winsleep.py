9b3fbc7451d0fe9b171b40d2af80fcd9aaae7bbf
Tatarize <tatarize@gmail.com>
2021-10-01 16:43:11 -0700

    Sleep_disable ordering

--- meerk40t/extra/winsleep.py
+++ meerk40t/extra/winsleep.py
@@ -1,10 +1,10 @@
 import sys
 
-SLEEP_DISABLED = [dict(), None, False]
+SLEEP_DISABLED = [None, dict(), False]
 
 
 def on_usb_running(origin, value):
-    running = SLEEP_DISABLED[0]
+    running = SLEEP_DISABLED[1]
     running[origin] = value
     any = False
     for v in running:
@@ -15,9 +15,9 @@ def on_usb_running(origin, value):
     if any != SLEEP_DISABLED[2]:
         SLEEP_DISABLED[2] = any
         if any:
-            SLEEP_DISABLED[1](".sleepmode_disable\n")
+            SLEEP_DISABLED[0](".sleepmode_disable\n")
         else:
-            SLEEP_DISABLED[1](".sleepmode_enable\n")
+            SLEEP_DISABLED[0](".sleepmode_enable\n")
 
 
 def plugin(kernel, lifecycle):
@@ -31,7 +31,7 @@ def plugin(kernel, lifecycle):
     elif lifecycle == "register":
         context = kernel.root
 
-        SLEEP_DISABLED[1] = context
+        SLEEP_DISABLED[0] = context
         _ = kernel.translation
 
         @context.console_command("sleepmode_disable", help=_("disables sleepmode"), hidden=True)
