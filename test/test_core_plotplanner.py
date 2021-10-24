012c179a1da79d3b886a7e1166ff8ccfa4e20e01
Tatarize <tatarize@gmail.com>
2021-10-24 10:57:14 -0700

    +test code plotplanner

--- test/test_core_plotplanner.py
+++ test/test_core_plotplanner.py
@@ -1,9 +1,13 @@
 
 import unittest
 
-from meerk40t.core.cutcode import LaserSettings, LineCut
+from PIL import Image, ImageDraw
+
+from meerk40t.core.cutcode import LaserSettings, LineCut, CutCode
+from meerk40t.core.elements import LaserOperation
 from meerk40t.core.plotplanner import PlotPlanner
-from meerk40t.svgelements import Point
+from meerk40t.device.basedevice import PLOT_AXIS, PLOT_SETTING
+from meerk40t.svgelements import Point, SVGImage, Path, Circle
 
 
 class TestPlotplanner(unittest.TestCase):
@@ -38,3 +42,50 @@ class TestPlotplanner(unittest.TestCase):
                     plan.clear()
                     break
                 q += 1
+
+    def test_plotplanner_walk_raster(self):
+        """
+        Test plotplanner operation of walking to a raster.
+
+        PLOT_FINISH = 256
+        PLOT_RAPID = 4
+        PLOT_JOG = 2
+        PLOT_SETTING = 128
+        PLOT_AXIS = 64
+        PLOT_DIRECTION = 32
+        PLOT_LEFT_UPPER = 512
+        PLOT_RIGHT_LOWER = 1024
+
+        1 means cut.
+        0 means move.
+
+        :return:
+        """
+
+        rasterop = LaserOperation(operation="Raster")
+        svg_image = SVGImage()
+        svg_image.image = Image.new("RGBA", (256, 256))
+        draw = ImageDraw.Draw(svg_image.image)
+        draw.ellipse((0, 0, 255, 255), "black")
+        rasterop.add(svg_image, type="opnode")
+
+        vectorop = LaserOperation(operation="Engrave")
+        vectorop.add(Path(Circle(cx=127, cy=127, r=128, fill="black")), type="opnode")
+        cutcode = CutCode()
+        cutcode.extend(vectorop.as_cutobjects())
+        cutcode.extend(rasterop.as_cutobjects())
+
+        plan = PlotPlanner(LaserSettings(power=500))
+        for c in cutcode.flat():
+            plan.push(c)
+
+        setting_changed = False
+        for x, y, on in plan.gen():
+            if on > 2:
+                if setting_changed:
+                    # Settings change happens at vector to raster switch and must repost the axis.
+                    self.assertEqual(on, PLOT_AXIS)
+                if on == PLOT_SETTING:
+                    setting_changed = True
+                else:
+                    setting_changed = False
