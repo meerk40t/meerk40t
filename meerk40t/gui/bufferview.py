020703e5466846a7f2b33fc4ecfb6e8dd5da5e94
Tatarize <tatarize@gmail.com>
2021-10-27 01:10:29 -0700

    Change bufferview menu

--- meerk40t/gui/bufferview.py
+++ meerk40t/gui/bufferview.py
@@ -65,20 +65,17 @@ class BufferView(MWindow):
         # begin wxGlade: BufferView.__set_properties
         self.SetTitle(_("BufferView"))
 
-        # Menu Bar
-        self.BufferView_menubar = wx.MenuBar()
-        wxglade_tmp_menu = wx.Menu()
-        item = wxglade_tmp_menu.Append(
-            wx.ID_ANY, _("Export EGV"), _("Export Engrave Data")
-        )
-        self.Bind(wx.EVT_MENU, self.on_menu_export, id=item.GetId())
-        item = wxglade_tmp_menu.Append(
-            wx.ID_ANY, _("Import EGV"), _("Import Engrave Data")
-        )
-        self.Bind(wx.EVT_MENU, self.on_menu_import, id=item.GetId())
-        self.BufferView_menubar.Append(wxglade_tmp_menu, _("File"))
-        self.SetMenuBar(self.BufferView_menubar)
-        # Menu Bar end
+        # ==========
+        # MENU BAR
+        # ==========
+        from sys import platform as _platform
+        if _platform != "darwin":
+            self.BufferView_menubar = wx.MenuBar()
+            self.create_menu(self.BufferView_menubar.Append)
+            self.SetMenuBar(self.BufferView_menubar)
+        # ==========
+        # MENUBAR END
+        # ==========
 
     def on_menu_export(self, event=None):  # wxGlade: BufferView.<event_handler>
         self.context.root("dev egv_export\n")
