6303f4bbd9b8fe47136a7862aaeb5c2da878337f
Tatarize <tatarize@gmail.com>
2021-10-27 01:05:55 -0700

    BufferView to frame

--- meerk40t/gui/bufferview.py
+++ meerk40t/gui/bufferview.py
@@ -6,29 +6,15 @@ from .mwindow import MWindow
 _ = wx.GetTranslation
 
 
-class BufferView(MWindow):
-    def __init__(self, *args, **kwds):
-        super().__init__(697, 586, *args, **kwds)
-
+class BufferViewPanel(wx.Panel):
+    def __init__(self, *args, context=None, **kwds):
+        kwds["style"] = kwds.get("style", 0) | wx.TAB_TRAVERSAL
+        wx.Panel.__init__(self, *args, **kwds)
+        self.context = context
         self.text_buffer_length = wx.TextCtrl(self, wx.ID_ANY, "")
         self.text_buffer_info = wx.TextCtrl(
             self, wx.ID_ANY, "", style=wx.TE_CHARWRAP | wx.TE_MULTILINE
         )
-
-        # Menu Bar
-        self.BufferView_menubar = wx.MenuBar()
-        wxglade_tmp_menu = wx.Menu()
-        item = wxglade_tmp_menu.Append(
-            wx.ID_ANY, _("Export EGV"), _("Export Engrave Data")
-        )
-        self.Bind(wx.EVT_MENU, self.on_menu_export, id=item.GetId())
-        item = wxglade_tmp_menu.Append(
-            wx.ID_ANY, _("Import EGV"), _("Import Engrave Data")
-        )
-        self.Bind(wx.EVT_MENU, self.on_menu_import, id=item.GetId())
-        self.BufferView_menubar.Append(wxglade_tmp_menu, _("File"))
-        self.SetMenuBar(self.BufferView_menubar)
-        # Menu Bar end
 
         self.__set_properties()
         self.__do_layout()
@@ -50,11 +36,6 @@ class BufferView(MWindow):
         self.text_buffer_info = self.text_buffer_info.SetValue(buffer)
 
     def __set_properties(self):
-        _icon = wx.NullIcon
-        _icon.CopyFromBitmap(icons8_comments_50.GetBitmap())
-        self.SetIcon(_icon)
-        # begin wxGlade: BufferView.__set_properties
-        self.SetTitle(_("BufferView"))
         self.text_buffer_length.SetMinSize((165, 23))
         # end wxGlade
 
@@ -72,6 +53,32 @@ class BufferView(MWindow):
         # end wxGlade
 
     # end of class BufferView
+
+
+class BufferView(MWindow):
+    def __init__(self, *args, **kwds):
+        super().__init__(697, 586, *args, **kwds)
+        self.panel = BufferViewPanel(self, wx.ID_ANY, context=self.context)
+        _icon = wx.NullIcon
+        _icon.CopyFromBitmap(icons8_comments_50.GetBitmap())
+        self.SetIcon(_icon)
+        # begin wxGlade: BufferView.__set_properties
+        self.SetTitle(_("BufferView"))
+
+        # Menu Bar
+        self.BufferView_menubar = wx.MenuBar()
+        wxglade_tmp_menu = wx.Menu()
+        item = wxglade_tmp_menu.Append(
+            wx.ID_ANY, _("Export EGV"), _("Export Engrave Data")
+        )
+        self.Bind(wx.EVT_MENU, self.on_menu_export, id=item.GetId())
+        item = wxglade_tmp_menu.Append(
+            wx.ID_ANY, _("Import EGV"), _("Import Engrave Data")
+        )
+        self.Bind(wx.EVT_MENU, self.on_menu_import, id=item.GetId())
+        self.BufferView_menubar.Append(wxglade_tmp_menu, _("File"))
+        self.SetMenuBar(self.BufferView_menubar)
+        # Menu Bar end
 
     def on_menu_export(self, event=None):  # wxGlade: BufferView.<event_handler>
         self.context.root("dev egv_export\n")
