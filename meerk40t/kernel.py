712aa4d4a26fc1a17e119134f0c551bbb9e6a0e8
Tatarize <tatarize@gmail.com>
2021-11-28 16:21:15 -0800

    Module direct shutdown calls the detaches for infos

--- meerk40t/kernel.py
+++ meerk40t/kernel.py
@@ -585,6 +585,8 @@ class Context:
         # Call module_close lifecycle event.
         instance.module_close(*args, **kwargs)
         instance._lifecycle = 200
+        self._kernel._signal_detach(instance)
+        self._kernel._lookup_detach(instance)
         self._kernel.update_delegate_lifecycles(instance)
 
     # ==========
