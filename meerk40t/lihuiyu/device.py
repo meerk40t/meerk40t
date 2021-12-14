154fc659254dbff93db062f9cd72c8eab659bcb0
Tatarize <tatarize@gmail.com>
2021-12-14 02:02:28 -0800

    Integrate Driver Base-class into lihuiyu driver.

--- meerk40t/lihuiyu/device.py
+++ meerk40t/lihuiyu/device.py
@@ -1,3 +1,5 @@
+import os
+import platform
 import socket
 import threading
 import time
@@ -7,8 +9,7 @@ from meerk40t.core.spoolers import Spooler
 from meerk40t.tools.zinglplotter import ZinglPlotter
 
 from ..core.cutcode import CutCode, LaserSettings, RawCut
-from ..core.drivers import Driver
-from ..core.plotplanner import grouped
+from ..core.plotplanner import grouped, PlotPlanner
 from ..device.basedevice import (
     DRIVER_STATE_FINISH,
     DRIVER_STATE_MODECHANGE,
@@ -720,7 +721,7 @@ class LihuiyuDevice(Service):
             return self.controller
 
 
-class LhystudiosDriver(Driver):
+class LhystudiosDriver:
     """
     LhystudiosDriver provides Lhystudios specific coding for elements and sends it to the backend
     to write to the usb.
@@ -730,7 +731,47 @@ class LhystudiosDriver(Driver):
 
     def __init__(self, context, *args, **kwargs):
         self.context = context
-        Driver.__init__(self, context=context, name=str(self.context))
+        self.name = str(self.context)
+
+        self.root_context = context.root
+        self.settings = LaserSettings()
+
+        self.next = None
+        self.prev = None
+
+        self.spooler = None
+        self.output = None
+
+        self.process_item = None
+        self.spooled_item = None
+        self.holds = []
+        self.temp_holds = []
+
+        self.current_x = 0
+        self.current_y = 0
+
+        context.setting(bool, "plot_shift", False)
+        self.plot_planner = PlotPlanner(self.settings)
+        self.plot_planner.force_shift = context.plot_shift
+        self.plot = None
+
+        self.state = DRIVER_STATE_RAPID
+        self.properties = 0
+        self.is_relative = False
+        self.laser = False
+        self.root_context.setting(bool, "opt_rapid_between", True)
+        self.root_context.setting(int, "opt_jog_mode", 0)
+        self.root_context.setting(int, "opt_jog_minimum", 127)
+        context._quit = False
+
+        self.rapid = self.root_context.opt_rapid_between
+        self.jog = self.root_context.opt_jog_mode
+        self.rapid_override = False
+        self.rapid_override_speed_x = 50.0
+        self.rapid_override_speed_y = 50.0
+        self._thread = None
+        self._shutdown = False
+        self.last_fetch = None
 
         kernel = context._kernel
         _ = kernel.translation
@@ -762,6 +803,7 @@ class LhystudiosDriver(Driver):
         self.max_y = self.current_y
         self.min_x = self.current_x
         self.min_y = self.current_y
+        self.context = context
 
     def __repr__(self):
         return "LhystudiosDriver(%s)" % self.name
@@ -908,7 +950,12 @@ class LhystudiosDriver(Driver):
         self.is_paused = False
 
     def reset(self):
-        Driver.reset(self)
+        if self.spooler is not None:
+            self.spooler.clear_queue()
+        self.plot_planner.clear()
+        self.spooled_item = None
+        self.temp_holds.clear()
+
         self.context.signal("pipe;buffer", 0)
         self.data_output(b"~I*\n~")
         self.laser = False
@@ -1588,6 +1635,350 @@ class LhystudiosDriver(Driver):
         if dy != 0:
             self.data_output(lhymicro_distance(abs(dy)))
             self.check_bounds()
+
+    ######################
+    # ORIGINAL DRIVER CODE
+    ######################
+
+    def shutdown(self, *args, **kwargs):
+        self._shutdown = True
+
+    def added(self, origin=None, *args):
+        if self._thread is None:
+
+            def clear_thread(*a):
+                self._shutdown = True
+
+            self._thread = self.context.threaded(
+                self._driver_threaded,
+                result=clear_thread,
+                thread_name="Driver(%s)" % self.context.path,
+            )
+            self._thread.stop = clear_thread
+
+    def _driver_threaded(self, *args):
+        """
+        Fetch and Execute.
+
+        :param args:
+        :return:
+        """
+        while True:
+            if self._shutdown:
+                return
+            if self.spooled_item is None:
+                self._fetch_next_item_from_spooler()
+            if self.spooled_item is None:
+                # There is no data to interpret. Fetch Failed.
+                if self.context._quit:
+                    self.context("quit\n")
+                    self._shutdown = True
+                    return
+                time.sleep(0.1)
+            self._process_spooled_item()
+
+    def _process_spooled_item(self):
+        """
+        Default Execution Cycle. If Held, we wait. Otherwise we process the spooler.
+
+        Processes one item in the spooler. If the spooler item is a generator. Process one generated item.
+        """
+        if self.hold():
+            time.sleep(0.01)
+            return
+        if self.plotplanner_process():
+            return
+        if self.spooled_item is None:
+            return  # Fetch Next.
+
+        # We have a spooled item to process.
+        if self.command(self.spooled_item):
+            self.spooled_item = None
+            self.spooler.pop()
+            return
+
+        # We are dealing with an iterator/generator
+        try:
+            e = next(self.spooled_item)
+            if not self.command(e):
+                raise ValueError
+        except StopIteration:
+            # The spooled item is finished.
+            self.spooled_item = None
+            self.spooler.pop()
+
+    def _fetch_next_item_from_spooler(self):
+        """
+        Fetches the next item from the spooler.
+
+        :return:
+        """
+        if self.spooler is None:
+            return  # Spooler does not exist.
+        element = self.spooler.peek()
+
+        if self.last_fetch is not None:
+            self.context.channel("spooler")(
+                "Time between fetches: %f" % (time.time() - self.last_fetch)
+            )
+            self.last_fetch = None
+
+        if element is None:
+            return  # Spooler is empty.
+
+        self.last_fetch = time.time()
+
+        # self.spooler.pop()
+        if isinstance(element, int):
+            self.spooled_item = (element,)
+        elif isinstance(element, tuple):
+            self.spooled_item = element
+        else:
+            self.rapid = self.root_context.opt_rapid_between
+            self.jog = self.root_context.opt_jog_mode
+            try:
+                self.spooled_item = element.generate()
+            except AttributeError:
+                try:
+                    self.spooled_item = element()
+                except TypeError:
+                    # This could be a text element, some unrecognized type.
+                    return
+
+    def command(self, command, *values):
+        """Commands are middle language LaserCommandConstants there values are given."""
+        if isinstance(command, tuple):
+            values = command[1:]
+            command = command[0]
+        if not isinstance(command, int):
+            return False  # Command type is not recognized.
+
+        try:
+            if command == COMMAND_LASER_OFF:
+                self.laser_off()
+            elif command == COMMAND_LASER_ON:
+                self.laser_on()
+            elif command == COMMAND_LASER_DISABLE:
+                self.laser_disable()
+            elif command == COMMAND_LASER_ENABLE:
+                self.laser_enable()
+            elif command == COMMAND_CUT:
+                x, y = values
+                self.cut(x, y)
+            elif command == COMMAND_MOVE:
+                x, y = values
+                self.move(x, y)
+            elif command == COMMAND_JOG:
+                x, y = values
+                self.jog(x, y, mode=0, min_jog=self.context.opt_jog_minimum)
+            elif command == COMMAND_JOG_SWITCH:
+                x, y = values
+                self.jog(x, y, mode=1, min_jog=self.context.opt_jog_minimum)
+            elif command == COMMAND_JOG_FINISH:
+                x, y = values
+                self.jog(x, y, mode=2, min_jog=self.context.opt_jog_minimum)
+            elif command == COMMAND_HOME:
+                self.home(*values)
+            elif command == COMMAND_LOCK:
+                self.lock_rail()
+            elif command == COMMAND_UNLOCK:
+                self.unlock_rail()
+            elif command == COMMAND_PLOT:
+                self.plot_plot(values[0])
+            elif command == COMMAND_BLOB:
+                self.send_blob(values[0], values[1])
+            elif command == COMMAND_PLOT_START:
+                self.plot_start()
+            elif command == COMMAND_SET_SPEED:
+                self.set_speed(values[0])
+            elif command == COMMAND_SET_POWER:
+                self.set_power(values[0])
+            elif command == COMMAND_SET_PPI:
+                self.set_ppi(values[0])
+            elif command == COMMAND_SET_PWM:
+                self.set_pwm(values[0])
+            elif command == COMMAND_SET_STEP:
+                self.set_step(values[0])
+            elif command == COMMAND_SET_OVERSCAN:
+                self.set_overscan(values[0])
+            elif command == COMMAND_SET_ACCELERATION:
+                self.set_acceleration(values[0])
+            elif command == COMMAND_SET_D_RATIO:
+                self.set_d_ratio(values[0])
+            elif command == COMMAND_SET_DIRECTION:
+                self.set_directions(values[0], values[1], values[2], values[3])
+            elif command == COMMAND_SET_INCREMENTAL:
+                self.set_incremental()
+            elif command == COMMAND_SET_ABSOLUTE:
+                self.set_absolute()
+            elif command == COMMAND_SET_POSITION:
+                self.set_position(values[0], values[1])
+            elif command == COMMAND_MODE_RAPID:
+                self.ensure_rapid_mode(*values)
+            elif command == COMMAND_MODE_PROGRAM:
+                self.ensure_program_mode(*values)
+            elif command == COMMAND_MODE_RASTER:
+                self.ensure_raster_mode(*values)
+            elif command == COMMAND_MODE_FINISHED:
+                self.ensure_finished_mode(*values)
+            elif command == COMMAND_WAIT:
+                self.wait(values[0])
+            elif command == COMMAND_WAIT_FINISH:
+                self.wait_finish()
+            elif command == COMMAND_BEEP:
+                OS_NAME = platform.system()
+                if OS_NAME == "Windows":
+                    try:
+                        import winsound
+
+                        for x in range(5):
+                            winsound.Beep(2000, 100)
+                    except Exception:
+                        pass
+                elif OS_NAME == "Darwin":  # Mac
+                    os.system("afplay /System/Library/Sounds/Ping.aiff")
+                else:  # Assuming other linux like system
+                    print("\a")  # Beep.
+            elif command == COMMAND_FUNCTION:
+                if len(values) >= 1:
+                    t = values[0]
+                    if callable(t):
+                        t()
+            elif command == COMMAND_SIGNAL:
+                if isinstance(values, str):
+                    self.context.signal(values, None)
+                elif len(values) >= 2:
+                    self.context.signal(values[0], *values[1:])
+        except AttributeError:
+            pass
+        return True
+
+    def realtime_command(self, command, *values):
+        """Asks for the execution of a realtime command. Unlike the spooled commands these
+        return False if rejected and something else if able to be performed. These will not
+        be queued. If rejected. They must be performed in realtime or cancelled.
+        """
+        try:
+            if command == REALTIME_PAUSE:
+                self.pause()
+            elif command == REALTIME_RESUME:
+                self.resume()
+            elif command == REALTIME_RESET:
+                self.reset()
+            elif command == REALTIME_STATUS:
+                self.status()
+        except AttributeError:
+            pass  # Method doesn't exist.
+
+    def data_output(self, e):
+        self.output.write(e)
+
+    def hold(self):
+        """
+        Holds are criteria to use to pause the data interpretation. These halt the production of new data until the
+        criteria is met. A hold is constant and will always halt the data while true. A temp_hold will be removed
+        as soon as it does not hold the data.
+
+        :return: Whether data interpretation should hold.
+        """
+        temp_hold = False
+        fail_hold = False
+        for i, hold in enumerate(self.temp_holds):
+            if not hold():
+                self.temp_holds[i] = None
+                fail_hold = True
+            else:
+                temp_hold = True
+        if fail_hold:
+            self.temp_holds = [hold for hold in self.temp_holds if hold is not None]
+        if temp_hold:
+            return True
+        for hold in self.holds:
+            if hold():
+                return True
+        return False
+
+    def laser_disable(self, *values):
+        self.settings.laser_enabled = False
+
+    def laser_enable(self, *values):
+        self.settings.laser_enabled = True
+
+    def plot_plot(self, plot):
+        """
+        :param plot:
+        :return:
+        """
+        self.plot_planner.push(plot)
+
+    def plot_start(self):
+        if self.plot is None:
+            self.plot = self.plot_planner.gen()
+
+    def set_power(self, power=1000.0):
+        self.settings.power = power
+        if self.settings.power > 1000.0:
+            self.settings.power = 1000.0
+        if self.settings.power <= 0:
+            self.settings.power = 0.0
+
+    def set_ppi(self, power=1000.0):
+        self.settings.power = power
+        if self.settings.power > 1000.0:
+            self.settings.power = 1000.0
+        if self.settings.power <= 0:
+            self.settings.power = 0.0
+
+    def set_pwm(self, power=1000.0):
+        self.settings.power = power
+        if self.settings.power > 1000.0:
+            self.settings.power = 1000.0
+        if self.settings.power <= 0:
+            self.settings.power = 0.0
+
+    def set_overscan(self, overscan=None):
+        self.settings.overscan = overscan
+
+    def set_incremental(self, *values):
+        self.is_relative = True
+
+    def set_absolute(self, *values):
+        self.is_relative = False
+
+    def set_position(self, x, y):
+        self.current_x = x
+        self.current_y = y
+
+    def wait(self, t):
+        time.sleep(float(t))
+
+    def wait_finish(self, *values):
+        """Adds an additional holding requirement if the pipe has any data."""
+        self.temp_holds.append(lambda: len(self.output) != 0)
+
+    def status(self):
+        parts = list()
+        parts.append("x=%f" % self.current_x)
+        parts.append("y=%f" % self.current_y)
+        parts.append("speed=%f" % self.settings.speed)
+        parts.append("power=%d" % self.settings.power)
+        status = ";".join(parts)
+        self.context.signal("driver;status", status)
+
+    def set_prop(self, mask):
+        self.properties |= mask
+
+    def unset_prop(self, mask):
+        self.properties &= ~mask
+
+    def is_prop(self, mask):
+        return bool(self.properties & mask)
+
+    def toggle_prop(self, mask):
+        if self.is_prop(mask):
+            self.unset_prop(mask)
+        else:
+            self.set_prop(mask)
 
     @property
     def type(self):
