68033db22c71ce9f8281e80e97208484b8a6331f
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2021-12-17 18:43:54 +0000

    Add tests for get_safe_path

--- test/test_kernel.py
+++ test/test_kernel.py
@@ -1,15 +1,10 @@
-
 import unittest
-
 from test import bootstrap
-
 
 class TestKernel(unittest.TestCase):
     def test_kernel_commands(self):
         """
         Tests all commands with no arguments to test for crashes...
-
-        :return:
         """
         kernel = bootstrap.bootstrap()
         try:
@@ -26,3 +21,34 @@ class TestKernel(unittest.TestCase):
                     kernel.console(command.split("/")[-1] + "\n")
         finally:
             kernel.shutdown()
+
+class TestGetSafePath(unittest.TestCase):
+    def test_get_safe_path(self):
+        from meerk40t.kernel import get_safe_path
+        import os
+        """
+        Tests the get_safe_path method for all o/ses
+        """
+        sep = os.sep
+        self.assertEquals(
+            str(get_safe_path("test", system="Darwin")),
+            (
+                os.path.expanduser("~")
+                + sep + "Library" + sep + "Application Support" + sep + "test"
+            )
+        )
+        self.assertEquals(
+            str(get_safe_path("test", system="Windows")),
+            (
+                os.path.expandvars("%LOCALAPPDATA%")
+                + sep + "test"
+            )
+        )
+        self.assertEquals(
+            str(get_safe_path("test", system="Linux")),
+            (
+                os.path.expanduser("~")
+                + sep + ".config" + sep + "test"
+            )
+        )
+
