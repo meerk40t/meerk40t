153a716c20c0dc038bfd26b14b23b8954e746d0b
Tatarize <tatarize@gmail.com>
2021-12-27 01:12:55 -0800

    Balor hard link

--- meerk40t/main.py
+++ meerk40t/main.py
@@ -245,6 +245,9 @@ def run():
     else:
         kernel.add_plugin(dxf_io.plugin)
 
+    from mkplugin import main
+    kernel.add_plugin(main.plugin)
+
     if not args.gui_suppress:
         try:
             from .gui import wxmeerk40t
