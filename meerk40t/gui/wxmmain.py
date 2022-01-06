a88bf8223460cff77435c962b4235655314ecf84
Tatarize <tatarize@gmail.com>
2022-01-06 01:27:36 -0800

    Menubar find index during execution

--- meerk40t/gui/wxmmain.py
+++ meerk40t/gui/wxmmain.py
@@ -1281,10 +1281,14 @@ class MeerK40t(MWindow):
             id=ID_MENU_JOB,
         )
         if self.context.has_feature("modifier/Camera"):
-            v = self.context.setting(int, "camera_default", 0)
+
+            def launch_camera(event=None):
+                v = self.context.setting(int, "camera_default", 0)
+                self.context("window toggle -m {v} CameraInterface {v}\n".format(v=v))
+
             self.Bind(
                 wx.EVT_MENU,
-                lambda e: self.context("window toggle -m {v} CameraInterface {v}\n".format(v=v)),
+                launch_camera,
                 id=ID_MENU_CAMERA,
             )
         self.Bind(
