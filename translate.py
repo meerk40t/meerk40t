eb8f7ac56914ab01a5c7909e2ff58ef927a9d836
jpirnay <jens@pirnay.com>
2022-02-09 15:25:04 +0100

    Translate only if po-File is newer

--- translate.py
+++ translate.py
@@ -13,9 +13,18 @@ def create_mo_files():
 		for po_file in po_files:
 			filename, extension = os.path.splitext(po_file)
 			mo_file = filename + '.mo'
-			po = polib.pofile(d + po_file)
-			po.save_as_mofile(d + mo_file)
-			mo_files.append(d + mo_file)
+			doit = 1
+
+			po_date = os.path.getmtime(d + po_file)
+			mo_date = os.path.getmtime(d + mo_file)
+			if mo_date > po_date:
+				print("mo-File for " + d + po_file + " is newer, so skip it...")
+				doit = 0 
+			if doit == 1:
+				print("Translate " + d + po_file)
+				po = polib.pofile(d + po_file)
+				po.save_as_mofile(d + mo_file)
+				mo_files.append(d + mo_file)
 		data_files.append((d, mo_files))
 	return data_files
 	
