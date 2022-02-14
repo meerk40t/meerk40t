95c7b59a669ba64b6af868204d39ba0201cc4224
Tatarize <tatarize@gmail.com>
2022-02-14 08:43:47 -0800

    Rename commands to avoid overlap of namespace

--- meerk40t/device/lhystudios/lhystudiosdevice.py
+++ meerk40t/device/lhystudios/lhystudiosdevice.py
@@ -211,7 +211,7 @@ def plugin(kernel, lifecycle=None):
             input_type="lhystudios",
             help=_("update movement codes for the drivers"),
         )
-        def realtime_pause(data=None, **kwargs):
+        def code_update(data=None, **kwargs):
             spooler, driver, output = data
             driver.update_codes()
 
@@ -220,7 +220,7 @@ def plugin(kernel, lifecycle=None):
             input_type="lhystudios",
             help=_("abort waiting process on the controller."),
         )
-        def realtime_pause(channel, _, data=None, **kwargs):
+        def realtime_status(channel, _, data=None, **kwargs):
             spooler, driver, output = data
             try:
                 output.update_status()
@@ -232,7 +232,7 @@ def plugin(kernel, lifecycle=None):
             input_type="lhystudios",
             help=_("abort waiting process on the controller."),
         )
-        def realtime_pause(data=None, **kwargs):
+        def realtime_continue(data=None, **kwargs):
             spooler, driver, output = data
             output.abort_waiting = True
 
