7ae7fb4ee1e01739daaa8b2ecd3601efee8da513
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2022-03-03 09:13:51 +0000

    Add Pane menu statusbar help.

--- meerk40t/gui/wxmmain.py
+++ meerk40t/gui/wxmmain.py
@@ -1178,7 +1178,8 @@ class MeerK40t(MWindow):
 
             if pane.caption:
                 id_new = wx.NewId()
-                menu_item = menu_context.AppendCheckItem(id_new, pane.caption, "")
+                help = _("Show/Hide the {name} pane").format(name=pane.caption)
+                menu_item = menu_context.AppendCheckItem(id_new, pane.caption, help)
                 menu_context.Bind(
                     wx.EVT_MENU,
                     toggle_pane(pane),
