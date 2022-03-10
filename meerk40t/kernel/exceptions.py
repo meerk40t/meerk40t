e701299ec09edd4b919633f4a5cafadeff317456
Tatarize <tatarize@gmail.com>
2022-03-10 09:52:24 -0800

    Move importing exceptions to kernel

--- meerk40t/kernel/exceptions.py
+++ meerk40t/kernel/exceptions.py
@@ -1,3 +1,18 @@
+class KernelError(Exception):
+    pass
+
+
+class KernelImportAbort(ImportError, KernelError):
+    """
+    MkImportAbort should be used as follows in plugins that import an optional prerequisite Pypi package:
+
+    try:
+        import wx
+    except ImportError as e:
+        raise Mk40tImportAbort("wx") from e
+    """
+
+
 class CommandMatchRejected(Exception):
     """
     Exception to be raised by a registered console command if the match to the command was erroneous
