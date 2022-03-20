bef0690122ca2cc168684519ea231b60beea4914
Tatarize <tatarize@gmail.com>
2022-03-20 04:46:18 -0700

    Update RC issue pointer.

--- meerk40t/gui/plugin.py
+++ meerk40t/gui/plugin.py
@@ -226,7 +226,7 @@ def plugin(kernel, lifecycle):
                 )
                 answer = dlg.ShowModal()
                 if answer in (wx.YES, wx.ID_YES):
-                    issue_page = "https://github.com/meerk40t/meerk40t/issues/896"
+                    issue_page = "https://github.com/meerk40t/meerk40t/issues/909"
                     import webbrowser
 
                     webbrowser.open(issue_page, new=0, autoraise=True)
