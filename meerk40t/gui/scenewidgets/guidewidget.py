e00f8d68da6e000c56755a6cd232f3fa84895961
jpirnay <jens@pirnay.com>
2022-03-29 23:08:26 +0200

    Initial attempt

--- meerk40t/gui/scenewidgets/guidewidget.py
+++ meerk40t/gui/scenewidgets/guidewidget.py
@@ -4,6 +4,8 @@ import wx
 
 from meerk40t.gui.laserrender import DRAW_MODE_GUIDES
 from meerk40t.gui.scene.widget import Widget
+from meerk40t.gui.scene.sceneconst import HITCHAIN_HIT, RESPONSE_CHAIN, RESPONSE_CONSUME
+from meerk40t.core.units import PX_PER_UNIT, UNITS_PER_PIXEL, Length
 
 
 class GuideWidget(Widget):
@@ -16,6 +18,79 @@ class GuideWidget(Widget):
     def __init__(self, scene):
         Widget.__init__(self, scene, all=False)
         self.scene.context.setting(bool, "show_negative_guide", True)
+        self.edge_gap = 5
+        self.line_length = 20
+        self.scale_x_lower = self.edge_gap
+        self.scale_x_upper = self.scale_x_lower + self.line_length
+        self.scale_y_lower = self.edge_gap
+        self.scale_y_upper = self.scale_y_lower + self.line_length
+
+    def hit(self):
+        return HITCHAIN_HIT
+
+    def contains(self, x, y=None):
+        # Slightly more complex than usual due to left, top area
+        value = False
+        if y is None:
+            y = x.y
+            x = x.x
+
+        if (
+            self.scale_x_lower <= x <= self.scale_x_upper
+            or self.scale_y_lower <= y <= self.scale_y_upper
+        ):
+            value = True
+        return value
+
+    def event(self, window_pos=None, space_pos=None, event_type=None):
+        """
+        Capture and deal with the doubleclick event.
+
+        Doubleclick in the grid loads a menu to remove the background.
+        """
+        if event_type == "hover":
+            return RESPONSE_CHAIN
+        elif event_type == "doubleclick":
+            value = 0
+            p = self.scene.context
+            scaled_conversion = (
+                p.device.length(str(1) + p.units_name, as_float=True)
+                * self.scene.widget_root.scene_widget.matrix.value_scale_x()
+            )
+            sx, sy = self.scene.convert_scene_to_window(
+                [
+                    p.device.unit_width * p.device.origin_x,
+                    p.device.unit_height * p.device.origin_y,
+                ]
+            )
+            mark_point_x = (window_pos[0] - sx) / scaled_conversion
+            mark_point_y = (window_pos[0] - sx) / scaled_conversion
+            print(
+                "Coordinates: x=%.1f, y=%.1f, sx=%.1f, sy=%.1f, mark-x=%.1f, y=%.1f"
+                % (window_pos[0], window_pos[1], sx, sy, mark_point_x, mark_point_y)
+            )
+
+            cx, cy = p.device.physical_to_scene_position(
+                window_pos[0] - sx, window_pos[1] - sy, UNITS_PER_PIXEL
+            )
+            print("cx=%.1f, cy=%.1f" % (cx, cy))
+            is_y = self.scale_x_lower <= space_pos[0] <= self.scale_x_upper
+            is_x = self.scale_y_lower <= space_pos[1] <= self.scale_y_upper
+            if is_x and is_y:
+                self.scene.clear_magnets()
+            elif is_x:
+                # Get the X coordinate from space_pos [0]
+                value = cx
+                self.scene.toggle_x_magnet(value)
+            elif is_y:
+                # Get the Y coordinate form space_pos [1]
+                value = cy
+                self.scene.toggle_y_magnet(value)
+
+            self.scene.request_refresh()
+            return RESPONSE_CONSUME
+        else:
+            return RESPONSE_CHAIN
 
     def process_draw(self, gc):
         """
@@ -32,7 +107,6 @@ class GuideWidget(Widget):
         )
         if scaled_conversion == 0:
             return
-        edge_gap = 5
         wpoints = w / 15.0
         hpoints = h / 15.0
         points = min(wpoints, hpoints)
@@ -53,7 +127,8 @@ class GuideWidget(Widget):
         starts = []
         ends = []
         x = offset_x
-        length = 20
+        length = self.line_length
+        edge_gap = self.edge_gap
         font = wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD)
         gc.SetFont(font, wx.BLACK)
         gc.DrawText(p.units_name, edge_gap, edge_gap)
