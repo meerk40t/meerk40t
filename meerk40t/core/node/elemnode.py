8bf59c13f98332c8bb9f4a9052a2fe2c75f54068
Tatarize <tatarize@gmail.com>
2022-04-04 16:37:01 -0700

    Implement elemnode points values

--- meerk40t/core/node/elemnode.py
+++ meerk40t/core/node/elemnode.py
@@ -25,3 +25,35 @@ class ElemNode(Node):
             drop_node.insert_sibling(drag_node)
             return True
         return False
+
+    def revalidate_points(self):
+        bounds = self.bounds
+        if bounds is None:
+            return
+        if len(self._points) < 9:
+            self._points.append([None] * (9 - len(self._points)))
+        self._points[0] = [bounds[0], bounds[1], "bounds top_left"]
+        self._points[1] = [bounds[2], bounds[1], "bounds top_right"]
+        self._points[2] = [bounds[0], bounds[3], "bounds bottom_left"]
+        self._points[3] = [bounds[2], bounds[3], "bounds bottom_right"]
+        cx = (bounds[0] + bounds[2]) / 2
+        cy = (bounds[1] + bounds[3]) / 2
+        self._points[4] = [cx, cy, "bounds center_center"]
+        self._points[5] = [cx, bounds[1], "bounds top_center"]
+        self._points[6] = [cx, bounds[3], "bounds bottom_center"]
+        self._points[7] = [bounds[0], cy, "bounds center_left"]
+        self._points[8] = [bounds[2], cy, "bounds center_right"]
+        obj = self.object
+        if hasattr(obj, "point"):
+            if len(self._points) <= 11:
+                self._points.append([None] * (11 - len(self._points)))
+            start = obj.point(0)
+            end = obj.point(1)
+            self._points[9] = [start[0], start[1], "endpoint"]
+            self._points[10] = [end[0], end[1], "endpoint"]
+
+    def update_point(self, index, point):
+        return False
+
+    def add_point(self, point, index=None):
+        return False
