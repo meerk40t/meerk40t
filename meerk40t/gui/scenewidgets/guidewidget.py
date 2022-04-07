454939c7ebc9ddcfe97af67db985c232cc7f6661
jpirnay <jens@pirnay.com>
2022-04-07 18:13:48 +0200

    Fix 'Fix' :-(

--- meerk40t/gui/scenewidgets/guidewidget.py
+++ meerk40t/gui/scenewidgets/guidewidget.py
@@ -51,7 +51,7 @@ class GuideWidget(Widget):
         else:
             self.scene.auto_tick = False
             self.scene.tick_distance = value
-        self.scene.signal("grid")
+        self.scene._signal_widget(self.scene.widget_root, "grid")
         self.scene.request_refresh()
 
     def change_tick_event(self, idx):
