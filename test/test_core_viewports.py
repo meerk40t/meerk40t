6171718a7f8a23e8481bd468d483efceb6b9bf5a
Tatarize <tatarize@gmail.com>
2022-04-09 07:51:43 -0700

    Include GRBL-esque tests

--- test/test_core_viewports.py
+++ test/test_core_viewports.py
@@ -1,9 +1,47 @@
 import unittest
 
-from meerk40t.core.units import Length, ViewPort
+from meerk40t.core.units import Length, ViewPort, UNITS_PER_MIL
 
 
 class TestViewport(unittest.TestCase):
+
+    def test_viewport_grbl(self):
+        """
+        Test GRBL-esque viewport.
+        Center x, y.
+
+        :return:
+        """
+        bed_size = "225mm"
+
+        view = ViewPort(
+            bed_size,
+            bed_size,
+            native_scale_x=UNITS_PER_MIL,
+            native_scale_y=UNITS_PER_MIL,
+            origin_x=0,
+            origin_y=1,
+            flip_y=True,
+        )
+        x, y = view.scene_to_device_position(0, 0)
+        self.assertGreaterEqual(x, -10)
+        self.assertGreaterEqual(10, x)
+        self.assertGreaterEqual(y, Length(bed_size).mil - 10)
+        self.assertGreaterEqual(Length(bed_size).mil + 10, y)
+
+        x, y = view.scene_to_device_position(0, float(Length(bed_size)))
+        self.assertGreaterEqual(x, -10)
+        self.assertGreaterEqual(10, x)
+        self.assertGreaterEqual(y, -10)
+        self.assertGreaterEqual(10, y)
+
+        x, y = view.device_to_scene_position(0, Length(bed_size).mil)
+        self.assertGreaterEqual(x, -10)
+        self.assertGreaterEqual(10, x)
+        self.assertGreaterEqual(y, -10)
+        self.assertGreaterEqual(10, y)
+
+
     def test_viewport_balor(self):
         """
         Test Balor-esque viewport.
