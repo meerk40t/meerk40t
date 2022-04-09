f2a2d1abde7260e1abaa3329a702429bd476cc77
Tatarize <tatarize@gmail.com>
2022-04-09 09:02:13 -0700

    Arbitrary viewport

--- test/test_core_viewports.py
+++ test/test_core_viewports.py
@@ -1,9 +1,39 @@
+import random
 import unittest
 
 from meerk40t.core.units import Length, ViewPort, UNITS_PER_MIL
 
 
 class TestViewport(unittest.TestCase):
+    def test_viewport_arbitrary(self):
+        """
+        Test arbitrary viewport.
+
+        :return:
+        """
+        bed_width = Length(amount=random.randint(0, 65535 * 1000)).length_mm
+        bed_height = Length(amount=random.randint(0, 65535 * 1000)).length_mm
+        for i in range(100):
+            view = ViewPort(
+                bed_width,
+                bed_height,
+                user_scale_x=1.0,
+                user_scale_y=1.0,
+                native_scale_x=UNITS_PER_MIL,
+                native_scale_y=UNITS_PER_MIL,
+                origin_x=random.random(),
+                origin_y=random.random(),
+                flip_x=bool(random.randint(0, 1)),
+                flip_y=bool(random.randint(0, 1)),
+                swap_xy=bool(random.randint(0, 1)),
+            )
+
+            x, y = view.scene_to_device_position(0, 0)
+            x, y = view.device_to_scene_position(x, y)
+            self.assertGreaterEqual(x, -10)
+            self.assertGreaterEqual(10, x)
+            self.assertGreaterEqual(y, -10)
+            self.assertGreaterEqual(10, y)
 
     def test_viewport_lihuiyu_user_scale(self):
         """
@@ -39,10 +69,12 @@ class TestViewport(unittest.TestCase):
         x, y = view.device_to_scene_position(0, Length(bed_height).mil)
         self.assertGreaterEqual(x, -10)
         self.assertGreaterEqual(10, x)
-        self.assertGreaterEqual(y, float(Length(bed_height))-10)
-        self.assertGreaterEqual(float(Length(bed_height))+10, y)
+        self.assertGreaterEqual(y, float(Length(bed_height)) - 10)
+        self.assertGreaterEqual(float(Length(bed_height)) + 10, y)
 
-        x, y = view.device_to_scene_position(Length(bed_width).mil, Length(bed_height).mil)
+        x, y = view.device_to_scene_position(
+            Length(bed_width).mil, Length(bed_height).mil
+        )
         self.assertGreaterEqual(x, float(Length(bed_width)) * 1.2 - 10)
         self.assertGreaterEqual(float(Length(bed_width)) * 1.2 + 10, x)
         self.assertGreaterEqual(y, float(Length(bed_height)) - 10)
@@ -66,7 +98,7 @@ class TestViewport(unittest.TestCase):
             native_scale_y=UNITS_PER_MIL,
             origin_x=0,
             origin_y=0,
-            swap_xy=True
+            swap_xy=True,
         )
         x, y = view.scene_to_device_position(0, 0)
         self.assertGreaterEqual(x, -1)
@@ -81,17 +113,18 @@ class TestViewport(unittest.TestCase):
         self.assertGreaterEqual(10, y)
 
         x, y = view.device_to_scene_position(0, Length(bed_height).mil)
-        self.assertGreaterEqual(x, float(Length(bed_height))-10)
-        self.assertGreaterEqual(float(Length(bed_height))+10, x)
+        self.assertGreaterEqual(x, float(Length(bed_height)) - 10)
+        self.assertGreaterEqual(float(Length(bed_height)) + 10, x)
         self.assertGreaterEqual(y, -10)
         self.assertGreaterEqual(10, y)
 
-        x, y = view.device_to_scene_position(Length(bed_width).mil, Length(bed_height).mil)
+        x, y = view.device_to_scene_position(
+            Length(bed_width).mil, Length(bed_height).mil
+        )
         self.assertGreaterEqual(x, float(Length(bed_height)) - 10)
         self.assertGreaterEqual(float(Length(bed_height)) + 10, x)
         self.assertGreaterEqual(y, float(Length(bed_width)) * 1.2 - 10)
         self.assertGreaterEqual(float(Length(bed_width)) * 1.2 + 10, y)
-
 
     def test_viewport_grbl(self):
         """
@@ -139,8 +172,8 @@ class TestViewport(unittest.TestCase):
         view = ViewPort(
             bed_size,
             bed_size,
-            user_scale_x=1.0/Length("1mil").mm,
-            user_scale_y=1.0/Length("1mil").mm,
+            user_scale_x=1.0 / Length("1mil").mm,
+            user_scale_y=1.0 / Length("1mil").mm,
             native_scale_x=UNITS_PER_MIL,
             native_scale_y=UNITS_PER_MIL,
             origin_x=0,
