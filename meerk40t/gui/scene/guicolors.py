43e79390c617faf2c572c547fa32c9d7bd529424
jpirnay <jens@pirnay.com>
2022-04-11 06:41:54 +0200

    Feedback incorporated

--- meerk40t/gui/scene/guicolors.py
+++ meerk40t/gui/scene/guicolors.py
@@ -20,50 +20,50 @@ class GuiColors():
     '''
     def __init__(self, context):
         self.context = context
-        self.default_color = (
-            wx.Colour(0xA0, 0x7F, 0xA0),
-            wx.Colour(0x00, 0x00, 0xFF, 0x40),
-            wx.Colour(0xA0, 0xA0, 0xA0, 128),
-            wx.Colour(0x00, 0x00, 0x00),
-            wx.Colour("Grey"),
-            wx.Colour(0xFF, 0xA0, 0xA0, 0x60),
-            wx.Colour(0xA0, 0xA0, 0xA0, 0x40),
-            wx.Colour(0x00, 0xFF, 0x00, 0xA0),
-            wx.Colour(0xFF, 0x00, 0x00),
-            wx.Colour(0x00, 0xFF, 0x00),
-            wx.Colour(0x00, 0x00, 0xFF),
-            wx.Colour(0xFF, 0xFF, 0xFF)
-        )
+        self.default_color = {
+            "manipulation": wx.Colour(0xA0, 0x7F, 0xA0),
+            "laserpath": wx.Colour(0x00, 0x00, 0xFF, 0x40),
+            "grid": wx.Colour(0xA0, 0xA0, 0xA0, 128),
+            "guide": wx.Colour(0x00, 0x00, 0x00),
+            "background": wx.Colour("Grey"),
+            "magnetline": wx.Colour(0xFF, 0xA0, 0xA0, 0x60),
+            "snap_visible": wx.Colour(0xA0, 0xA0, 0xA0, 0x40),
+            "snap_closeup": wx.Colour(0x00, 0xFF, 0x00, 0xA0),
+            "selection1": wx.Colour(0xFF, 0x00, 0x00),
+            "selection2": wx.Colour(0x00, 0xFF, 0x00),
+            "selection3": wx.Colour(0x00, 0x00, 0xFF),
+            "bed": wx.Colour(0xFF, 0xFF, 0xFF)
+        }
 
-        self.context.setting(str, "color_manipulation", color_to_str(self.default_color[0].GetRGBA()))
-        self.context.setting(str, "color_laserpath", color_to_str(self.default_color[1].GetRGBA()))
-        self.context.setting(str, "color_grid", color_to_str(self.default_color[2].GetRGBA()))
-        self.context.setting(str, "color_guide", color_to_str(self.default_color[3].GetRGBA()))
-        self.context.setting(str, "color_background", color_to_str(self.default_color[4].GetRGBA()))
-        self.context.setting(str, "color_magnetline", color_to_str(self.default_color[5].GetRGBA()))
-        self.context.setting(str, "color_snap_visible", color_to_str(self.default_color[6].GetRGBA()))
-        self.context.setting(str, "color_snap_closeup", color_to_str(self.default_color[7].GetRGBA()))
-        self.context.setting(str, "color_selection1", color_to_str(self.default_color[8].GetRGBA()))
-        self.context.setting(str, "color_selection2", color_to_str(self.default_color[9].GetRGBA()))
-        self.context.setting(str, "color_selection3", color_to_str(self.default_color[10].GetRGBA()))
-        self.context.setting(str, "color_bed", color_to_str(self.default_color[11].GetRGBA()))
+        self.context.setting(str, "color_manipulation", color_to_str(self.default_color["manipulation"].GetRGBA()))
+        self.context.setting(str, "color_laserpath", color_to_str(self.default_color["laserpath"].GetRGBA()))
+        self.context.setting(str, "color_grid", color_to_str(self.default_color["grid"].GetRGBA()))
+        self.context.setting(str, "color_guide", color_to_str(self.default_color["guide"].GetRGBA()))
+        self.context.setting(str, "color_background", color_to_str(self.default_color["background"].GetRGBA()))
+        self.context.setting(str, "color_magnetline", color_to_str(self.default_color["magnetline"].GetRGBA()))
+        self.context.setting(str, "color_snap_visible", color_to_str(self.default_color["snap_visible"].GetRGBA()))
+        self.context.setting(str, "color_snap_closeup", color_to_str(self.default_color["snap_closeup"].GetRGBA()))
+        self.context.setting(str, "color_selection1", color_to_str(self.default_color["selection1"].GetRGBA()))
+        self.context.setting(str, "color_selection2", color_to_str(self.default_color["selection2"].GetRGBA()))
+        self.context.setting(str, "color_selection3", color_to_str(self.default_color["selection3"].GetRGBA()))
+        self.context.setting(str, "color_bed", color_to_str(self.default_color["bed"].GetRGBA()))
 
     def set_default_colors(self):
         '''
         Reset all colors to default values...
         '''
-        self.context("set color_manipulation %s" % color_to_str(self.default_color[0].GetRGBA()))
-        self.context("set color_laserpath %s" % color_to_str(self.default_color[1].GetRGBA()))
-        self.context("set color_grid %s" % color_to_str(self.default_color[2].GetRGBA()))
-        self.context("set color_guide %s" % color_to_str(self.default_color[3].GetRGBA()))
-        self.context("set color_background %s" % color_to_str(self.default_color[4].GetRGBA()))
-        self.context("set color_magnetline %s" % color_to_str(self.default_color[5].GetRGBA()))
-        self.context("set color_snap_visible %s" % color_to_str(self.default_color[6].GetRGBA()))
-        self.context("set color_snap_closeup %s" % color_to_str(self.default_color[7].GetRGBA()))
-        self.context("set color_selection1 %s" % color_to_str(self.default_color[8].GetRGBA()))
-        self.context("set color_selection2 %s" % color_to_str(self.default_color[9].GetRGBA()))
-        self.context("set color_selection3 %s" % color_to_str(self.default_color[10].GetRGBA()))
-        self.context("set color_bed %s" % color_to_str(self.default_color[11].GetRGBA()))
+        self.context.color_manipulation = color_to_str(self.default_color["manipulation"].GetRGBA())
+        self.context.color_laserpath = color_to_str(self.default_color["laserpath"].GetRGBA())
+        self.context.color_grid = color_to_str(self.default_color["grid"].GetRGBA())
+        self.context.color_guide = color_to_str(self.default_color["guide"].GetRGBA())
+        self.context.color_background = color_to_str(self.default_color["background"].GetRGBA())
+        self.context.color_magnetline = color_to_str(self.default_color["magnetline"].GetRGBA())
+        self.context.color_snap_visible = color_to_str(self.default_color["snap_visible"].GetRGBA())
+        self.context.color_snap_closeup = color_to_str(self.default_color["snap_closeup"].GetRGBA())
+        self.context.color_selection1 = color_to_str(self.default_color["selection1"].GetRGBA())
+        self.context.color_selection2 = color_to_str(self.default_color["selection2"].GetRGBA())
+        self.context.color_selection3 = color_to_str(self.default_color["selection3"].GetRGBA())
+        self.context.color_bed = color_to_str(self.default_color["bed"].GetRGBA())
 
     @property
     def color_manipulation(self):
@@ -71,7 +71,8 @@ class GuiColors():
             value = str_to_color(self.context.color_manipulation)
             result = wx.Colour(value)
         except (ValueError, TypeError):
-            result = wx.Colour(self.default_color[0].GetRGBA())
+            result = wx.Colour(self.default_color["manipulation"].GetRGBA())
+            self.context.color_manipulation = color_to_str(self.default_color["manipulation"].GetRGBA())
         return result
 
     @property
@@ -80,7 +81,8 @@ class GuiColors():
             value = str_to_color(self.context.color_laserpath)
             result = wx.Colour(value)
         except (ValueError, TypeError):
-            result = wx.Colour(self.default_color[1].GetRGBA())
+            result = wx.Colour(self.default_color["laserpath"].GetRGBA())
+            self.context.color_laserpath = color_to_str(self.default_color["laserpath"].GetRGBA())
         return result
 
     @property
@@ -89,7 +91,8 @@ class GuiColors():
             value = str_to_color(self.context.color_grid)
             result = wx.Colour(value)
         except (ValueError, TypeError):
-            result = wx.Colour(self.default_color[2].GetRGBA())
+            result = wx.Colour(self.default_color["grid"].GetRGBA())
+            self.context.color_grid = color_to_str(self.default_color["grid"].GetRGBA())
         return result
 
     @property
@@ -98,7 +101,8 @@ class GuiColors():
             value = str_to_color(self.context.color_guide)
             result = wx.Colour(value)
         except (ValueError, TypeError):
-            result = wx.Colour(self.default_color[3].GetRGBA())
+            result = wx.Colour(self.default_color["guide"].GetRGBA())
+            self.context.color_guide = color_to_str(self.default_color["guide"].GetRGBA())
         return result
 
     @property
@@ -107,7 +111,8 @@ class GuiColors():
             value = str_to_color(self.context.color_background)
             result = wx.Colour(value)
         except (ValueError, TypeError):
-            result = wx.Colour(self.default_color[4].GetRGBA())
+            result = wx.Colour(self.default_color["background"].GetRGBA())
+            self.context.color_background = color_to_str(self.default_color["background"].GetRGBA())
         return result
 
     @property
@@ -116,7 +121,8 @@ class GuiColors():
             value = str_to_color(self.context.color_magnetline)
             result = wx.Colour(value)
         except (ValueError, TypeError):
-            result = wx.Colour(self.default_color[5].GetRGBA())
+            result = wx.Colour(self.default_color["magnetline"].GetRGBA())
+            self.context.color_magnetline = color_to_str(self.default_color["magnetline"].GetRGBA())
         return result
 
     @property
@@ -125,7 +131,8 @@ class GuiColors():
             value = str_to_color(self.context.color_snap_visible)
             result = wx.Colour(value)
         except (ValueError, TypeError):
-            result = wx.Colour(self.default_color[6].GetRGBA())
+            result = wx.Colour(self.default_color["snap_visible"].GetRGBA())
+            self.context.color_snap_visible = color_to_str(self.default_color["snap_visible"].GetRGBA())
         return result
 
     @property
@@ -134,7 +141,8 @@ class GuiColors():
             value = str_to_color(self.context.color_snap_closeup)
             result = wx.Colour(value)
         except (ValueError, TypeError):
-            result = wx.Colour(self.default_color[7].GetRGBA())
+            result = wx.Colour(self.default_color["snap_closeup"].GetRGBA())
+            self.context.color_snap_closeup = color_to_str(self.default_color["snap_closeup"].GetRGBA())
         return result
 
     @property
@@ -143,7 +151,8 @@ class GuiColors():
             value = str_to_color(self.context.color_selection1)
             result = wx.Colour(value)
         except (ValueError, TypeError):
-            result = wx.Colour(self.default_color[8].GetRGBA())
+            result = wx.Colour(self.default_color["selection1"].GetRGBA())
+            self.context.color_selection1 = color_to_str(self.default_color["selection1"].GetRGBA())
         return result
 
     @property
@@ -152,7 +161,8 @@ class GuiColors():
             value = str_to_color(self.context.color_selection2)
             result = wx.Colour(value)
         except (ValueError, TypeError):
-            result = wx.Colour(self.default_color[9].GetRGBA())
+            result = wx.Colour(self.default_color["selection2"].GetRGBA())
+            self.context.color_selection2 = color_to_str(self.default_color["selection2"].GetRGBA())
         return result
 
     @property
@@ -161,7 +171,8 @@ class GuiColors():
             value = str_to_color(self.context.color_selection3)
             result = wx.Colour(value)
         except (ValueError, TypeError):
-            result = wx.Colour(self.default_color[10].GetRGBA())
+            result = wx.Colour(self.default_color["selection3"].GetRGBA())
+            self.context.color_selection3 = color_to_str(self.default_color["selection3"].GetRGBA())
         return result
 
     @property
@@ -170,5 +181,6 @@ class GuiColors():
             value = str_to_color(self.context.color_bed)
             result = wx.Colour(value)
         except (ValueError, TypeError):
-            result = wx.Colour(self.default_color[11].GetRGBA())
+            result = wx.Colour(self.default_color["bed"].GetRGBA())
+            self.context.color_bed = color_to_str(self.default_color["bed"].GetRGBA())
         return result
