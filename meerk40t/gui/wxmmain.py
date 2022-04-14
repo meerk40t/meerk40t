f2096a1222b4515cba98c41d8d45173d08bd0f8c
Tatarize <tatarize@gmail.com>
2022-04-13 23:26:35 -0700

    Correct straight save

--- meerk40t/gui/wxmmain.py
+++ meerk40t/gui/wxmmain.py
@@ -894,7 +894,7 @@ class MeerK40t(MWindow):
             else:
                 gui.set_file_as_recently_used(gui.working_file)
                 gui.validate_save()
-                context.save(gui.working_file)
+                context.elements.save(gui.working_file)
 
         @context.console_command("dialog_import_egv", hidden=True)
         def egv_in_dialog(**kwargs):
