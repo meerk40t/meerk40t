9ad977c1daf27879fe2ce0f2c3f32c96279a3435
jpirnay <jens@pirnay.com>
2022-04-26 13:05:12 +0200

    Additional tool: Measure

--- meerk40t/gui/toolwidgets/toolmeasure.py
+++ meerk40t/gui/toolwidgets/toolmeasure.py
@@ -1,7 +1,7 @@
-from math import sqrt, atan, tau
+from math import sin, cos, sqrt, atan, tau
 import wx
 from meerk40t.core.units import Length
-from meerk40t.gui.scene.sceneconst import RESPONSE_CHAIN, RESPONSE_CONSUME
+from meerk40t.gui.scene.sceneconst import RESPONSE_CHAIN, RESPONSE_CONSUME, RESPONSE_ABORT
 from meerk40t.gui.toolwidgets.toolwidget import ToolWidget
 
 
@@ -20,103 +20,110 @@ class MeasureTool(ToolWidget):
         self.length_series = []
         self.mouse_position = None
         self.scene = scene
-        self.pen = wx.Pen()
-        self.pen.SetColour(self.scene.colors.color_manipulation)
-        self.pen.SetStyle(wx.PENSTYLE_DOT)
+        self.line_pen = wx.Pen()
+        self.line_pen.SetColour(self.scene.colors.color_measure_line)
+        self.line_pen.SetStyle(wx.PENSTYLE_DOT)
 
-    def process_draw(self, gc: wx.GraphicsContext):
-        matrix = self.scene.matrix
-        print("Scale-Factors, matrix=%.4f" % matrix.value_scale_x())
+    def process_draw(self, gc):
+        matrix= gc.GetTransform().Get()
         if self.point_series:
             try:
-                self.font_size = 14.0 / matrix.value_scale_x()
+                self.font_size = 10.0 / matrix[0]
             except ZeroDivisionError:
-                matrix.reset()
+                self.font_size = 5000
                 return
+            # print ("Fontsize=%.3f, " % self.font_size)
+            if self.font_size < 1.0:
+                self.font_size = 1.0  # Mac does not allow values lower than 1.
             try:
                 font = wx.Font(self.font_size, wx.SWISS, wx.NORMAL, wx.BOLD)
             except TypeError:
                 font = wx.Font(int(self.font_size), wx.SWISS, wx.NORMAL, wx.BOLD)
-            gc.SetFont(font, self.scene.colors.color_manipulation)
-            gc.SetPen(self.pen)
+            gc.SetFont(font, self.scene.colors.color_measure_text)
+
+            gc.SetPen(self.line_pen)
             gc.SetBrush(wx.TRANSPARENT_BRUSH)
             points = list(self.point_series)
             if self.mouse_position is not None:
                 points.append(self.mouse_position)
-            points.append(points[0])
-            gc.DrawLines(points)
-            first_point = None
+            all_cx = 0
+            all_cy = 0
+            area_x_y = 0
+            area_y_x = 0
             # https://www.wikihow.com/Calculate-the-Area-of-a-Polygon
-            area_xy = 0
-            area_yx = 0
+            idx = -1
+            for pt in points:
+                idx += 1
+                # print("%d, %.1f, %.1f" % (idx, pt[0],pt[1]))
+                all_cx += pt[0]
+                all_cy += pt[1]
+                if idx>0:
+                    area_x_y += last_x * pt[1]
+                    area_y_x += last_y * pt[0]
+                last_x = pt[0]
+                last_y = pt[1]
+
+            # Complet calculation of area by closng the loop
+            area_x_y += last_x * points[0][1]
+            area_y_x += last_y * points[0][0]
+            area = 0.5 * abs(area_x_y - area_y_x)
+
+            points.append(points[0])
+            first_point = None
 
             context = self.scene.context
             units = context.units_name
 
             pt_count = 0
             perimeter = 0
-            all_cx = 0
-            all_cy = 0
-            print(points)
             for pt in points:
                 pt_count += 1
-                print("Pt %d, (%.1f, %.1f)" % (pt_count, pt[0], pt[1]))
-                all_cx += pt[0]
-                all_cy += pt[1]
                 if not first_point is None:
-                    area_xy += first_point[0] * pt[1]
-                    area_yx += first_point[1] * pt[0]
-                    print(
-                        "Area, x*y =%.1f, y*x=%.1f, a=%.1f "
-                        % (area_xy, area_yx, 0.5 * abs(area_xy - area_yx))
-                    )
                     dx = pt[0] - first_point[0]
                     dy = pt[1] - first_point[1]
-                    if dy == 0:
+                    if dx == 0:
+                        slope = 0
                         slope_angle = tau / 4
                     else:
-                        slope = dx / dy
-                        slope_angle = atan(slope)
+                        slope = dy / dx
+                        slope_angle = -1 * atan(slope)
                     dlen = sqrt(dx * dx + dy * dy)
                     cx = (pt[0] + first_point[0]) / 2
                     cy = (pt[1] + first_point[1]) / 2
+
                     perimeter += dlen
+
                     s_txt = str(Length(amount=dlen, digits=2, preferred_units=units))
                     (t_width, t_height) = gc.GetTextExtent(s_txt)
-                    print("Line %d: %s" % (pt_count, s_txt))
-                    gc.DrawText(
-                        s_txt,
-                        cx + 0.5 * t_height,
-                        cy + 0.5 * t_width,
-                        slope_angle,
-                    )
+
+                    tcx = cx - cos(slope_angle) * t_width/2
+                    tcy = cy + sin(slope_angle) * t_width/2
+                    dx = tcx - cx
+                    dy = tcy - dy
+                    gc.DrawText(s_txt, tcx, tcy, slope_angle)
                 first_point = pt
 
-            if pt_count > 2:
+            if pt_count > 3:
+                # remove last point, as this one coincides with the first...
+                pt_count -= 1
                 all_cx = all_cx / pt_count
                 all_cy = all_cy / pt_count
-                area = 0.5 + abs(area_xy - area_yx)
-                # perimeter is in base units, so back to units
-                print("Area before: %.2f" % area)
+                # area is in base units^2, so back to units
+
                 base_square = float(Length("1{units}".format(units=units)))
                 base_square *= base_square
-                print("base square: %.2f" % base_square)
                 area = area / base_square
-                print("Area after: %.5f" % area)
 
-                s_txt = (
-                    "Area: "
-                    + str(Length(amount=area, digits=3, preferred_units=units))
-                    + "²\nTotal Length: "
-                    + str(Length(amount=perimeter, digits=1, preferred_units=units))
-                )
+                s_txt = "Area={area:.1f}{units}²\nPerimeter: {perim}".format(area=area, units=units, perim=str(Length(amount=perimeter, digits=1, preferred_units=units)))
                 (t_width, t_height) = gc.GetTextExtent(s_txt)
                 gc.DrawText(
                     s_txt,
-                    all_cx + 0.5 * t_height,
-                    all_cy + 0.5 * t_width,
+                    all_cx - 0.5 * t_width,
+                    all_cy - 0.5 * t_height,
                 )
-                print("Total: %s" % (s_txt))
+
+            # Draw lines at last to make them more visible
+            gc.DrawLines(points)
 
     def event(self, window_pos=None, space_pos=None, event_type=None):
         response = RESPONSE_CHAIN
@@ -129,7 +136,7 @@ class MeasureTool(ToolWidget):
             self.point_series = []
             self.mouse_position = None
             self.scene.request_refresh()
-            response = RESPONSE_CONSUME
+            response = RESPONSE_ABORT
         elif event_type == "hover":
             self.mouse_position = space_pos[0], space_pos[1]
             if self.point_series:
@@ -139,7 +146,7 @@ class MeasureTool(ToolWidget):
             self.point_series = []
             self.mouse_position = None
             self.scene.request_refresh()
-            response = RESPONSE_CONSUME
+            response = RESPONSE_ABORT
         elif event_type == "lost":
             self.scene.tool_active = False
             self.point_series = []
