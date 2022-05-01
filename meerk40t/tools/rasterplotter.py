6125ed37e341e73199bdc71dd495b6935c47abbf
Tatarize <tatarize@gmail.com>
2022-05-01 06:19:32 -0700

    divide plot_horizontal and plot_vertical

--- meerk40t/tools/rasterplotter.py
+++ meerk40t/tools/rasterplotter.py
@@ -557,7 +557,78 @@ class RasterPlotter:
                 yield offset_x + step_x * x, offset_y + y * step_y, on
 
     def plot_pixels(self):
+        if self.vertical:
+            yield from self.plot_vertical
+        else:
+            yield from self.plot_horizontal
+
+    def plot_vertical(self):
+        """
+        This code is for vertical rastering.
+
+        @return:
+        """
         width = self.width
+
+        skip_pixel = self.skip_pixel
+
+        x, y = self.initial_position()
+        dx = 1
+        dy = 1
+        if self.right:
+            dx = -1
+        if self.bottom:
+            dy = -1
+        yield x, y, 0
+        while 0 <= x < width:
+            lower_bound = self.topmost_not_equal(x)
+            if lower_bound is None:
+                x += dx
+                yield x, y, 0
+                continue
+            upper_bound = self.bottommost_not_equal(x)
+
+            next_x, next_y = self.calculate_next_vertical_pixel(x + dx, dx, dy > 0)
+            if next_y is not None:
+                upper_bound = max(next_y, upper_bound) + self.overscan
+                lower_bound = min(next_y, lower_bound) - self.overscan
+
+            while (dy > 0 and y <= upper_bound) or (dy < 0 and lower_bound <= y):
+                if dy > 0:  # going right
+                    bound = upper_bound
+                    try:
+                        pixel = self.px(x, y)
+                    except IndexError:
+                        pixel = 0
+                    y = self.nextcolor_bottom(x, y, upper_bound)
+                    y = min(y, upper_bound)
+                else:
+                    bound = lower_bound
+                    try:
+                        pixel = self.px(x, y)
+                    except IndexError:
+                        pixel = 0
+                    y = self.nextcolor_top(x, y, lower_bound)
+                    y = max(y, lower_bound)
+                if pixel == skip_pixel:
+                    yield x, y, 0
+                else:
+                    yield x, y, pixel
+                if y == bound:
+                    break
+            if next_x is None:
+                # remaining image is blank, we stop right here.
+                break
+            x = next_x
+            yield x, y, 0
+            dy = -dy
+
+    def plot_horizontal(self):
+        """
+        This code is horizontal rastering.
+
+        @return:
+        """
         height = self.height
 
         skip_pixel = self.skip_pixel
@@ -570,93 +641,47 @@ class RasterPlotter:
         if self.bottom:
             dy = -1
         yield x, y, 0
-        if self.vertical:
-            # This code is for vertical rastering.
-            while 0 <= x < width:
-                lower_bound = self.topmost_not_equal(x)
-                if lower_bound is None:
-                    x += dx
-                    yield x, y, 0
-                    continue
-                upper_bound = self.bottommost_not_equal(x)
-
-                next_x, next_y = self.calculate_next_vertical_pixel(x + dx, dx, dy > 0)
-                if next_y is not None:
-                    upper_bound = max(next_y, upper_bound) + self.overscan
-                    lower_bound = min(next_y, lower_bound) - self.overscan
-
-                while (dy > 0 and y <= upper_bound) or (dy < 0 and lower_bound <= y):
-                    if dy > 0:  # going right
-                        bound = upper_bound
-                        try:
-                            pixel = self.px(x, y)
-                        except IndexError:
-                            pixel = 0
-                        y = self.nextcolor_bottom(x, y, upper_bound)
-                        y = min(y, upper_bound)
-                    else:
-                        bound = lower_bound
-                        try:
-                            pixel = self.px(x, y)
-                        except IndexError:
-                            pixel = 0
-                        y = self.nextcolor_top(x, y, lower_bound)
-                        y = max(y, lower_bound)
-                    if pixel == skip_pixel:
-                        yield x, y, 0
-                    else:
-                        yield x, y, pixel
-                    if y == bound:
-                        break
-                if next_x is None:
-                    # remaining image is blank, we stop right here.
-                    break
-                x = next_x
+        while 0 <= y < height:
+            lower_bound = self.leftmost_not_equal(y)
+            if lower_bound is None:
+                y += dy
                 yield x, y, 0
-                dy = -dy
-        else:
-            # This code is horizontal rastering.
-            while 0 <= y < height:
-                lower_bound = self.leftmost_not_equal(y)
-                if lower_bound is None:
-                    y += dy
+                continue
+            upper_bound = self.rightmost_not_equal(y)
+
+            next_x, next_y = self.calculate_next_horizontal_pixel(
+                y + dy, dy, dx > 0
+            )
+            if next_x is not None:
+                upper_bound = max(next_x, upper_bound) + self.overscan
+                lower_bound = min(next_x, lower_bound) - self.overscan
+
+            while (dx > 0 and x <= upper_bound) or (dx < 0 and lower_bound <= x):
+                if dx > 0:  # going right
+                    bound = upper_bound
+                    try:
+                        pixel = self.px(x, y)
+                    except IndexError:
+                        pixel = 0
+                    x = self.nextcolor_right(x, y, upper_bound)
+                    x = min(x, upper_bound)
+                else:
+                    bound = lower_bound
+                    try:
+                        pixel = self.px(x, y)
+                    except IndexError:
+                        pixel = 0
+                    x = self.nextcolor_left(x, y, lower_bound)
+                    x = max(x, lower_bound)
+                if pixel == skip_pixel:
                     yield x, y, 0
-                    continue
-                upper_bound = self.rightmost_not_equal(y)
-
-                next_x, next_y = self.calculate_next_horizontal_pixel(
-                    y + dy, dy, dx > 0
-                )
-                if next_x is not None:
-                    upper_bound = max(next_x, upper_bound) + self.overscan
-                    lower_bound = min(next_x, lower_bound) - self.overscan
-
-                while (dx > 0 and x <= upper_bound) or (dx < 0 and lower_bound <= x):
-                    if dx > 0:  # going right
-                        bound = upper_bound
-                        try:
-                            pixel = self.px(x, y)
-                        except IndexError:
-                            pixel = 0
-                        x = self.nextcolor_right(x, y, upper_bound)
-                        x = min(x, upper_bound)
-                    else:
-                        bound = lower_bound
-                        try:
-                            pixel = self.px(x, y)
-                        except IndexError:
-                            pixel = 0
-                        x = self.nextcolor_left(x, y, lower_bound)
-                        x = max(x, lower_bound)
-                    if pixel == skip_pixel:
-                        yield x, y, 0
-                    else:
-                        yield x, y, pixel
-                    if x == bound:
-                        break
-                if next_y is None:
-                    # remaining image is blank, we stop right here.
+                else:
+                    yield x, y, pixel
+                if x == bound:
                     break
-                y = next_y
-                yield x, y, 0
-                dx = -dx
+            if next_y is None:
+                # remaining image is blank, we stop right here.
+                break
+            y = next_y
+            yield x, y, 0
+            dx = -dx
