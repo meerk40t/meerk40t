f9640149c4af8b3cb4346ec49c74f5e0372dec6a
Tatarize <tatarize@gmail.com>
2022-05-28 22:15:54 -0700

    Divide up scanline and eulerian fills

--- meerk40t/core/node/op_hatch.py
+++ meerk40t/core/node/op_hatch.py
@@ -172,9 +172,77 @@ class HatchOpNode(Node, Parameters):
             if pos != len(points):
                 yield points[pos : len(points)]
 
-        def create_fill():
+        def create_eulerian_fill():
             """
-            This currently applies Eulerian fill when it could just apply scanline fill.
+            Applies optimized Eulerian fill
+            @return:
+            """
+            c = list()
+            for node in self.children:
+                path = node.as_path()
+                path.approximate_arcs_with_cubics()
+                self.settings["line_color"] = path.stroke
+                for subpath in path.as_subpaths():
+                    sp = Path(subpath)
+                    if len(sp) == 0:
+                        continue
+                    c.append(sp)
+            self.remove_all_children()
+
+            penbox_pass = self.settings.get("penbox_pass")
+            if penbox_pass is not None:
+                try:
+                    penbox_pass = context.elements.penbox[penbox_pass]
+                except KeyError:
+                    penbox_pass = None
+
+            polyline_lookup = dict()
+            for p in range(self.implicit_passes):
+                settings = dict(self.settings)
+                if penbox_pass is not None:
+                    try:
+                        settings.update(penbox_pass[p])
+                    except IndexError:
+                        pass
+                h_dist = settings.get("hatch_distance", "1mm")
+                h_angle = settings.get("hatch_angle", "0deg")
+                distance_y = float(Length(h_dist))
+                if isinstance(h_angle, float):
+                    angle = Angle.degrees(h_angle)
+                    h_angle = str(h_angle)
+                else:
+                    angle = Angle.parse(h_angle)
+
+                key = f"{h_angle},{h_dist}"
+                if key in polyline_lookup:
+                    polylines = polyline_lookup[key]
+                else:
+                    counter_rotate = Matrix.rotate(-angle)
+                    transformed_vector = matrix.transform_vector([0, distance_y])
+                    efill = EulerianFill(
+                        abs(complex(transformed_vector[0], transformed_vector[1]))
+                    )
+                    for sp in c:
+                        sp.transform.reset()
+                        if angle is not None:
+                            sp *= Matrix.rotate(angle)
+                        sp = abs(sp)
+                        efill += [sp.point(i / 100.0, error=1e-4) for i in range(101)]
+                    points = efill.get_fill()
+                    polylines = list()
+                    for pts in split(points):
+                        polyline = Polyline(pts, stoke=settings.get("line_color"))
+                        polyline *= counter_rotate
+                        polylines.append(abs(polyline))
+                    polyline_lookup[key] = polylines
+                for polyline in polylines:
+                    node = PolylineNode(shape=abs(polyline))
+                    node.settings.update(settings)
+                    self.add_node(node)
+
+        def create_scanline_fill():
+            """
+            Apply scanline fill.
             @return:
             """
             c = list()
@@ -241,7 +309,11 @@ class HatchOpNode(Node, Parameters):
                     self.add_node(node)
 
         if self.children:
-            commands.append(create_fill)
+            hatch_type = self.settings.get("hatch_type", 0)
+            if hatch_type == 0:
+                commands.append(create_eulerian_fill)
+            else:
+                commands.append(create_scanline_fill)
 
     def as_cutobjects(self, closed_distance=15, passes=1):
         """Generator of cutobjects for a particular operation."""
