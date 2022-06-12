8c0f01c1df4e2e242db38d0fd8dc91a3379769f1
jpirnay <jens@pirnay.com>
2022-06-12 10:45:04 +0200

    Debug sycolors

--- meerk40t/gui/wxmribbon.py
+++ meerk40t/gui/wxmribbon.py
@@ -17,7 +17,73 @@ ID_PAGE_MAIN = 10
 ID_PAGE_TOOL = 20
 ID_PAGE_TOGGLE = 30
 
+def debug_system_colors():
+    slist = (
+        (wx.SYS_COLOUR_SCROLLBAR, "The scrollbar grey area."),
+        (wx.SYS_COLOUR_DESKTOP, "The desktop colour."),
+        (wx.SYS_COLOUR_ACTIVECAPTION, "Active window caption colour."),
+        (wx.SYS_COLOUR_INACTIVECAPTION, "Inactive window caption colour."),
+        (wx.SYS_COLOUR_MENU, "Menu background colour."),
+        (wx.SYS_COLOUR_WINDOW, "Window background colour."),
+        (wx.SYS_COLOUR_WINDOWFRAME, "Window frame colour."),
+        (wx.SYS_COLOUR_MENUTEXT, "Colour of the text used in the menus."),
+        (wx.SYS_COLOUR_WINDOWTEXT, "Colour of the text used in generic windows."),
+        (wx.SYS_COLOUR_CAPTIONTEXT, "Colour of the text used in captions, size boxes and scrollbar arrow boxes."),
+        (wx.SYS_COLOUR_ACTIVEBORDER, "Active window border colour."),
+        (wx.SYS_COLOUR_INACTIVEBORDER, "Inactive window border colour."),
+        (wx.SYS_COLOUR_APPWORKSPACE, "Background colour for MDI applications."),
+        (wx.SYS_COLOUR_HIGHLIGHT, "Colour of item(s) selected in a control."),
+        (wx.SYS_COLOUR_HIGHLIGHTTEXT, "Colour of the text of item(s) selected in a control."),
+        (wx.SYS_COLOUR_BTNFACE, "Face shading colour on push buttons."),
+        (wx.SYS_COLOUR_BTNSHADOW, "Edge shading colour on push buttons."),
+        (wx.SYS_COLOUR_GRAYTEXT, "Colour of greyed (disabled) text."),
+        (wx.SYS_COLOUR_BTNTEXT, "Colour of the text on push buttons."),
+        (wx.SYS_COLOUR_INACTIVECAPTIONTEXT, "Colour of the text in inactive captions."),
+        (wx.SYS_COLOUR_BTNHIGHLIGHT, "Highlight colour for buttons."),
+        (wx.SYS_COLOUR_3DDKSHADOW, "Dark shadow colour for three-dimensional display elements."),
+        (wx.SYS_COLOUR_3DLIGHT, "Light colour for three-dimensional display elements."),
+        (wx.SYS_COLOUR_INFOTEXT, "Text colour for tooltip controls."),
+        (wx.SYS_COLOUR_INFOBK, "Background colour for tooltip controls."),
+        (wx.SYS_COLOUR_LISTBOX, "Background colour for list-like controls."),
+        (wx.SYS_COLOUR_HOTLIGHT, "Colour for a hyperlink or hot-tracked item."),
+        (wx.SYS_COLOUR_GRADIENTACTIVECAPTION, "Right side colour in the colour gradient of an active window’s title bar."),
+        (wx.SYS_COLOUR_GRADIENTINACTIVECAPTION, "Right side colour in the colour gradient of an inactive window’s title bar."),
+        (wx.SYS_COLOUR_MENUHILIGHT, "The colour used to highlight menu items when the menu appears as a flat menu."),
+        (wx.SYS_COLOUR_MENUBAR, "The background colour for the menu bar when menus appear as flat menus."),
+        (wx.SYS_COLOUR_LISTBOXTEXT, "Text colour for list-like controls."),
+        (wx.SYS_COLOUR_LISTBOXHIGHLIGHTTEXT, "Text colour for the unfocused selection of list-like controls."),
+        (wx.SYS_COLOUR_BACKGROUND, "Synonym for SYS_COLOUR_DESKTOP ."),
+        (wx.SYS_COLOUR_3DFACE, "Synonym for SYS_COLOUR_BTNFACE ."),
+        (wx.SYS_COLOUR_3DSHADOW, "Synonym for SYS_COLOUR_BTNSHADOW ."),
+        (wx.SYS_COLOUR_BTNHILIGHT, "Synonym for SYS_COLOUR_BTNHIGHLIGHT ."),
+        (wx.SYS_COLOUR_3DHIGHLIGHT, "Synonym for SYS_COLOUR_BTNHIGHLIGHT ."),
+        (wx.SYS_COLOUR_3DHILIGHT, "Synonym for SYS_COLOUR_BTNHIGHLIGHT ."),
+        (wx.SYS_COLOUR_FRAMEBK, "Synonym for SYS_COLOUR_BTNFACE "),
+    )
+    is_dark = False
+    dark_bg = False
+    try:
+        sysappearance = wx.SystemSettings().GetAppearance()
+        source = "Sysappearance"
+        is_dark = sysappearance.IsDark()
+        dark_bg = sysappearance.IsUsingDarkBackground()
+    except:
+        source = "Default"
+        pass
+    print("%s delivered: is_dark=%s, dark_bg=%s" % ( source, is_dark, dark_bg))
+    for colpair in slist:
+        syscol = wx.SystemSettings().GetColour(colpair[0])
+        if syscol is None:
+            s = "Null"
+        else:
+            try:
+                s = syscol.GetAsString(wx.C2S_NAME)
+            except AssertionError:
+                s = syscol.GetAsString(wx.C2S_CSS_SYNTAX)
+        print ("%s for %s" % (s, colpair[1]))
+
 def register_panel_ribbon(window, context):
+    debug_system_colors()
     minh = 75 # 150
     pane = (
         aui.AuiPaneInfo()
