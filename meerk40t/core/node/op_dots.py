4a337e0c3d1e5b1e400ae7904366f98bcedad96f
Tatarize <tatarize@gmail.com>
2022-06-30 17:26:18 -0700

    Dot stroke matching is not required, all elem points are classified here

--- meerk40t/core/node/op_dots.py
+++ meerk40t/core/node/op_dots.py
@@ -109,11 +109,6 @@ class DotsOpNode(Node, Parameters):
         return False
 
     def classify(self, node):
-        if not self.default and hasattr(node, "stroke") and node.stroke is not None:
-            plain_color_op = abs(self.color)
-            plain_color_node = abs(node.stroke)
-            if plain_color_op != plain_color_node:
-                return False, False
         if node.type in self.allowed_elements:
             self.add_reference(node)
             return True, True
