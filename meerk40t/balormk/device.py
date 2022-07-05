0fb5826fbae26e518543ce70a78cec49941d8178
Tatarize <tatarize@gmail.com>
2022-07-05 05:25:03 -0700

    Add -r flag for pure hex data

--- meerk40t/balormk/device.py
+++ meerk40t/balormk/device.py
@@ -1115,6 +1115,13 @@ class BalorDevice(Service, ViewPort):
             action="store_true",
         )
         @self.console_option(
+            "raw",
+            "r",
+            help=_("Data is explicitly little-ended hex from a data capture"),
+            type=bool,
+            action="store_true",
+        )
+        @self.console_option(
             "input", "i", type=str, default=None, help="input data for given file"
         )
         @self.console_option(
@@ -1124,7 +1131,16 @@ class BalorDevice(Service, ViewPort):
             "raw",
             help=_("sends raw galvo list command exactly as composed"),
         )
-        def galvo_raw(channel, _, default=False, input=None, output=None, remainder=None, **kwgs):
+        def galvo_raw(
+            channel,
+            _,
+            default=False,
+            raw=False,
+            input=None,
+            output=None,
+            remainder=None,
+            **kwgs,
+        ):
             from meerk40t.balormk.lmc_controller import list_command_lookup
 
             reverse_lookup = {}
@@ -1156,10 +1172,16 @@ class BalorDevice(Service, ViewPort):
                     return
 
             cmds = None
-            if len(remainder) == 0x1800:
+            if len(remainder) == 0x1800 or raw:
                 try:
-                    cmds = [struct.unpack("<6H", bytearray.fromhex(remainder[i:i+24])) for i in range(0, len(remainder), 24)]
-                    cmds = [f'{v[0]:04x} {v[1]:04x} {v[2]:04x} {v[3]:04x} {v[4]:04x} {v[5]:04x}' for v in cmds]
+                    cmds = [
+                        struct.unpack("<6H", bytearray.fromhex(remainder[i : i + 24]))
+                        for i in range(0, len(remainder), 24)
+                    ]
+                    cmds = [
+                        f"{v[0]:04x} {v[1]:04x} {v[2]:04x} {v[3]:04x} {v[4]:04x} {v[5]:04x}"
+                        for v in cmds
+                    ]
                 except (struct.error, ValueError):
                     pass
 
@@ -1212,8 +1234,10 @@ class BalorDevice(Service, ViewPort):
                     try:
                         lines = []
                         for v in raw_commands:
-                            lines.append(f"{list_command_lookup.get(v[0],f'{v[0]:04x}').ljust(20)} "
-                                         f"{v[1]:04x} {v[2]:04x} {v[3]:04x} {v[4]:04x} {v[5]:04x}\n")
+                            lines.append(
+                                f"{list_command_lookup.get(v[0],f'{v[0]:04x}').ljust(20)} "
+                                f"{v[1]:04x} {v[2]:04x} {v[3]:04x} {v[4]:04x} {v[5]:04x}\n"
+                            )
                         with open(output, "w") as f:
                             f.writelines(lines)
                             # f.write(remainder)
