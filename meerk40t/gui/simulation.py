426f769fc2562e2bf40832aae8f604e5378f117a
jpirnay <jens@pirnay.com>
2022-08-09 12:02:47 +0200

    Copy and paste nonsense

--- meerk40t/gui/simulation.py
+++ meerk40t/gui/simulation.py
@@ -607,8 +607,6 @@ class SimulationPanel(wx.Panel, Job):
     def on_combo_device(self, event=None):  # wxGlade: Preview.<event_handler>
         index = self.combo_device.GetSelection()
         self.selected_device = self.available_devices[index]
-        index = self.combo_device.GetSelection()
-        self.selected_device = self.available_devices[index]
         self.selected_device.kernel.activate_service_path(
             "device", self.selected_device.path
         )
