1af7cb5273da77e860c22a605656d7717ecb97db
Tatarize <tatarize@gmail.com>
2022-08-10 00:30:31 -0700

    rename duplicate function name

--- meerk40t/gui/wxmeerk40t.py
+++ meerk40t/gui/wxmeerk40t.py
@@ -496,7 +496,7 @@ class wxMeerK40t(wx.App, Module):
             output_type="window",
             help=_("Give display info for the current opened windows"),
         )
-        def window_list(channel, _, data, **kwargs):
+        def displays(channel, _, data, **kwargs):
             for idx in range(wx.Display.GetCount()):
                 d = wx.Display(idx)
                 channel(f"{idx} Primary: {d.IsPrimary()} {d.GetGeometry()}")
