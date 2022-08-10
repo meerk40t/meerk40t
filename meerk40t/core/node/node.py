b47000cf6a9a4fa651c4690e0614cc8420f1a9f0
Tatarize <tatarize@gmail.com>
2022-08-09 23:02:07 -0700

    objects of children no longer useful, nodes don't universally have objects (most are their own classes)

--- meerk40t/core/node/node.py
+++ meerk40t/core/node/node.py
@@ -609,13 +609,6 @@ class Node:
     def count_children(self):
         return len(self._children)
 
-    def objects_of_children(self, types):
-        if isinstance(self.object, types):
-            yield self.object
-        for q in self._children:
-            for o in q.objects_of_children(types):
-                yield o
-
     def append_child(self, new_child):
         """
         Add the new_child node as the last child of the current node.
