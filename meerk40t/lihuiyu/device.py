05a7aa0a4aff926050435dbdfb1a5b607ac6c3b2
Tatarize <tatarize@gmail.com>
2022-08-19 22:39:39 -0700

    fix location for raster_alt and _force_twitchless

--- meerk40t/lihuiyu/device.py
+++ meerk40t/lihuiyu/device.py
@@ -1354,7 +1354,7 @@ class LihuiyuDriver(Parameters):
         self.step_index = 0
         self.step = self.raster_step_x
         self.step_value_set = 0
-        if self.service.settings.get("_raster_alt", False):
+        if self.settings.get("_raster_alt", False):
             pass
         elif self.service.nse_raster and not self.service.nse_stepraster:
             pass
@@ -1365,7 +1365,7 @@ class LihuiyuDriver(Parameters):
         suffix_c = None
         if (
             not self.service.twitches
-            or self.service.settings.get("_force_twitchless", False)
+            or self.settings.get("_force_twitchless", False)
         ) and not self.step:
             suffix_c = True
         if self._request_leftward is not None:
