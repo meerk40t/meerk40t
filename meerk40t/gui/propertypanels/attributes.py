30ebfb4e0b599480cba71bde1e9e10de46277ddc
jpirnay <jens@pirnay.com>
2022-08-28 21:54:10 +0200

    Minor adjustments

--- meerk40t/gui/propertypanels/attributes.py
+++ meerk40t/gui/propertypanels/attributes.py
@@ -276,8 +276,8 @@ class PositionSizePanel(wx.Panel):
         sizer_v_xywh.Add(sizer_h_xy, 0, wx.EXPAND, 0)
         sizer_v_xywh.Add(sizer_h_wh, 0, wx.EXPAND, 0)
 
-        sizer_main.Add(sizer_v_xywh, 0, wx.EXPAND, 0)
         sizer_main.Add(sizer_lock, 0, wx.EXPAND, 0)
+        sizer_main.Add(sizer_v_xywh, 0, wx.EXPAND, 0)
 
         self.SetSizer(sizer_main)
         sizer_main.Fit(self)
@@ -324,10 +324,10 @@ class PositionSizePanel(wx.Panel):
             y = bb[1]
             w = bb[2] - bb[0]
             h = bb[3] - bb[1]
-            self.text_x.SetValue(f"{Length(x, unitless=1).mm:.4f}mm")
-            self.text_y.SetValue(f"{Length(y, unitless=1).mm:.4f}mm")
-            self.text_w.SetValue(f"{Length(w, unitless=1).mm:.4f}mm")
-            self.text_h.SetValue(f"{Length(h, unitless=1).mm:.4f}mm")
+            self.text_x.SetValue(Length(x, unitless=1, digits=4).length_mm)
+            self.text_y.SetValue(Length(y, unitless=1, digits=4).length_mm)
+            self.text_w.SetValue(Length(w, unitless=1, digits=4).length_mm)
+            self.text_h.SetValue(Length(h, unitless=1, digits=4).length_mm)
             self.text_x.Enable(en_xy)
             self.text_y.Enable(en_xy)
             self.text_w.Enable(en_wh)
