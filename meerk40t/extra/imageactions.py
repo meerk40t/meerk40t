13cbf517ac8fcb10d2a88ec5bf2bcf1aee15d22b
jpirnay <jens@pirnay.com>
2022-09-02 00:40:18 +0200

    Imageactions

---
+++ meerk40t/extra/imageactions.py
@@ -0,0 +1,275 @@
+from copy import copy
+from math import isinf
+
+import PIL
+
+from meerk40t.core.node.elem_image import ImageNode
+from meerk40t.core.node.elem_rect import RectNode
+from meerk40t.core.node.node import Node
+from meerk40t.core.units import UNITS_PER_INCH, Length
+from meerk40t.svgelements import Color, Matrix, Rect
+
+
+def plugin(kernel, lifecycle):
+    if lifecycle == "register":
+        _ = kernel.translation
+        context = kernel.root
+
+        @kernel.console_argument("cols", type=int, help=_("Number of columns"))
+        @kernel.console_argument("rows", type=int, help=_("Number of rows"))
+        @kernel.console_argument("dpi", type=int, help=_("Resolution of created image"))
+        @kernel.console_option(
+            "order", "o", help=_("ordering selection: none, first, last"), type=str
+        )
+        @kernel.console_command(
+            "render_split",
+            help=_("render_split <columns> <rows> <dpi>")
+            + "\n"
+            + _("Render selected elements and split the image into multiple parts"),
+            input_type=(None, "elements"),
+            output_type="elements",
+        )
+        def render_split(
+            command,
+            channel,
+            _,
+            cols=None,
+            rows=None,
+            dpi=None,
+            order=None,
+            origin=None,
+            data=None,
+            **kwargs,
+        ):
+            def prepare_data(data, dsort):
+                if dsort == "first":
+                    data.sort(key=lambda n: n.emphasized_time)
+                elif dsort == "last":
+                    data.sort(reverse=True, key=lambda n: n.emphasized_time)
+                bounds = Node.union_bounds(data, attr="paint_bounds")
+                return bounds
+
+            def create_image(data, data_bounds, dpi):
+                make_raster = elements.lookup("render-op/make_raster")
+                if not make_raster:
+                    return None, None
+
+                if data_bounds is None:
+                    return None, None
+                xmin, ymin, xmax, ymax = data_bounds
+                if isinf(xmin):
+                    # No bounds for selected elements."))
+                    return None
+                width = xmax - xmin
+                height = ymax - ymin
+
+                dots_per_units = dpi / UNITS_PER_INCH
+                new_width = width * dots_per_units
+                new_height = height * dots_per_units
+                new_height = max(new_height, 1)
+                new_width = max(new_width, 1)
+
+                image = make_raster(
+                    data,
+                    bounds=data_bounds,
+                    width=new_width,
+                    height=new_height,
+                )
+
+                matrix = Matrix.scale(width / new_width, height / new_height)
+                return image, matrix
+
+            def split_image(image, matrix, bounds, dpi, cols, rows):
+                data_out = []
+                groupit = False
+                if cols != 1 or rows != 1:
+                    groupit = True
+                    group_node = elements.elem_branch.add(
+                        type="group", label="Splitted Images"
+                    )
+                    data_out.append(group_node)
+
+                imgwidth, imgheight = image.size
+                deltax_image = imgwidth // cols
+                deltay_image = imgheight // rows
+
+                starty = 0
+                offset_y = bounds[1]
+                deltax_bound = (bounds[2] - bounds[0]) / cols
+                deltay_bound = (bounds[3] - bounds[1]) / rows
+                for yidx in range(rows):
+                    startx = 0
+                    offset_x = bounds[0]
+                    endy = starty + deltay_image - 1
+                    if yidx == rows - 1:
+                        # Just to make sure we get the residual pixels
+                        endy = imgheight - 1
+                    for xidx in range(cols):
+                        endx = startx + deltax_image - 1
+                        if xidx == cols - 1:
+                            # Just to make sure we get the residual pixels
+                            endx = imgwidth - 1
+                        # print(
+                        #     f"Image={imgwidth}x{imgheight}, Segment={xidx}:{yidx}, Box={startx},{starty}-{endx},{endy}"
+                        # )
+                        tile = image.crop((startx, starty, endx, endy))
+                        tilematrix = copy(matrix)
+                        tilematrix.post_translate(offset_x, offset_y)
+                        image_node = ImageNode(image=tile, matrix=tilematrix, dpi=dpi)
+                        elements.elem_branch.add_node(image_node)
+                        if groupit:
+                            group_node.append_child(image_node)
+                        data_out.append(image_node)
+
+                        startx = endx + 1
+                        offset_x += deltax_bound
+                    starty = endy + 1
+                    offset_y += deltay_bound
+
+            elements = context.elements
+            if data is None:
+                data = list(elements.elems(emphasized=True))
+            if cols is None:
+                cols = 1
+            if rows is None:
+                rows = cols
+            if order is None:
+                order = ""
+            if dpi is None or dpi <= 0:
+                dpi = 500
+            bb = prepare_data(data, order)
+            image, matrix = create_image(data, bb, dpi)
+            if image is None:
+                data_out = None
+            else:
+                data_out = split_image(image, matrix, bb, dpi, cols, rows)
+            return "elements", data_out
+
+        @kernel.console_argument("dpi", type=int, help=_("Resolution of created image"))
+        @kernel.console_option(
+            "order", "o", help=_("ordering selection: none, first, last"), type=str
+        )
+        @kernel.console_option(
+            "invert", "i", help=_("invert masking of image"), type=int
+        )
+        @kernel.console_command(
+            "render_keyhole",
+            help=_("render_keyhole <columns> <rows> <dpi>")
+            + "\n"
+            + _("Render selected elements and split the image into multiple parts"),
+            input_type=(None, "elements"),
+            output_type="elements",
+        )
+        def render_keyhole(
+            command,
+            channel,
+            _,
+            dpi=None,
+            order=None,
+            invert=None,
+            origin=None,
+            data=None,
+            **kwargs,
+        ):
+            def prepare_data(data, dsort):
+                if dsort == "first":
+                    data.sort(key=lambda n: n.emphasized_time)
+                elif dsort == "last":
+                    data.sort(reverse=True, key=lambda n: n.emphasized_time)
+                bounds = Node.union_bounds(data, attr="paint_bounds")
+                return bounds
+
+            def create_image(data, data_bounds, dpi):
+                make_raster = elements.lookup("render-op/make_raster")
+                if not make_raster:
+                    return None, None
+
+                if data_bounds is None:
+                    return None, None
+                xmin, ymin, xmax, ymax = data_bounds
+                if isinf(xmin):
+                    # No bounds for selected elements."))
+                    return None
+                width = xmax - xmin
+                height = ymax - ymin
+
+                dots_per_units = dpi / UNITS_PER_INCH
+                new_width = width * dots_per_units
+                new_height = height * dots_per_units
+                new_height = max(new_height, 1)
+                new_width = max(new_width, 1)
+
+                image = make_raster(
+                    data,
+                    bounds=data_bounds,
+                    width=new_width,
+                    height=new_height,
+                )
+
+                matrix = Matrix.scale(width / new_width, height / new_height)
+                return image, matrix
+
+            def split_image(image, matrix, bounds, dpi, cols, rows):
+                data_out = []
+                groupit = False
+                if cols != 1 or rows != 1:
+                    groupit = True
+                    group_node = elements.elem_branch.add(
+                        type="group", label="Splitted Images"
+                    )
+                    data_out.append(group_node)
+
+                imgwidth, imgheight = image.size
+                deltax_image = imgwidth // cols
+                deltay_image = imgheight // rows
+
+                starty = 0
+                offset_y = bounds[1]
+                deltax_bound = (bounds[2] - bounds[0]) / cols
+                deltay_bound = (bounds[3] - bounds[1]) / rows
+                for yidx in range(rows):
+                    startx = 0
+                    offset_x = bounds[0]
+                    endy = starty + deltay_image - 1
+                    if yidx == rows - 1:
+                        # Just to make sure we get the residual pixels
+                        endy = imgheight - 1
+                    for xidx in range(cols):
+                        endx = startx + deltax_image - 1
+                        if xidx == cols - 1:
+                            # Just to make sure we get the residual pixels
+                            endx = imgwidth - 1
+                        # print(
+                        #     f"Image={imgwidth}x{imgheight}, Segment={xidx}:{yidx}, Box={startx},{starty}-{endx},{endy}"
+                        # )
+                        tile = image.crop((startx, starty, endx, endy))
+                        tilematrix = copy(matrix)
+                        tilematrix.post_translate(offset_x, offset_y)
+                        image_node = ImageNode(image=tile, matrix=tilematrix, dpi=dpi)
+                        elements.elem_branch.add_node(image_node)
+                        if groupit:
+                            group_node.append_child(image_node)
+                        data_out.append(image_node)
+
+                        startx = endx + 1
+                        offset_x += deltax_bound
+                    starty = endy + 1
+                    offset_y += deltay_bound
+
+            elements = context.elements
+            if data is None:
+                data = list(elements.elems(emphasized=True))
+            if order is None:
+                order = ""
+            if dpi is None or dpi <= 0:
+                dpi = 500
+            if invert is None or invert == 0:
+                invert = False
+            invert = bool(invert)
+            bb = prepare_data(data, order)
+            image, matrix = create_image(data, bb, dpi)
+            if image is None:
+                data_out = None
+            else:
+                data_out = split_image(image, matrix, bb, dpi, cols, rows)
+            return "elements", data_out
---
+++ meerk40t/gui/imagesplitter.py
@@ -0,0 +1,409 @@
+import wx
+
+from meerk40t.gui.icons import STD_ICON_SIZE, icons8_split_table_50
+from meerk40t.gui.mwindow import MWindow
+from meerk40t.gui.wxutils import TextCtrl
+from meerk40t.kernel import signal_listener
+
+_ = wx.GetTranslation
+
+
+class SplitterPanel(wx.Panel):
+    def __init__(self, *args, context=None, scene=None, **kwds):
+        kwds["style"] = kwds.get("style", 0)
+        wx.Panel.__init__(self, *args, **kwds)
+        self.context = context
+        self.scene = scene
+        # Amount of currently selected
+        self.count = 0
+        self.first_node = None
+        self.last_node = None
+
+        sizer_main = wx.BoxSizer(wx.VERTICAL)
+        self.selchoices = (
+            _("Selection"),
+            _("First Selected"),
+            _("Last Selected"),
+        )
+        self.selectparam = ("default", "first", "last")
+
+        self.split_x = wx.SpinCtrl(self, wx.ID_ANY, initial=1, min=1, max=25)
+        self.split_y = wx.SpinCtrl(self, wx.ID_ANY, initial=1, min=1, max=25)
+
+        self.rbox_selection = wx.RadioBox(
+            self,
+            wx.ID_ANY,
+            _("Order to process:"),
+            choices=self.selchoices,
+            majorDimension=3,
+            style=wx.RA_SPECIFY_COLS,
+        )
+        self.rbox_selection.SetSelection(0)
+        self.text_dpi = TextCtrl(self, wx.ID_ANY, limited=True, check="int")
+        self.text_dpi.SetValue("500")
+        self.lbl_info = wx.StaticText(self, wx.ID_ANY, "")
+        self.btn_align = wx.Button(self, wx.ID_ANY, _("Create split images"))
+        self.btn_align.SetBitmap(icons8_split_table_50.GetBitmap(resize=25))
+
+        lbl_dpi = wx.StaticText(self, wx.ID_ANY, "DPI:")
+        sizer_dpi = wx.StaticBoxSizer(
+            wx.StaticBox(self, wx.ID_ANY, _("Image resolution:")), wx.HORIZONTAL
+        )
+        sizer_dpi.Add(lbl_dpi, 0, wx.ALIGN_CENTER_VERTICAL, 0)
+        sizer_dpi.Add(self.text_dpi, 1, wx.EXPAND, 0)
+
+        sizer_dimensions = wx.BoxSizer(wx.HORIZONTAL)
+        sizer_dim_x = wx.StaticBoxSizer(
+            wx.StaticBox(self, wx.ID_ANY, _("X-Axis:")),
+            wx.VERTICAL,
+        )
+        sizer_dim_x.Add(self.split_x, 0, wx.EXPAND, 0)
+
+        sizer_dim_y = wx.StaticBoxSizer(
+            wx.StaticBox(self, wx.ID_ANY, _("Y-Axis:")),
+            wx.VERTICAL,
+        )
+        sizer_dim_y.Add(self.split_y, 0, wx.EXPAND, 0)
+
+        sizer_dimensions.Add(sizer_dim_x, 1, wx.EXPAND, 0)
+        sizer_dimensions.Add(sizer_dim_y, 1, wx.EXPAND, 0)
+
+        sizer_main.Add(sizer_dimensions, 0, wx.EXPAND, 0)
+
+        sizer_main.Add(self.rbox_selection, 0, wx.EXPAND, 0)
+        sizer_main.Add(sizer_dpi, 0, wx.EXPAND, 0)
+        sizer_main.Add(self.btn_align, 0, wx.EXPAND, 0)
+        sizer_main.Add(self.lbl_info, 1, wx.EXPAND, 0)
+
+        self.SetSizer(sizer_main)
+        sizer_main.Fit(self)
+
+        self.Layout()
+
+        self.Bind(wx.EVT_BUTTON, self.on_button_split, self.btn_align)
+        self.Bind(wx.EVT_RADIOBOX, self.validate_data, self.rbox_selection)
+        self.Bind(wx.EVT_SPINCTRL, self.validate_data, self.split_x)
+        self.Bind(wx.EVT_SPINCTRL, self.validate_data, self.split_y)
+        self.Bind(wx.EVT_TEXT, self.validate_data, self.text_dpi)
+        has_emph = self.context.elements.has_emphasis()
+        self.context.setting(int, "split_x", 0)
+        self.context.setting(int, "split_y", 0)
+        self.context.setting(int, "split_selection", 0)
+        self.context.setting(str, "split_dpi", "500")
+        self.restore_setting()
+        self.show_stuff(has_emph)
+
+    def validate_data(self, event=None):
+        if event is not None:
+            event.Skip()
+        if self.context.elements.has_emphasis():
+            active = True
+            num_cols = self.split_x.GetValue()
+            num_rows = self.split_y.GetValue()
+            idx = self.rbox_selection.GetSelection()
+            if idx < 0:
+                idx = 0
+            esort = self.selectparam[idx]
+            try:
+                dpi = int(self.text_dpi.GetValue())
+            except ValueError:
+                dpi = 0
+            if dpi <= 0:
+                active = False
+        else:
+            active = False
+        # active = True
+        self.btn_align.Enable(active)
+
+    def on_button_split(self, event):
+        num_cols = self.split_x.GetValue()
+        num_rows = self.split_y.GetValue()
+        idx = self.rbox_selection.GetSelection()
+        if idx < 0:
+            idx = 0
+        esort = self.selectparam[idx]
+        try:
+            mydpi = int(self.text_dpi.GetValue())
+        except ValueError:
+            mydpi = 500
+        cmdstr = f"render_split {num_cols} {num_rows} {mydpi} --order {esort}\n"
+        self.context(cmdstr)
+        self.save_setting()
+
+    def save_setting(self):
+        self.context.split_selection = self.rbox_selection.GetSelection()
+        self.context.split_x = self.split_x.GetValue()
+        self.context.split_y = self.split_y.GetValue()
+        self.context.split_dpi = self.text_dpi.GetValue()
+
+    def restore_setting(self):
+        try:
+            self.rbox_selection.SetSelection(self.context.split_selection)
+            self.split_x.SetValue(self.context.split_x)
+            self.split_y.SetValue(self.context.split_y)
+            self.text_dpi.SetValue(self.context.split_dpi)
+        except (ValueError, AttributeError, RuntimeError):
+            pass
+
+    def show_stuff(self, has_emph):
+        self.count = 0
+        msg = ""
+        if has_emph:
+            data = list(self.context.elements.flat(emphasized=True))
+            self.count = len(data)
+            msg = _("Selected elements: {count}").format(count=self.count) + "\n"
+            if self.count > 0:
+                data.sort(key=lambda n: n.emphasized_time)
+                node = data[0]
+                msg += (
+                    _("First selected: {type} {lbl}").format(
+                        type=node.type, lbl=node.label
+                    )
+                    + "\n"
+                )
+                node = data[-1]
+                msg += (
+                    _("Last selected: {type} {lbl}").format(
+                        type=node.type, lbl=node.label
+                    )
+                    + "\n"
+                )
+        self.lbl_info.SetLabel(msg)
+        self.rbox_selection.Enable(has_emph)
+        self.split_x.Enable(has_emph)
+        self.split_y.Enable(has_emph)
+        self.text_dpi.Enable(has_emph)
+        self.validate_data()
+
+
+class KeyholePanel(wx.Panel):
+    def __init__(self, *args, context=None, scene=None, **kwds):
+        kwds["style"] = kwds.get("style", 0)
+        wx.Panel.__init__(self, *args, **kwds)
+        self.context = context
+        self.scene = scene
+        # Amount of currently selected
+        self.count = 0
+        self.first_node = None
+        self.last_node = None
+
+        sizer_main = wx.BoxSizer(wx.VERTICAL)
+        self.selchoices = (
+            _("First Selected"),
+            _("Last Selected"),
+        )
+        self.selectparam = ("first", "last")
+
+        self.rbox_selection = wx.RadioBox(
+            self,
+            wx.ID_ANY,
+            _("Keyhole Object:"),
+            choices=self.selchoices,
+            majorDimension=2,
+            style=wx.RA_SPECIFY_COLS,
+        )
+        self.rbox_selection.SetSelection(0)
+        self.text_dpi = TextCtrl(self, wx.ID_ANY, limited=True, check="int")
+        self.text_dpi.SetValue("500")
+        self.lbl_info = wx.StaticText(self, wx.ID_ANY, "")
+        self.btn_align = wx.Button(self, wx.ID_ANY, _("Create keyhole image"))
+        self.btn_align.SetBitmap(icons8_split_table_50.GetBitmap(resize=25))
+
+        lbl_dpi = wx.StaticText(self, wx.ID_ANY, "DPI:")
+        sizer_dpi = wx.StaticBoxSizer(
+            wx.StaticBox(self, wx.ID_ANY, _("Image resolution:")), wx.HORIZONTAL
+        )
+        sizer_dpi.Add(lbl_dpi, 0, wx.ALIGN_CENTER_VERTICAL, 0)
+        sizer_dpi.Add(self.text_dpi, 1, wx.EXPAND, 0)
+
+        sizer_options = wx.BoxSizer(wx.HORIZONTAL)
+        sizer_check = wx.StaticBoxSizer(
+            wx.StaticBox(self, wx.ID_ANY, _("Invert:")),
+            wx.HORIZONTAL,
+        )
+        self.check_invert = wx.CheckBox(self, wx.ID_ANY, "Invert mask from keyhole")
+        sizer_check.Add(self.check_invert, 0, wx.ALIGN_CENTER_VERTICAL, 0)
+        sizer_options.Add(sizer_dpi, 1, wx.EXPAND, 0)
+        sizer_options.Add(sizer_check, 1, wx.EXPAND, 0)
+
+        sizer_main.Add(self.rbox_selection, 0, wx.EXPAND, 0)
+        sizer_main.Add(sizer_options, 0, wx.EXPAND, 0)
+        sizer_main.Add(self.btn_align, 0, wx.EXPAND, 0)
+        sizer_main.Add(self.lbl_info, 1, wx.EXPAND, 0)
+
+        self.SetSizer(sizer_main)
+        sizer_main.Fit(self)
+
+        self.Layout()
+
+        self.Bind(wx.EVT_BUTTON, self.on_button_keyhole, self.btn_align)
+        self.Bind(wx.EVT_RADIOBOX, self.validate_data, self.rbox_selection)
+        self.Bind(wx.EVT_CHECKBOX, self.validate_data, self.check_invert)
+        self.Bind(wx.EVT_TEXT, self.validate_data, self.text_dpi)
+        has_emph = self.context.elements.has_emphasis()
+        self.context.setting(int, "keyhole_selection", 0)
+        self.context.setting(str, "keyhole_dpi", "500")
+        self.context.setting(bool, "keyhole_invert", False)
+        self.restore_setting()
+        self.show_stuff(has_emph)
+
+    def validate_data(self, event=None):
+        if event is not None:
+            event.Skip()
+        if self.context.elements.has_emphasis():
+            active = True
+            invert = self.check_invert.GetValue()
+            idx = self.rbox_selection.GetSelection()
+            if idx < 0:
+                idx = 0
+            esort = self.selectparam[idx]
+            try:
+                dpi = int(self.text_dpi.GetValue())
+            except ValueError:
+                dpi = 0
+            if dpi <= 0:
+                active = False
+            # if self.count < 2:
+            #     active = False
+        else:
+            active = False
+        # active = True
+        self.btn_align.Enable(active)
+
+    def on_button_keyhole(self, event):
+        idx = self.rbox_selection.GetSelection()
+        if idx < 0:
+            idx = 0
+        esort = self.selectparam[idx]
+        try:
+            mydpi = int(self.text_dpi.GetValue())
+        except ValueError:
+            mydpi = 500
+        if self.check_invert.GetValue():
+            invert = "--invert 1"
+        else:
+            invert = ""
+        cmdstr = f"render_keyhole {mydpi} --order {esort} {invert}\n"
+        self.context(cmdstr)
+        self.save_setting()
+
+    def save_setting(self):
+        self.context.keyhole_selection = self.rbox_selection.GetSelection()
+        self.context.keyhole_invert = self.check_invert.GetValue()
+        self.context.keyhole_dpi = self.text_dpi.GetValue()
+
+    def restore_setting(self):
+        try:
+            self.rbox_selection.SetSelection(self.context.keyhole_selection)
+            self.check_invert.SetValue(bool(self.context.keyhole_invert))
+            self.text_dpi.SetValue(self.context.keyhole_dpi)
+        except (ValueError, AttributeError, RuntimeError):
+            pass
+
+    def show_stuff(self, has_emph):
+        self.count = 0
+        msg = ""
+        if has_emph:
+            data = list(self.context.elements.flat(emphasized=True))
+            self.count = len(data)
+            msg = _("Selected elements: {count}").format(count=self.count) + "\n"
+            if self.count > 0:
+                data.sort(key=lambda n: n.emphasized_time)
+                node = data[0]
+                msg += (
+                    _("First selected: {type} {lbl}").format(
+                        type=node.type, lbl=node.label
+                    )
+                    + "\n"
+                )
+                node = data[-1]
+                msg += (
+                    _("Last selected: {type} {lbl}").format(
+                        type=node.type, lbl=node.label
+                    )
+                    + "\n"
+                )
+        self.lbl_info.SetLabel(msg)
+        self.rbox_selection.Enable(has_emph)
+        self.check_invert.Enable(has_emph)
+        self.text_dpi.Enable(has_emph)
+        self.validate_data()
+
+
+class RenderSplit(MWindow):
+    def __init__(self, *args, **kwds):
+        super().__init__(
+            350,
+            350,
+            *args,
+            style=wx.CAPTION
+            | wx.CLOSE_BOX
+            | wx.FRAME_FLOAT_ON_PARENT
+            | wx.TAB_TRAVERSAL
+            | wx.RESIZE_BORDER,
+            **kwds,
+        )
+        self.notebook_main = wx.aui.AuiNotebook(
+            self,
+            -1,
+            style=wx.aui.AUI_NB_TAB_EXTERNAL_MOVE
+            | wx.aui.AUI_NB_SCROLL_BUTTONS
+            | wx.aui.AUI_NB_TAB_SPLIT
+            | wx.aui.AUI_NB_TAB_MOVE,
+        )
+        self.scene = getattr(self.context.root, "mainscene", None)
+        # Hide Arrangement until ready...
+        self.panel_split = SplitterPanel(
+            self, wx.ID_ANY, context=self.context, scene=self.scene
+        )
+        self.panel_keyhole = KeyholePanel(
+            self, wx.ID_ANY, context=self.context, scene=self.scene
+        )
+        self.notebook_main.AddPage(self.panel_split, _("Render + Split"))
+        self.notebook_main.AddPage(self.panel_keyhole, _("Keyhole operation"))
+
+        self.Layout()
+
+        _icon = wx.NullIcon
+        _icon.CopyFromBitmap(icons8_split_table_50.GetBitmap(resize=25))
+        self.SetIcon(_icon)
+        self.SetTitle(_("Create split images"))
+
+    def delegates(self):
+        yield self.panel_split
+        yield self.panel_keyhole
+
+    @signal_listener("reference")
+    @signal_listener("emphasized")
+    def on_emphasize_signal(self, origin, *args):
+        has_emph = self.context.elements.has_emphasis()
+        self.panel_split.show_stuff(has_emph)
+        self.panel_keyhole.show_stuff(has_emph)
+
+    @staticmethod
+    def sub_register(kernel):
+        buttonsize = int(STD_ICON_SIZE / 2)
+        kernel.register(
+            "button/align/SplitImage",
+            {
+                "label": _("Splitter"),
+                "icon": icons8_split_table_50,
+                "tip": _("Open create split image dialog"),
+                "action": lambda v: kernel.console("window toggle SplitImage\n"),
+                "size": buttonsize,
+                "rule_enabled": lambda cond: len(
+                    list(kernel.elements.elems(emphasized=True))
+                )
+                > 0,
+            },
+        )
+
+    def window_open(self):
+        pass
+
+    def window_close(self):
+        pass
+
+    @staticmethod
+    def submenu():
+        return ("Editing", "Image Splitting")
