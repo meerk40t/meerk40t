5f9c79ffe9c0e1386680d459dbd02a217803c087
Tatarize <tatarize@gmail.com>
2022-09-12 23:08:29 -0700

    change mode to report if column is added

--- meerk40t/device/gui/defaultactions.py
+++ meerk40t/device/gui/defaultactions.py
@@ -72,7 +72,7 @@ class DefaultActionPanel(wx.Panel):
         self.option_list = wx.ListCtrl(
             self,
             wx.ID_ANY,
-            style=wx.LC_LIST,
+            style=wx.LC_REPORT,
         )
         self.option_list.AppendColumn(_("Operation"), format=wx.LIST_FORMAT_LEFT)
 
