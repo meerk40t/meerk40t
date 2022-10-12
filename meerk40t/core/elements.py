65545048bfb32ccb3344df05031885a86ed88e05
jpirnay <jens@pirnay.com>
2022-10-12 19:59:40 +0200

    Clear empty groups on load

--- meerk40t/core/elements.py
+++ meerk40t/core/elements.py
@@ -10176,6 +10176,16 @@ class Elemental(Service):
                     return True
         return False
 
+    def remove_empty_groups(self):
+        something_was_deleted = True
+        while something_was_deleted:
+            something_was_deleted = False
+            for node in self.elems_nodes():
+                if node.type in ("file", "group"):
+                    if len(node.children) == 0:
+                        node.remove_node()
+                        something_was_deleted = True
+
     @staticmethod
     def element_classify_color(element: SVGElement):
         element_color = element.stroke
@@ -10192,6 +10202,7 @@ class Elemental(Service):
                     try:
                         self.signal("freeze_tree", True)
                         results = loader.load(self, self, pathname, **kwargs)
+                        self.remove_empty_groups()
                         self.signal("freeze_tree", False)
                     except FileNotFoundError:
                         return False
