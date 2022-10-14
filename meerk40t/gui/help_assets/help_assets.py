029ac2aab7e3ca56073615dff4800a78840131cd
jpirnay <jens@pirnay.com>
2022-10-14 20:34:06 +0200

    Ich schon:-) German text added

--- meerk40t/gui/help_assets/help_assets.py
+++ meerk40t/gui/help_assets/help_assets.py
@@ -1,5 +1,5 @@
 english_wordlist_howto = """
-WordLists allow you to create text elements in your design which contain placeholder text that is replaced at burn time from this WordList. You can then burn several items with different text without having to change your design each time. 
+WordLists allow you to create text elements in your design which contain placeholder text that is replaced at burn time from this WordList. You can then burn several items with different text without having to change your design each time.
 
 A placeholder consists of a name inside curly brackets e.g. '{FIRSTNAME}'. You use the name in the WordList Editor to associate it with the placeholder and the placeholder will be replaced by the text you enter into the associated WordList Contents.
 
@@ -22,11 +22,11 @@ You can use as many different placeholder names as you like in text fields in yo
 
 The 'Index' value in the WordList table indicates which entry in the Contents list will be used next, zero meaning the first entry. The index is automatically increased by one at the end of each burn.
 
-But suppose for efficiency you now want to burn two seat reservation tags at the same time each having a different name from the same list. In this case, if the first tag use '{NAME#+0}' and in the second '{NAME#+1}' (note the plus sign). '{NAME#+0}' uses the current entry (pointed to by the Index value), '{NAME#+1}' uses the next entry after the current one etc. 
+But suppose for efficiency you now want to burn two seat reservation tags at the same time each having a different name from the same list. In this case, if the first tag use '{NAME#+0}' and in the second '{NAME#+1}' (note the plus sign). '{NAME#+0}' uses the current entry (pointed to by the Index value), '{NAME#+1}' uses the next entry after the current one etc.
 
 With the above usage, you can use e.g. '{NAME#+0}' as many times as you wish in your design. However, if you are only using the placeholder once in your design, then an alternative is to use '{NAME++}' which advances the Index each time the placeholder is used.
 
-As an alternative to manually entering the wordlist values using this WordList Editor, you can use a standard comma-separated CSV file. The placeholder names are defined in standard CSV header line (the first line in the CSV file), and the contents are then taken from all the following lines. The easiest way to create a CSV file is using a spreadsheet e.g. Excel, however e.g. for ecommerce sites your website might automaticallycreate the CSV file from the orders placed online by customers. 
+As an alternative to manually entering the wordlist values using this WordList Editor, you can use a standard comma-separated CSV file. The placeholder names are defined in standard CSV header line (the first line in the CSV file), and the contents are then taken from all the following lines. The easiest way to create a CSV file is using a spreadsheet e.g. Excel, however e.g. for ecommerce sites your website might automaticallycreate the CSV file from the orders placed online by customers.
 
 Entries loaded from a CSV file are shown as Type CSV, and you can set the Index values for all CSV entries at the same time.
 
@@ -50,7 +50,57 @@ For a complete set of format-directives see: https://docs.python.org/3/library/d
 """
 
 german_wordlist_howto = """
-Ick Sprechen Kinen Duetch
+Wortlisten erlauben es, innerhalb von Text-Elementen Platzhalter zu verwenden, die beim Brennvorgang durch Inhalte der Wortliste ersetzt werden.
+So ist es möglich, bei jedem Brennvorgang andere Textinhalte zu verwenden, ohne dass dafür jedes mal das Design verändert werden müsste.
+
+Ein Platzhalter besteht aus einem Namen in geschweiften Klammern (etwa '{VORNAME}'). Dieser Name wird im Wortlisten-Editor zusammen mit zugeordneten Texten definiert, so dass er anschließend im Design durch die 'richtigen' Inhalte ersetzt werden kann.
+
+Kommen wir zu unserem Anwendungsfall: wir wollen eine Namensschild-Vorlage für eine Feier erstellen, die leicht wiederverwendbar ist. Erstellen wir also zunächst einen Rahmen (z.B. ein Rechteck) und platzieren mit dem Text-Tool ein Text-Element innerhalb dieses Rahmens. Den Text verändern wir zu:
+'Hier sitzt {VORNAME}'
+
+Mit dem Wortlist-Editor definieren wir nun diesen Platzhalter:
+	|-----------|------|-------|
+	|    Name   | Type | Index |
+	|-----------|------|-------|
+	| vorname   | Text |   0   |
+	|-----------|------|-------|
+Wir klicken auf diesen Eintrag in der linken Liste und fügen auf der rechten Seite mehrere passende Einträge hinzu:
+	Antje
+	David
+	Oma
+
+Wenn wir nun den Brennvorgang starten, erhalten wir individualisierte Platzkarten mit den einzelnen Namen aus der Wortliste, also etwa: 'Hier sitzt Antje'.
+
+Wir können beliebig viele soclher Platzhalter definieren und verwenden.
+
+Der 'Index' Wert in der Wortlist-Tabelle legt fest, welche der auf der rechten Seite angezeigten Einträge das nächste mal verwendet werden soll (wobei der Wert 0 den ersten Eintrag in der Liste meint).
+
+Wir sind nicht beschränkt auf eine einmalige Verwendung eines Platzhalters (nützlich beispielsweise wenn man nicht nur ein Schild sondern gleich mehrere in einem Rutsch brennen will). Die Standardverwendung {VORNAME} nimmt den Wert an der Position #index der geladenen Liste, {VORNAME#+1} (beachte das #+1 am Ende) verwendet den nächsten Eintrag, {VORNAME#+2} den übernächsten usw.
+
+Auf diese Weise kann z.B. der Wert {VORNAME#+0} beliebig häufig verwendet werden, der Index wird dadurch nicht weitergeschaltet. Der Einfachheit halber kann die Syntax {VORNAME++} verwendet werden, diese schaltet pro Vorkommen den Indexeintrag um eins weiter. Empfohlen wird aber die explizite Verwendung von #+1 #+2 etc., da dies wie beschrieben Mehrfachverwendungen ermöglicht.
+
+Wir können einen ganzen Satz von Variablen (Wortliste genannt) definieren, der z.B. aus einer Standard Komma-separierten CSV-Datei eingelesen werden kann. Dann hätten wir nicht nur wenige Einträge für 'VORNAME', sondern vielleicht Hunderte davon. Welcher der Mehrfach-Einträge gerade aktiv ist entscheidet der sogenannte Index.
+
+Der einfachste Weg solche Dateien zu erzeugen, ist das Verwenden einer Tabellenkalkulation wie Excel oder LibreCalc, sie kann aber auch anderen Quellen wie einer Webseite stammen, die solche Daten automatisch bei Kundenaufträgen anlegt.
+
+Einträge die aus einer CSV-Datei stammen, werden mit der Typ 'CSV' gekennzeichnet, und der Index aller CSV-Einträge wird zur gleichen Zeit festgelegt.
+
+Achtung: Hat die CSV-Datei keine Kopfzeile mit Spaltennamen, so werden die einzelnen Spalten 'column_1', 'column_2' etc. benannt werden.
+
+Es gibt eine Reihe von vordefinierten Variablen, die Infos zum aktuellen Arbeitsgang (etwa {op_power}, {op_speed} u.a. - nützlich z.B. bei Kalibrier-Tests) oder Datums-Zeit-Infomationen liefern ({date}, {time})."
+	* 'version'   - Meerk40t Version
+	* 'date'      - Datum für den Start des Brennvorgangs
+	* 'time'      - Zeit für den Start des Brennvorgangs
+	* 'op_device' - Name des Geräts auf dem gelasert wird
+	* 'op_speed'  - Geschwindigkeit des aktuellen Arbeitsvorgangs
+	* 'op_power'  - Leistung des aktuellen Arbeitsvorgangs
+	* 'op_dpi'    - DPI (Pixelauflösung) des aktuellen Arbeitsvorgangs
+	* 'op_passes' - Anzahl der Durchläufe des aktuellen Arbeitsvorgangs
+
+Die Platzhalter {date} und {time} können mit einem Format angegeben werden, so dass ihr Aussehen mit lokalen Standards übereinstimmt: z.B.
+    {date@%d.%m.%Y} - 31.12.2022
+    {time@%H:%M} - 23:59
+Für eine komplette Liste der Format-Codes: https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
 """
 
 
