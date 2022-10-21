1afd95050263890db6f4615f7b5ffc2e77238f28
jpirnay <jens@pirnay.com>
2022-10-21 17:31:44 +0200

    Amend after conflict resolve

--- meerk40t/gui/propertypanels/propertywindow.py
+++ meerk40t/gui/propertypanels/propertywindow.py
@@ -41,10 +41,7 @@ class PropertyWindow(MWindow):
             pass
 
     @signal_listener("selected")
-    def properties_show(self, nodes):
-        if nodes is None:
-            return
-
+    def on_selected(self, origin, *args):
         self.Freeze()
         for p in self.panel_instances:
             try:
@@ -61,6 +58,9 @@ class PropertyWindow(MWindow):
                 else 0
             )
 
+        nodes = list(self.context.elements.flat(selected=True, cascade=False))
+        if nodes is None:
+            return
         pages_to_instance = []
         for node in nodes:
             pages_in_node = []
@@ -124,17 +124,6 @@ class PropertyWindow(MWindow):
         self.Layout()
         self.Thaw()
         # self.Refresh()
-
-    @signal_listener("propupdate")
-    def on_propupdate(self, origin, node, *args):
-        if node is None:
-            return
-        self.properties_show([node])
-
-    @signal_listener("selected")
-    def on_selected(self, origin, *args):
-        nodes = list(self.context.elements.flat(selected=True, cascade=False))
-        self.properties_show(nodes)
 
     @staticmethod
     def sub_register(kernel):
