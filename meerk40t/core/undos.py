ebf660ae9b371713c82bff3f7c86d8152c0403fc
Tatarize <tatarize@gmail.com>
2022-11-05 04:44:29 -0700

    Add UndoState with message, print message

--- meerk40t/core/undos.py
+++ meerk40t/core/undos.py
@@ -7,17 +7,37 @@ execute undo and redo operations for the tree.
 """
 
 
+class UndoState:
+    def __init__(self, elements, message=None):
+        self.elements = elements
+        self.message = message
+        if self.message is None:
+            self.message = str(id(elements))
+
+    def __str__(self):
+        return self.message
+
+
 class Undo:
     def __init__(self, tree):
         self.tree = tree
         self._undo_stack = []
         self._undo_index = -1
-        self.mark()  # Set initial tree state.
+        self.mark("init")  # Set initial tree state.
+        self.message = None
 
-    def mark(self):
+    def __str__(self):
+        return f"Undo({self._undo_index} of {len(self._undo_stack)})"
+
+    def mark(self, message=None):
         self._undo_index += 1
-        self._undo_stack.insert(self._undo_index, self.tree.backup_tree())
-        del self._undo_stack[self._undo_index + 1:]
+        if message is None:
+            message = self.message
+        self._undo_stack.insert(
+            self._undo_index, UndoState(self.tree.backup_tree(), message=message)
+        )
+        del self._undo_stack[self._undo_index + 1 :]
+        self.message = None
 
     def undo(self):
         if self._undo_index == 0:
@@ -39,4 +59,4 @@ class Undo:
     def undolist(self):
         for i, v in enumerate(self._undo_stack):
             q = "*" if i == self._undo_index else " "
-            yield f"{q}{str(i).ljust(5)}: undo {id(v)}:{str(v)} elements "
+            yield f"{q}{str(i).ljust(5)}: state {str(v)}"
