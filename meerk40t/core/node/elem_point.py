a569b40fd1909cd59d683069b97d7448b9d7f656
Tatarize <tatarize@gmail.com>
2022-11-15 09:14:50 -0800

    update elem point node

--- meerk40t/core/node/elem_point.py
+++ meerk40t/core/node/elem_point.py
@@ -9,54 +9,31 @@ class PointNode(Node):
     PointNode is the bootstrapped node type for the 'elem point' type.
     """
 
-    def __init__(
-        self,
-        point=None,
-        matrix=None,
-        fill=None,
-        stroke=None,
-        stroke_width=None,
-        id=None,
-        label=None,
-        lock=False,
-        settings=None,
-        **kwargs,
-    ):
-        if settings is None:
-            settings = dict()
-        settings.update(kwargs)
-        if "type" in settings:
-            del settings["type"]
-        super(PointNode, self).__init__(
-            type="elem point", id=id, label=label, lock=lock, **settings
-        )
+    def __init__(self, **kwargs):
+        self.point = None
+        self.x = 0
+        self.y = 0
+        self.matrix = None
+        self.fill = None
+        self.stroke = None
+        self.stroke_width = None
+        super(PointNode, self).__init__(type="elem point", **kwargs)
         self._formatter = "{element_type} {id} {stroke}"
-        self.point = point
-        self.matrix = matrix
-        self.settings = settings
-        self.fill = fill
-        self.stroke = stroke
-        self.stroke_width = stroke_width
 
     def __copy__(self):
-        return PointNode(
-            point=copy(self.point),
-            matrix=copy(self.matrix),
-            fill=copy(self.fill),
-            stroke=copy(self.stroke),
-            stroke_width=self.stroke_width,
-            id=self.id,
-            label=self.label,
-            lock=self.lock,
-            settings=self.settings,
-        )
+        nd = self.node_dict
+        nd['point'] = copy(self.point)
+        nd['matrix'] = copy(self.matrix)
+        nd['fill'] = copy(self.fill)
+        nd['stroke_width'] = copy(self.stroke_width)
+        return PointNode(**nd)
 
     def validate(self):
         if self.matrix is None:
             self.matrix = Matrix()
         if self.point is None:
-            x = float(self.settings.get("x", 0))
-            y = float(self.settings.get("y", 0))
+            x = float(self.x)
+            y = float(self.y)
             self.matrix.pre_translate(x, y)
             self.point = Point(0, 0)
 
@@ -79,11 +56,7 @@ class PointNode(Node):
         else:
             default_map["x"] = 0
             default_map["y"] = 0
-        default_map.update(self.settings)
-        default_map["stroke"] = self.stroke
-        default_map["fill"] = self.fill
-        default_map["stroke-width"] = self.stroke_width
-        default_map["matrix"] = self.matrix
+        default_map.update(self.__dict__)
         return default_map
 
     def drop(self, drag_node, modify=True):
