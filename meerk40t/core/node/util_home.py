65e0476bea06af0a7f332597aa4e46014931a37d
Tatarize <tatarize@gmail.com>
2022-11-15 06:50:29 -0800

    Simply home node

--- meerk40t/core/node/util_home.py
+++ meerk40t/core/node/util_home.py
@@ -10,43 +10,19 @@ class HomeOperation(Node):
     Node type "util home"
     """
 
-    def __init__(self, id=None, label=None, lock=False, **kwargs):
-        super().__init__(type="util home", id=id, label=label, lock=lock, **kwargs)
-        self.settings = {"output": True}
+    def __init__(self, **kwargs):
+        self.output = True
+        super().__init__(type="util home", **kwargs)
         self._formatter = "{enabled}{element_type}"
 
     def __repr__(self):
         return "HomeOperation()"
 
     def __copy__(self):
-        return HomeOperation(
-            id=self.id, label=self.label, lock=self.lock, **self.settings
-        )
+        return HomeOperation(**self.node_dict)
 
     def __len__(self):
         return 1
-
-    def validate(self):
-        parameters = [
-            ("output", lambda v: str(v).lower() == "true"),
-        ]
-        settings = self.settings
-        for param, cast in parameters:
-            try:
-                if param in settings and settings[param] is not None:
-                    settings[param] = (
-                        cast(settings[param]) if settings[param] != "None" else None
-                    )
-            except (KeyError, ValueError):
-                pass
-
-    @property
-    def output(self):
-        return self.settings.get("output", True)
-
-    @output.setter
-    def output(self, v):
-        self.settings["output"] = v
 
     @property
     def implicit_passes(self):
@@ -56,7 +32,7 @@ class HomeOperation(Node):
         default_map = super(HomeOperation, self).default_map(default_map=default_map)
         default_map["element_type"] = "Home"
         default_map["enabled"] = "(Disabled) " if not self.output else ""
-        default_map.update(self.settings)
+        default_map.update(self.__dict__)
         return default_map
 
     def drop(self, drag_node, modify=True):
@@ -71,14 +47,6 @@ class HomeOperation(Node):
                 drop_node.append_child(drag_node)
             return True
         return False
-
-    def load(self, settings, section):
-        update_dict = settings.read_persistent_string_dict(section, suffix=True)
-        self.settings.update(update_dict)
-        self.validate()
-
-    def save(self, settings, section):
-        settings.write_persistent_dict(section, self.settings)
 
     def as_cutobjects(self, closed_distance=15, passes=1):
         """
