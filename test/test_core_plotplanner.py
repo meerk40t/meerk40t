315815ce9edf9a0100c7583a29452ea3123ba6b0
Tatarize <tatarize@gmail.com>
2022-11-17 01:47:10 -0800

    Additional import changes

--- test/test_core_plotplanner.py
+++ test/test_core_plotplanner.py
@@ -3,7 +3,8 @@ import unittest
 
 from PIL import Image, ImageDraw
 
-from meerk40t.core.cutcode import CutCode, LineCut
+from meerk40t.core.cutcode.cutcode import CutCode
+from meerk40t.core.cutcode.linecut import LineCut
 from meerk40t.core.node.elem_image import ImageNode
 from meerk40t.core.node.elem_path import PathNode
 from meerk40t.core.node.op_engrave import EngraveOpNode
