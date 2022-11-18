d38fd8e419a89c6a951b321e20ddc63f8ad20b4b
Tatarize <tatarize@gmail.com>
2022-11-18 04:56:45 -0800

    Process classify new as a post-execute command

--- meerk40t/core/elements.py
+++ meerk40t/core/elements.py
@@ -1699,20 +1699,6 @@ class Elemental(Service):
             self._emphasized_bounds_painted = None
             self.set_emphasis(None)
 
-    @signal_listener("classify_new")
-    def ask_for_classification(self, origin, data, *args):
-        # Why are we doing it here? An immediate classification
-        # at the end of the element creation might not provide
-        # the right assignment as additional commands might be
-        # chained to it:
-        # e.g. "circle 1cm 1cm 1cm" will classify differently than
-        # "circle 1cm 1cm 1cm stroke red"
-        if not isinstance(data, (list, tuple)):
-            data = [data]
-        if self.classify_new and len(data) > 0:
-            self.classify(data)
-            self.signal("tree_changed")
-
     def classify(self, elements, operations=None, add_op_function=None):
         """
         @param elements: list of elements to classify.
