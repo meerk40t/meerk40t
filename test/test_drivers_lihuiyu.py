ad3f9abfb55e356f9959b4509f2b8e747a9b326a
Tatarize <tatarize@gmail.com>
2022-12-17 06:52:53 -0800

    Add in imageop test

--- test/test_drivers_lihuiyu.py
+++ test/test_drivers_lihuiyu.py
@@ -1,6 +1,12 @@
 import os
 import unittest
 
+from PIL import ImageDraw
+from PIL import Image
+
+from meerk40t.core.node.elem_image import ImageNode
+from meerk40t.core.units import UNITS_PER_MM
+from meerk40t.svgelements import Matrix
 from test import bootstrap
 
 
@@ -12,6 +18,16 @@ Creator-Software: MeerK40t v0.0.0-testing
 %0%0%0%0%
 IBzzzvRzzzvS1P
 ICV2490731016000027CNLBS1EDz139Rz139Tz139Lz139FNSE-
+"""
+
+egv_image = """Document type : LHYMICRO-GL file
+File version: 1.0.01
+Copyright: Unknown
+Creator-Software: MeerK40t v0.0.0-testing
+
+%0%0%0%0%
+IB067R091S1P
+IV1552121G002NLTS1EDnU|sB|oD|aU|qT|oD|iU|oB|oD|mU|qT|oD|sU|oB|oD|uU|oT|oD|yU|oB|oD054U|mT|oD054U|oB|oD058U|mT|oD058U|mB|oD058U|oT|oD062U|mB|oD062U|mT|oD062U|mB|oD062U|mT|oD062U|mB|oD062U|mT|oD062U|mB|qD058U|mT|oD058U|mB|oD058U|mT|qD054U|mB|oD054U|mT|qD|yU|mB|qD|uU|mT|qD|qU|mB|qD|mU|mT|qD|iU|mB|sD|aU|mT|uDlFNSE-
 """
 
 egv_blank = """Document type : LHYMICRO-GL file
@@ -81,3 +97,34 @@ class TestDriverLihuiyu(unittest.TestCase):
         with open(file1, "r") as f:
             data = f.read()
         self.assertEqual(data, egv_blank)
+
+    def test_driver_basic_ellipse_image(self):
+        """
+        Attempts a raster operation however wxPython isn't available so nothing is produced.
+
+        @return:
+        """
+        file1 = "testi.egv"
+        self.addCleanup(os.remove, file1)
+
+        image = Image.new("RGBA", (256, 256), "white")
+        matrix = Matrix.scale(UNITS_PER_MM / 64)
+        matrix.translate(UNITS_PER_MM * 2, UNITS_PER_MM * 2)
+
+        draw = ImageDraw.Draw(image)
+        draw.ellipse((50, 50, 150, 150), "black")
+
+        kernel = bootstrap.bootstrap()
+        try:
+            image_node = ImageNode(image=image, matrix=matrix)
+            kernel.elements.elem_branch.add_node(image_node)
+            kernel.console("element* list\n")
+            kernel.console("service device start -i lhystudios 0\n")
+            kernel.console("operation* remove\n")
+            kernel.console(
+                f"element0 imageop -s 15 plan copy-selected preprocess validate blob preopt optimize save_job {file1}\n")
+        finally:
+            kernel.shutdown()
+        with open(file1, "r") as f:
+            data = f.read()
+        self.assertEqual(data, egv_image)
