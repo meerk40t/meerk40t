ad2565419f20b9a8000771da5e01ae581b376d3a
Tatarize <tatarize@gmail.com>
2022-12-24 00:55:06 -0800

    Correct matrix movement value

--- meerk40t/gui/toolwidgets/toolnodeedit.py
+++ meerk40t/gui/toolwidgets/toolnodeedit.py
@@ -1,9 +1,11 @@
+import math
+
 import wx
 
 from meerk40t.gui.laserrender import swizzlecolor
 from meerk40t.gui.scene.sceneconst import (
     RESPONSE_CHAIN,
-    RESPONSE_CONSUME,
+    RESPONSE_CONSUME, RESPONSE_DROP,
 )
 from meerk40t.gui.toolwidgets.toolwidget import ToolWidget
 
@@ -30,138 +32,98 @@ class EditTool(ToolWidget):
 
     def calculate_points(self):
         self.nodes = []
-        offset = 5000
+        offset = 5
+        s = math.sqrt(abs(self.scene.widget_root.scene_widget.matrix.determinant))
+        offset /= s
         selected_node = self.scene.context.elements.first_element(emphasized=True)
         try:
             path = selected_node.path
         except AttributeError:
             return
+        matrix = path.transform
         for segment in path:
             q = type(segment).__name__
             if q in ("Line", "Close"):
-                # self.nodes.append(
-                #     (
-                #         segment.start.x - offset,
-                #         segment.start.y - offset,
-                #         offset * 2,
-                #         offset * 2,
-                #         segment,
-                #         segment.start,
-                #     )
-                # )
                 self.nodes.append(
                     (
-                        segment.end.x - offset,
-                        segment.end.y - offset,
-                        offset * 2,
-                        offset * 2,
-                        selected_node,
-                        segment,
                         segment.end,
+                        segment,
+                        path,
+                        selected_node,
                     )
                 )
             elif q == "Move":
                 self.nodes.append(
                     (
-                        segment.end.x - offset,
-                        segment.end.y - offset,
-                        offset * 2,
-                        offset * 2,
-                        selected_node,
-                        segment,
                         segment.end,
+                        segment,
+                        path,
+                        selected_node,
                     )
                 )
             elif q == "QuadraticBezier":
-                # self.nodes.append(
-                #     (
-                #         segment.start.x - offset,
-                #         segment.start.y - offset,
-                #         offset * 2,
-                #         offset * 2,
-                #         segment,
-                #         segment.start,
-                #     )
-                # )
                 self.nodes.append(
                     (
-                        segment.control.x - offset,
-                        segment.control.y - offset,
-                        offset * 2,
-                        offset * 2,
-                        selected_node,
-                        segment,
                         segment.control,
+                        segment,
+                        path,
+                        selected_node,
                     )
                 )
                 self.nodes.append(
                     (
-                        segment.end.x - offset,
-                        segment.end.y - offset,
-                        offset * 2,
-                        offset * 2,
-                        selected_node,
-                        segment,
                         segment.end,
+                        segment,
+                        path,
+                        selected_node,
                     )
                 )
             elif q == "CubicBezier":
-                # self.nodes.append(
-                #     (
-                #         segment.start.x - offset,
-                #         segment.start.y - offset,
-                #         offset * 2,
-                #         offset * 2,
-                #         segment,
-                #         segment.start,
-                #     )
-                # )
                 self.nodes.append(
                     (
-                        segment.control1.x - offset,
-                        segment.control1.y - offset,
-                        offset * 2,
-                        offset * 2,
-                        selected_node,
-                        segment,
                         segment.control1,
+                        segment,
+                        path,
+                        selected_node,
                     )
                 )
                 self.nodes.append(
                     (
-                        segment.control2.x - offset,
-                        segment.control2.y - offset,
-                        offset * 2,
-                        offset * 2,
-                        selected_node,
-                        segment,
                         segment.control2,
+                        segment,
+                        path,
+                        selected_node,
                     )
                 )
                 self.nodes.append(
                     (
-                        segment.end.x - offset,
-                        segment.end.y - offset,
-                        offset * 2,
-                        offset * 2,
-                        selected_node,
-                        segment,
                         segment.end,
+                        segment,
+                        path,
+                        selected_node,
                     )
                 )
 
     def process_draw(self, gc: wx.GraphicsContext):
         if not self.nodes:
             return
+
+        offset = 5
+        s = math.sqrt(abs(self.scene.widget_root.scene_widget.matrix.determinant))
+        offset /= s
+
         gc.SetPen(self.pen)
-        for x, y, w, h, node, segment, point in self.nodes:
-            gc.DrawRectangle(x, y, w, h)
+        for pt, segment, path, node in self.nodes:
+            ptx, pty = node.matrix.point_in_matrix_space(pt)
+            gc.DrawEllipse(ptx - offset, pty - offset, offset * 2, offset * 2)
 
     def event(
         self, window_pos=None, space_pos=None, event_type=None, modifiers=None, **kwargs
     ):
-        offset = 5000
-        response = RESPONSE_CHAIN
+        offset = 5
+        s = math.sqrt(abs(self.scene.widget_root.scene_widget.matrix.determinant))
+        offset /= s
+
         if event_type == "leftdown":
             self.pen = wx.Pen()
             self.pen.SetColour(
@@ -171,42 +133,46 @@ class EditTool(ToolWidget):
             xp = space_pos[0]
             yp = space_pos[1]
             if self.nodes:
+                w = offset * 4
+                h = offset * 4
                 for i, n in enumerate(self.nodes):
-                    x, y, w, h, node, segment, point = n
+                    pt, segment, path, node = n
+                    ptx, pty = node.matrix.point_in_matrix_space(pt)
+                    x = ptx - 2 * offset
+                    y = pty - 2 * offset
                     if x <= xp <= x + w and y <= yp <= y + h:
                         self.selected_index = i
                         break
                 else:  # For-else == icky
                     self.selected_index = None
-            response = RESPONSE_CONSUME
+            return RESPONSE_CONSUME
+        elif event_type == "middledown" or event_type == "rightdown":
+            return RESPONSE_DROP
         elif event_type == "move":
             if not self.selected_index:
                 self.scene.request_refresh()
                 return RESPONSE_CONSUME
-            current_node = self.nodes[self.selected_index]
-            x, y, w, h, node, segment, point = current_node
-            point.x += space_pos[4]
-            point.y += space_pos[5]
-
+            current = self.nodes[self.selected_index]
+            pt, segment, path, node = current
+            m = node.matrix.point_in_inverse_space(space_pos[:2])
+            pt.x = m[0]
+            pt.y = m[1]
             self.nodes[self.selected_index] = (
-                point.x - offset,
-                point.y - offset,
-                offset * 2,
-                offset * 2,
-                node,
+                pt,
                 segment,
-                point,
+                path,
+                node
             )
             node.altered()
             self.scene.request_refresh()
-            response = RESPONSE_CONSUME
+            return RESPONSE_CONSUME
         elif event_type == "lost" or (event_type == "key_up" and modifiers == "escape"):
             if self.scene.tool_active:
                 self.scene.tool_active = False
                 self.scene.request_refresh()
-                response = RESPONSE_CONSUME
+                return RESPONSE_CONSUME
             else:
-                response = RESPONSE_CHAIN
+                return RESPONSE_CHAIN
         elif event_type == "leftup":
-            response = RESPONSE_CONSUME
-        return response
+            return RESPONSE_CONSUME
+        return RESPONSE_CHAIN
