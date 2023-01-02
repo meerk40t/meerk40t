c8f10dce85875b29f9fa24b419d6e21263f15ccc
Tatarize <tatarize@gmail.com>
2023-01-01 21:13:15 -0800

    remove unneeded comment from default-stroke

--- meerk40t/core/elements.py
+++ meerk40t/core/elements.py
@@ -468,7 +468,6 @@ class Elemental(Service):
 
     @property
     def default_strokewidth(self):
-        # We dont allow an empty stroke color as default (why not?!) -- Empty stroke colors are hard to see.
         if self._default_stroke is not None:
             return self._default_stroke
         return 1000.0
