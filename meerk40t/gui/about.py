e1afe71b4bc3efd2c7a4feec66ef52ee8eb5cedc
Tatarize <tatarize@gmail.com>
2023-01-02 14:48:57 -0800

    Format goes outside the _() translation

--- meerk40t/gui/about.py
+++ meerk40t/gui/about.py
@@ -98,10 +98,8 @@ class AboutPanel(wx.Panel):
             + _(
                 "* @joerlane for his hardware investigation wizardry into how the M2-Nano works.\n"
             )
-            + _(
-                "* All the MeerKittens, {developer}. \n".format(
-                    developer=", ".join(hall_of_fame)
-                )
+            + _("* All the MeerKittens, {developer}. \n").format(
+                developer=", ".join(hall_of_fame)
             )
             + _(
                 "* Beta testers and anyone who reported issues that helped us improve things.\n"
