279bdf1030db9b412ebddd39461fab7598208a3d
Tatarize <tatarize@gmail.com>
2023-01-03 00:54:20 -0800

    Restore and preswap inputs

--- meerk40t/balormk/driver.py
+++ meerk40t/balormk/driver.py
@@ -322,6 +322,8 @@ class BalorDriver:
         @param y:
         @return:
         """
+        if self.service.swap_xy:
+            x, y = y, x
         old_current = self.service.current
         self.native_x, self.native_y = self.service.physical_to_device_position(x, y)
         if self.native_x > 0xFFFF:
@@ -348,6 +350,8 @@ class BalorDriver:
         @param dy:
         @return:
         """
+        if self.service.swap_xy:
+            dx, dy = dy, dx
         old_current = self.service.current
         unit_dx, unit_dy = self.service.physical_to_device_length(dx, dy)
         self.native_x += unit_dx
