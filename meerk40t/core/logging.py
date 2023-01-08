3fbfa641c293380d22a2a3e59620fcc42e251dd5
Tatarize <tatarize@gmail.com>
2023-01-08 07:24:00 -0800

    Add in matching events to search for any provided keys and yield matches

--- meerk40t/core/logging.py
+++ meerk40t/core/logging.py
@@ -18,6 +18,19 @@ class Logging(Service):
         self._setting_config = Settings(self.kernel.name, "meerk40t.log")
         self.logs = self._setting_config.literal_dict()
 
+    def matching_events(self, prefix, **kwargs):
+        for event_key in list(self.logs):
+            if not str(event_key).startswith(prefix):
+                continue
+            event = self.logs[event_key]
+            good = True
+            for key in kwargs:
+                if key in event and kwargs[key] != event[key]:
+                    good = False
+                    break
+            if good:
+                yield event_key, event
+
     def uid(self, prefix):
         if "uid" not in self.logs:
             self.logs["uid"] = dict()
