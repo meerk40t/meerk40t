a788762a93e48d5f5de2f14e8b1a84442d7bf894
Tatarize <tatarize@gmail.com>
2023-01-20 22:36:23 -0800

    Correct line drawing within rotation widget

--- meerk40t/gui/utilitywidgets/rotationwidget.py
+++ meerk40t/gui/utilitywidgets/rotationwidget.py
@@ -18,16 +18,15 @@ class RotationWidget(HandleWidget):
         self.apply_delta = apply_delta
 
     def event(self, window_pos=None, space_pos=None, event_type=None, **kwargs):
-
         if event_type == "leftup":
             self.tool = None
         if event_type == "move":
             cx = (self.left + self.right) / 2.0
             cy = (self.top + self.bottom) / 2.0
-            angle_previous = Geomstr.angle(None, complex(cx, cy), complex(*space_pos[2:3]))
+            angle_previous = Geomstr.angle(None, complex(cx, cy), complex(*space_pos[2:4]))
             if math.isnan(angle_previous):
                 return RESPONSE_CONSUME
-            angle_current = Geomstr.angle(None, complex(cx, cy), complex(*space_pos[0:2]))
+            angle_current = Geomstr.angle(None, complex(cx, cy), complex(*space_pos[:2]))
 
             delta_theta = angle_current - angle_previous
             if delta_theta > math.tau / 2:
@@ -36,14 +35,14 @@ class RotationWidget(HandleWidget):
                 delta_theta += math.tau
             if not math.isnan(delta_theta):
                 self.apply_delta(delta_theta)
-            self.tool = list(space_pos[0:2])
+            self.tool = list(space_pos[:2])
         return RESPONSE_CONSUME
 
-    def draw(self, gc):
+    def draw(self, gc: wx.GraphicsContext):
         super().draw(gc)
-        if self.tool is not None:
-            cx = (self.left + self.right) / 2.0
-            cy = (self.top + self.bottom) / 2.0
-            gc.SetPen(self.tool_pen)
-            gc.DrawLine((cx, cy), self.tool)
+        if not self.tool:
+            return
+        gc.SetPen(self.tool_pen)
+        gc.StrokeLine((self.left + self.right) / 2.0, (self.top + self.bottom) / 2.0, self.tool[0], self.tool[1])
+
 
