f405e2937b27eed49defabd7abb3d9ec5a48abec
Tatarize <tatarize@gmail.com>
2023-01-24 15:02:00 -0800

    divide console_server away from tcp_server

--- meerk40t/network/kernelserver.py
+++ meerk40t/network/kernelserver.py
@@ -3,6 +3,7 @@ def plugin(kernel, lifecycle=None):
     if lifecycle == "plugins":
         from .tcp_server import plugin as tcp
         from .udp_server import plugin as udp
-        return [tcp, udp]
+        from .console_server import plugin as console_server
+        return [tcp, udp, console_server]
     if lifecycle == "invalidate":
         return True
