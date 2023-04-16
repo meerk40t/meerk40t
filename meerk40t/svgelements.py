390ce9641beca62f65811585cee453edcbc6c754
Tatarize <tatarize@gmail.com>
2023-04-16 14:52:29 -0700

    wider fallback for numerical instability

--- meerk40t/svgelements.py
+++ meerk40t/svgelements.py
@@ -4775,7 +4775,7 @@ class CubicBezier(Curve):
         local_extremizers = [0, 1]
         a = [c[v] for c in self]
         denom = a[0] - 3 * a[1] + 3 * a[2] - a[3]
-        if abs(denom) >= 1e-12:
+        if abs(denom) >= 1e-8:
             delta = (
                 a[1] * a[1] - (a[0] + a[1]) * a[2] + a[2] * a[2] + (a[0] - a[1]) * a[3]
             )
