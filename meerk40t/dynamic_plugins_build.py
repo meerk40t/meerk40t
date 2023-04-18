092866b357b9544647c98873be1729e2e479fc63
Tatarize <tatarize@gmail.com>
2023-04-18 16:27:53 -0700

    Add in hard-coded list of approved plugins.

---
+++ meerk40t/dynamic_plugins_build.py
@@ -0,0 +1,24 @@
+"""
+This file is used in builds to replace the dynamic plugins file. This file should not be included anywhere. It should
+be swapped during a full build so that entry_points get switched for hard-linked references. This ensures that builds
+can contain a list of useful desired plugins.
+
+If you have written such a plugin, raise an issue to have it reviewed and included into the builds.
+"""
+
+
+def plugin(kernel, lifecycle):
+    if lifecycle == "plugins":
+        plugins = list()
+
+        """
+        This loads Jpirnay's meerk40t-barcodes plugin.
+
+        https://github.com/meerk40t/meerk40t-barcodes
+        """
+
+        from barcodes.main import plugin as barplugin
+
+        plugins.append(barplugin)
+
+        return plugins
