7659674510fd6e63c6b5fd69e12f3fd26c7eca28
Tatarize <tatarize@gmail.com>
2023-04-19 15:30:59 -0700

    Add view and space service

---
+++ meerk40t/core/space.py
@@ -0,0 +1,91 @@
+from meerk40t.core.units import UNITS_PER_MM, Length
+from meerk40t.core.view import View
+from meerk40t.kernel import Service
+
+
+def plugin(kernel, lifecycle=None):
+    _ = kernel.translation
+    if lifecycle == "register":
+        kernel.add_service("space", CoordinateSystem(kernel))
+
+
+class CoordinateSystem(Service):
+    def __init__(self, kernel, *args, **kwargs):
+        Service.__init__(self, kernel, "space")
+        _ = kernel.translation
+        choices = [
+            {
+                "attr": "origin_x",
+                "object": self,
+                "default": 0.0,
+                "type": float,
+                "label": _("Origin X"),
+                "tip": _("Value between 0-1 for the location of the origin x parameter"),
+            },
+            {
+                "attr": "origin_y",
+                "object": self,
+                "default": 0.0,
+                "type": float,
+                "label": _("Origin Y"),
+                "tip": _("Value between 0-1 for the location of the origin x parameter"),
+            },
+            {
+                "attr": "right_positive",
+                "object": self,
+                "default": True,
+                "type": bool,
+                "label": _("Right Positive"),
+                "tip": _("Are positive values to the right?"),
+            },
+            {
+                "attr": "bottom_positive",
+                "object": self,
+                "default": True,
+                "type": bool,
+                "label": _("Bottom Positive"),
+                "tip": _("Are positive values towards the bottom?"),
+            },
+            {
+                "attr": "swap_xy",
+                "object": self,
+                "default": True,
+                "type": bool,
+                "label": _("Swap XY"),
+                "tip": _("XY coordinates are swapped"),
+            },
+            {
+                "attr": "rotation",
+                "object": self,
+                "default": 0,
+                "type": int,
+                "label": _("Rotation"),
+                "tip": _("Rotation in degrees"),
+            },
+            {
+                "attr": "units",
+                "object": self,
+                "default": 0,
+                "type": int,
+                "label": _("Preferred Units"),
+                "tip": _("Set default units for positions"),
+                "style": "option",
+                "display": (_("tat"), _("mm"), _("cm"), _("inch"), _("mil")),
+                "choices": (0, 1, 2, 3, 4),
+            },
+        ]
+        kernel.register_choices("space", choices)
+        self.x = None
+        self.y = None
+        self.width = None
+        self.height = None
+        self.display = None
+        self.update_bounds(0, 0, "100mm", "100mm")
+
+    def update_bounds(self, x, y, width, height):
+        self.x = float(Length(x))
+        self.y = float(Length(y))
+        self.width = float(Length(width))
+        self.height = float(Length(height))
+        self.display = View(self.width, self.height, dpi_x=UNITS_PER_MM, dpi_y=UNITS_PER_MM)
+
