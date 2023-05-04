343586f3a90f042ad4389deac37d7d7cbe5499c8
jpirnay <jens.k.pirnay@googlemail.com>
2023-05-04 10:20:42 +0200

    Fix kerf code

--- meerk40t/core/node/nutils.py
+++ meerk40t/core/node/nutils.py
@@ -7,11 +7,27 @@ from meerk40t.core.cutcode.cutgroup import CutGroup
 from meerk40t.core.cutcode.linecut import LineCut
 from meerk40t.core.cutcode.quadcut import QuadCut
 from meerk40t.core.units import UNITS_PER_INCH
-from meerk40t.svgelements import Close, CubicBezier, Line, Move, Path, QuadraticBezier, Arc
+from meerk40t.svgelements import (
+    Arc,
+    Close,
+    CubicBezier,
+    Line,
+    Move,
+    Path,
+    QuadraticBezier,
+    Rect,
+)
 from meerk40t.tools.offset import OffsetPath
 
+
 def path_to_cutobjects(
-    path, settings, closed_distance=15, passes=1, original_op=None, color=None, kerf=0,
+    path,
+    settings,
+    closed_distance=15,
+    passes=1,
+    original_op=None,
+    color=None,
+    kerf=0,
 ):
     for subpath in path.as_subpaths():
         sp = Path(subpath)
@@ -29,11 +45,15 @@ def path_to_cutobjects(
             color=color,
         )
         if kerf != 0:
+
             offset = OffsetPath(originalpath=sp, offset=kerf, connection=1)
             sp = offset.result()
+            # Just a test to see if it works, replace path by it bounding box
+            # bb = sp.bbox(transformed=True)
+            # sp = Path(Rect(x=bb[0], y=bb[1], width=bb[2] - bb[0], height=bb[3] - bb[1]))
         group.path = sp
         group.original_op = original_op
-        for seg in subpath:
+        for seg in sp:
             if isinstance(seg, Move):
                 pass  # Move operations are ignored.
             elif isinstance(seg, Close):
@@ -85,7 +105,6 @@ def path_to_cutobjects(
                         color=color,
                     )
                 )
-
 
         if len(group) > 0:
             group[0].first = True
