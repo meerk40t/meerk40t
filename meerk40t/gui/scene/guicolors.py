ae7fffb23866445c2ae32d99800555180993a496
Tatarize <tatarize@gmail.com>
2023-05-07 01:35:50 -0700

    externalize default_color, random_color, and add `get` command.

--- meerk40t/gui/scene/guicolors.py
+++ meerk40t/gui/scene/guicolors.py
@@ -5,6 +5,38 @@ import wx
 from meerk40t.svgelements import Color
 
 
+def random_color():
+    return (
+        f"#"
+        f"{random.randint(0, 255):02X}"
+        f"{random.randint(0, 255):02X}"
+        f"{random.randint(0, 255):02X}"
+    )
+
+
+default_color = {
+    "manipulation": "#A07FA0",
+    "manipulation_handle": "#A07FA0",
+    "laserpath": "#FF000040",
+    "grid": "#A0A0A080",
+    "guide": "#000000",
+    "background": "#7F7F7F",
+    "magnetline": "#A0A0FF60",
+    "snap_visible": "#A0A0A040",
+    "snap_closeup": "#00FF00A0",
+    "selection1": "#0000FF",
+    "selection2": "#00FF00",
+    "selection3": "#FF0000",
+    "measure_line": "#0000FF80",
+    "measure_text": "#FF000060",
+    "bed": "#FFFFFF",
+    "grid2": "#A0A0A080",
+    "guide2": "#000000",
+    "grid3": "#A0A0A080",
+    "guide3": "#A0A0A080",
+}
+
+
 class GuiColors:
     """
     Provides and stores all relevant colors for a scene
@@ -12,34 +44,25 @@ class GuiColors:
 
     def __init__(self, context):
         self.context = context
-        self.default_color = {
-            "manipulation": "#A07FA0",
-            "manipulation_handle": "#A07FA0",
-            "laserpath": "#FF000040",
-            "grid": "#A0A0A080",
-            "guide": "#000000",
-            "background": "#7F7F7F",
-            "magnetline": "#A0A0FF60",
-            "snap_visible": "#A0A0A040",
-            "snap_closeup": "#00FF00A0",
-            "selection1": "#0000FF",
-            "selection2": "#00FF00",
-            "selection3": "#FF0000",
-            "measure_line": "#0000FF80",
-            "measure_text": "#FF000060",
-            "bed": "#FFFFFF",
-            "grid2": "#A0A0A080",
-            "guide2": "#000000",
-            "grid3": "#A0A0A080",
-            "guide3": "#A0A0A080",
-        }
-        for key in self.default_color:
-            self.context.setting(str, f"color_{key}", self.default_color[key])
+        for key in default_color:
+            self.context.setting(str, f"color_{key}", default_color[key])
         self.sanity_check()
 
+    def get(self, item, default=None):
+        if default is None:
+            if item in default_color:
+                default = default_color[item]
+            else:
+                default = random_color()
+        return self.context.setting(str, f"color_{item}", default)
+
     def sanity_check(self):
-        # Look at some color-combinations to see if there are identical colors, if that's the case
-        # then this is degenerate and will lead to default colors
+        """
+        Look at some color-combinations to see if there are identical colors, if that's the case
+        then this is degenerate and will lead to default colors
+        @return:
+        """
+
         def identical(color1, color2):
             return color1.GetRGB() == color2.GetRGB()
 
@@ -60,7 +83,7 @@ class GuiColors:
             if identical(bed_color, item_color):
                 fixed += 1
                 color_key = f"color_{key}"
-                setattr(self.context, color_key, self.default_color[key])
+                setattr(self.context, color_key, default_color[key])
         # if fixed>0:
         # self.context("Reset %d colors to their defaults, as thye wre indistinguishable from the bed" % fixed)
 
@@ -68,23 +91,17 @@ class GuiColors:
         """
         Reset all colors to random values...
         """
-        for key in self.default_color:
+        for key in default_color:
             color_key = f"color_{key}"
-            random_color = (
-                f"#"
-                f"{random.randint(0, 255):02X}"
-                f"{random.randint(0, 255):02X}"
-                f"{random.randint(0, 255):02X}"
-            )
-            setattr(self.context, color_key, random_color)
+            setattr(self.context, color_key, random_color())
 
     def set_default_colors(self):
         """
         Reset all colors to default values...
         """
-        for key in self.default_color:
+        for key in default_color:
             color_key = f"color_{key}"
-            setattr(self.context, color_key, self.default_color[key])
+            setattr(self.context, color_key, default_color[key])
 
     def _get_color(self, key):
         color_key = f"color_{key}"
@@ -92,10 +109,10 @@ class GuiColors:
             s = getattr(self.context, color_key)
             if len(s) == 0 or s == "default":
                 # print ("Reset requested for color: %s" % color_key)
-                s = self.default_color[key]
+                s = default_color[key]
                 setattr(self.context, color_key, s)
         else:
-            s = self.default_color[key]
+            s = default_color[key]
         c = Color(s)
         return wx.Colour(red=c.red, green=c.green, blue=c.blue, alpha=c.alpha)
 
