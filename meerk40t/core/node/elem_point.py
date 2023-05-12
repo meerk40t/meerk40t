f57295b5c0c522c5ed3a47e923c248a0e4e20432
Tatarize <tatarize@gmail.com>
2023-05-12 01:11:02 -0700

    elem point update to unbacked

--- meerk40t/core/node/elem_point.py
+++ meerk40t/core/node/elem_point.py
@@ -1,7 +1,8 @@
 from copy import copy
 
 from meerk40t.core.node.node import Node
-from meerk40t.svgelements import Matrix, Point
+from meerk40t.svgelements import Point
+from meerk40t.tools.geomstr import Geomstr
 
 
 class PointNode(Node):
@@ -10,7 +11,6 @@ class PointNode(Node):
     """
 
     def __init__(self, **kwargs):
-        self.point = None
         self.x = 0
         self.y = 0
         self.matrix = None
@@ -22,45 +22,37 @@ class PointNode(Node):
 
     def __copy__(self):
         nd = self.node_dict
-        nd["point"] = copy(self.point)
         nd["matrix"] = copy(self.matrix)
         nd["fill"] = copy(self.fill)
+        nd["stroke"] = copy(self.stroke)
         nd["stroke_width"] = copy(self.stroke_width)
         return PointNode(**nd)
 
-    def validate(self):
-        if self.matrix is None:
-            self.matrix = Matrix()
-        if self.point is None:
-            x = float(self.x)
-            y = float(self.y)
-            self.matrix.pre_translate(x, y)
-            self.point = Point(0, 0)
+    def as_geometry(self):
+        path = Geomstr()
+        path.point(complex(self.x, self.y))
+        path.transform(self.matrix)
+        return path
+
+    @property
+    def point(self):
+        x = float(self.x)
+        y = float(self.y)
+        return Point(*self.matrix.point_in_matrix_space((x, y)))
 
     def preprocess(self, context, matrix, plan):
-        if self.point is None:
-            self.validate()
         self.matrix *= matrix
         self.set_dirty_bounds()
-        if not self.matrix.is_identity():
-            self.point = matrix.point_in_matrix_space(self.point)
-            self.matrix.reset()
 
     def bbox(self, transformed=True, with_stroke=False):
-        if self.point is None:
-            return None
-        p = self.matrix.point_in_matrix_space(self.point)
+        p = self.point
         return p[0], p[1], p[0], p[1]
 
     def default_map(self, default_map=None):
         default_map = super().default_map(default_map=default_map)
         default_map["element_type"] = "Point"
-        if self.point is not None:
-            default_map["x"] = self.point[0]
-            default_map["y"] = self.point[1]
-        else:
-            default_map["x"] = 0
-            default_map["y"] = 0
+        default_map["x"] = self.point[0]
+        default_map["y"] = self.point[1]
         default_map.update(self.__dict__)
         return default_map
 
