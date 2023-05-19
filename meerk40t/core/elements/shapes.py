e2ebd7e7b4a33a567506c161b73132fb2d54963d
Tatarize <tatarize@gmail.com>
2023-05-19 04:40:45 -0700

    Add shape_node command to convert geometry to default shape-node values

--- meerk40t/core/elements/shapes.py
+++ meerk40t/core/elements/shapes.py
@@ -676,6 +676,21 @@ def init_commands(kernel):
         return "elements", data
 
     @self.console_command(
+        "shape_node",
+        help=_("Convert any shapes to pathnodes"),
+        input_type="geometry",
+        output_type="elements",
+    )
+    def element_shape_convert(data, **kwargs):
+        node = self.elem_branch.add(
+            geometry=data,
+            stroke=self.default_stroke,
+            stroke_width=self.default_strokewidth,
+            type="elem path",
+        )
+        return "elements", [node]
+
+    @self.console_command(
         "path",
         help=_("Convert any element nodes to paths"),
         input_type="elements",
