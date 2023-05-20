a30bd0ccfc8103eb8f85cd980ee9e96362b609f3
Tatarize <tatarize@gmail.com>
2023-05-20 06:56:10 -0700

    bounds: Restore old functionality

--- meerk40t/balormk/livelightjob.py
+++ meerk40t/balormk/livelightjob.py
@@ -137,11 +137,10 @@ class LiveLightJob:
         if self.stopped:
             return False
         bounds = self.service.elements.selected_area()
-        # if self._last_bounds is not None and bounds != self._last_bounds:
-        #     # Emphasis did not change but the bounds did. We dragged something.
-        #     self.changed = True
-        # if bounds is not None:
-        self._last_bounds = copy(bounds)
+        if self._last_bounds is not None and bounds != self._last_bounds:
+            # Emphasis did not change but the bounds did. We dragged something.
+            self.changed = True
+        self._last_bounds = bounds
 
         if self.changed:
             # The emphasis selection has changed.
