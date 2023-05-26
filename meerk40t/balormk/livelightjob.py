179a4133d36b647f6b905373ee9ed8354589fa7f
Tatarize <tatarize@gmail.com>
2023-05-25 21:38:12 -0700

    make goto last position on cancel

--- meerk40t/balormk/livelightjob.py
+++ meerk40t/balormk/livelightjob.py
@@ -151,9 +151,10 @@ class LiveLightJob:
             # The emphasis selection has changed.
             self.changed = False
             con.abort()
+            first_x, first_y = con.get_last_xy()
             # first_x = 0x8000
             # first_y = 0x8000
-            # con.goto_xy(first_x, first_y, distance=0xFFFF)
+            con.goto_xy(first_x, first_y, distance=0xFFFF)
             con.light_mode()
         con._light_speed = self.service.redlight_speed
         con._dark_speed = self.service.redlight_speed
