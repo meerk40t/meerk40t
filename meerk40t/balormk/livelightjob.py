13951ee5caf7ef8f646cb985501ba4e19fccb8fe
Tatarize <tatarize@gmail.com>
2023-05-25 21:43:04 -0700

    goto with dark

--- meerk40t/balormk/livelightjob.py
+++ meerk40t/balormk/livelightjob.py
@@ -154,7 +154,8 @@ class LiveLightJob:
             first_x, first_y = con.get_last_xy()
             # first_x = 0x8000
             # first_y = 0x8000
-            con.goto_xy(first_x, first_y, distance=0xFFFF)
+
+            con.dark(first_x, first_y, distance=0xFFFF)
             con.light_mode()
         con._light_speed = self.service.redlight_speed
         con._dark_speed = self.service.redlight_speed
