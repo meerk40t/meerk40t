edc5b753f8f1700d5a2453c9dde0052eaadd912a
Tatarize <tatarize@gmail.com>
2023-06-12 22:43:29 -0700

    Move delete to basic editing

--- meerk40t/gui/wxmmain.py
+++ meerk40t/gui/wxmmain.py
@@ -1153,18 +1153,6 @@ class MeerK40t(MWindow):
             },
         )
         kernel.register(
-            "button/tools/Delete",
-            {
-                "label": _("Delete"),
-                "icon": icons8_delete_50,
-                "tip": _(
-                    "Delete selected items"
-                ),
-                "action": lambda v: kernel.elements("tree selected delete\n"),
-                "size": bsize_normal,
-            },
-        )
-        kernel.register(
             "button/tools/Text",
             {
                 "label": _("Text"),
@@ -1177,6 +1165,19 @@ class MeerK40t(MWindow):
             },
         )
 
+        kernel.register(
+            "button/basicediting/Delete",
+            {
+                "label": _("Delete"),
+                "icon": icons8_delete_50,
+                "tip": _(
+                    "Delete selected items"
+                ),
+                "action": lambda v: kernel.elements("tree selected delete\n"),
+                "size": bsize_normal,
+                "rule_enabled": lambda cond: bool(kernel.elements.has_emphasis()),
+            },
+        )
         kernel.register(
             "button/basicediting/Cut",
             {
