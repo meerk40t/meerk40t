ba458ea30aeecd969fe430f3e52ac7de590050d1
tatarize <tatarize@gmail.com>
2023-09-18 13:09:56 -0700

    Add additional tests

--- test/test_operations.py
+++ test/test_operations.py
@@ -1,10 +1,13 @@
 import unittest
 from copy import copy
 
+from meerk40t.core.node.branch_ops import BranchOperationsNode
 from meerk40t.core.node.op_cut import CutOpNode
 from meerk40t.core.node.op_engrave import EngraveOpNode
 from meerk40t.core.node.op_image import ImageOpNode
 from meerk40t.core.node.op_raster import RasterOpNode
+from meerk40t.core.node.rootnode import RootNode
+from test import bootstrap
 
 
 class TestOperations(unittest.TestCase):
@@ -44,7 +47,7 @@ class TestOperations(unittest.TestCase):
 
     def test_operation_copy_knockon(self):
         """
-        Test code to ensure operations copy correctly
+        Ensure test copy is not reusing the same data.
 
         :return:
         """
@@ -66,6 +69,39 @@ class TestOperations(unittest.TestCase):
         self.assertNotEquals(node_copy.dancing_bear, node.dancing_bear)
         node_copy_copy = copy(node_copy)
         self.assertEquals(node_copy_copy.dancing_bear, node_copy.dancing_bear)
+        node.allowed_attributes.append("Fancy pants")
+        self.assertNotEquals(node_copy_copy.allowed_attributes, node.allowed_attributes)
+
+    def test_operation_copy_root(self):
+        """
+        Ensure that copy, does not copy the root of the node (it's a copy of the node not in the tree).
+
+        :return:
+        """
+        kernel = bootstrap.bootstrap()
+        try:
+            kernel_root = kernel.get_context("/")
+
+            root = RootNode(context=kernel_root, label="RootNode")
+            node = EngraveOpNode(
+                id="fake_id",
+                label="My label",
+                color="green",
+                lock=True,
+                allowed_attributes=["number_of_unicorns"],
+                stroke="green",
+                fill="gray",
+                stroke_width=7,
+                dancing_bear=0.2,
+            )
+            root.add_node(node)
+            self.assertIs(root, node._root)
+            
+            node_copy = copy(node)
+            self.assertIsNot(node_copy._root, root)
+
+        finally:
+            kernel.shutdown()
 
     def test_operation_copy_cut(self):
         """
