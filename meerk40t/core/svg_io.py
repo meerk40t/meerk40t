ba56227707d3d166f5f63d8537c26f72ecbe4d6d
tatarize <tatarize@gmail.com>
2023-10-06 23:47:55 -0700

    op-hatch from svg should be placed inside the hatch-effect now

--- meerk40t/core/svg_io.py
+++ meerk40t/core/svg_io.py
@@ -1183,13 +1183,15 @@ class SVGProcessor:
                         # Special fallback operation, op hatch is an op engrave with an effect hatch within it.
                         node_type = "op engrave"
                         op = self.elements.op_branch.create(type=node_type, **attrs)
-                        op.add(type="effect hatch", **attrs)
+                        op = op.add(type="effect hatch", **attrs)
                     else:
                         op = self.elements.op_branch.create(type=node_type, **attrs)
+
                     if op is None or not hasattr(op, "type") or op.type is None:
                         return
                     if hasattr(op, "validate"):
                         op.validate()
+
                     op.id = node_id
                     if context_node.type.startswith("effect"):
                         op.append_child(context_node)
