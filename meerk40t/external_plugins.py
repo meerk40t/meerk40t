6ea292473a7e5085129f835f7be062e764fb71c4
tatarize <tatarize@gmail.com>
2023-10-12 16:05:50 -0700

    Use the pkg_importer as fallback

--- meerk40t/external_plugins.py
+++ meerk40t/external_plugins.py
@@ -4,6 +4,57 @@ builds, since compiled versions do not have access to entry points. External plu
 builds. See the external_plugin_build.py file for regular built plugins.
 """
 
+MEERK40T_ENTRYPOINT = "meerk40t.extension"
+
+
+def plugins_pre_38():
+    """
+    Import plugins using pkg_resources. pkg_resources was deprecated in 3.10 and removed in 3.12
+    @return:
+    """
+    import pkg_resources
+
+    for entry_point in pkg_resources.iter_entry_points(MEERK40T_ENTRYPOINT):
+        try:
+            plugin = entry_point.load()
+        except pkg_resources.DistributionNotFound:
+            pass
+        except pkg_resources.VersionConflict as e:
+            print(
+                "Cannot install plugin - '{entrypoint}' due to version conflict.".format(
+                    entrypoint=str(entry_point)
+                )
+            )
+            print(e)
+        else:
+            yield plugin
+
+
+def plugins_importlib():
+    """
+    Import entry points with importlib.
+
+    @return:
+    """
+    from importlib.metadata import entry_points
+    entry_points = entry_points()
+
+    try:
+        ep = entry_points.select(group=MEERK40T_ENTRYPOINT)
+    except AttributeError:
+        ep = entry_points.get(MEERK40T_ENTRYPOINT)
+
+    for entry_point in ep:
+        try:
+            yield entry_point.load()
+        except ImportError as e:
+            print(
+                "Cannot install plugin - '{entrypoint}'.".format(
+                    entrypoint=str(entry_point.value)
+                )
+            )
+            print(e)
+
 
 def plugin(kernel, lifecycle):
     import sys
@@ -14,30 +65,11 @@ def plugin(kernel, lifecycle):
         if kernel.args.no_plugins:
             return
 
-        plugins = list()
-
         try:
-            from importlib.metadata import entry_points
+            return list(plugins_importlib())
         except ImportError:
-            from importlib_metadata import entry_points
+            return list(plugins_pre_38())
 
-        entry_points = entry_points()
-        try:
-            ep = entry_points.select(group="meerk40t.extension")
-        except AttributeError:
-            ep = entry_points.get("meerk40t.extension")
-
-        for entry_point in ep:
-            try:
-                plugin = entry_point.load()
-                plugins.append(plugin)
-            except ImportError as e:
-                print(
-                    "Cannot install plugin - '{entrypoint}'.".format(
-                        entrypoint=str(entry_point.value)
-                    )
-                )
-                print(e)
-        return plugins
     if lifecycle == "invalidate":
+        # Do not maintain plugin.
         return True
