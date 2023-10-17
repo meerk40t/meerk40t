5af0260753087acf343767b47bdf4ea4203c29c0
jpirnay <jens@pirnay.com>
2023-10-17 22:19:29 +0200

    Remove debug statement

--- meerk40t/gui/wxmeerk40t.py
+++ meerk40t/gui/wxmeerk40t.py
@@ -305,14 +305,13 @@ class wxMeerK40t(wx.App, Module):
         # monitors with font scaling enabled.
         from platform import system
 
-        no_dpi = context.setting(bool, "no_high_dpi", False)
-        if system() == "Windows" and not no_dpi:
+        high_dpi = context.setting(bool, "high_dpi", True)
+        if system() == "Windows" and high_dpi:
             try:
                 # https://discuss.wxpython.org/t/support-for-high-dpi-on-windows-10/32925
                 from ctypes import OleDLL
 
                 OleDLL("shcore").SetProcessDpiAwareness(1)
-                print("High DPI set")
             except (AttributeError, ImportError):
                 # We're on a non-Windows box.
                 pass
