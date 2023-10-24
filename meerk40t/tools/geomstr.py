8d76f36ec2185f35a84c58f512e1fa232975a5dc
tatarize <tatarize@gmail.com>
2023-10-24 10:04:41 -0700

    Add find intersection kross

--- meerk40t/tools/geomstr.py
+++ meerk40t/tools/geomstr.py
@@ -2804,9 +2804,9 @@ class Geomstr:
         fun2 = self._get_segment_function(segment2[2].real)
         if fun1 is None or fun2 is None:
             return  # Only shapes can intersect. We don't do point x point.
-        yield from self._find_intersections_main(segment1, segment2, fun1, fun2)
+        yield from self._find_intersections_kross(segment1, segment2, fun1, fun2)
 
-    def _find_intersections_main(
+    def _find_intersections_intercept(
         self,
         segment1,
         segment2,
@@ -2890,7 +2890,7 @@ class Geomstr:
                 # We've enhanced as best as we can, yield the current + segment t-value to our answer
                 yield at + a_fractional, bt + b_fractional
             else:
-                yield from self._find_intersections_main(
+                yield from self._find_intersections_intercept(
                     segment1,
                     segment2,
                     fun1,
@@ -2902,6 +2902,118 @@ class Geomstr:
                     enhancements=enhancements,
                     enhance_samples=enhance_samples,
                 )
+
+    def _find_intersections_kross(
+        self,
+        segment1,
+        segment2,
+        fun1,
+        fun2,
+        samples=50,
+        ta=(0.0, 1.0, None),
+        tb=(0.0, 1.0, None),
+        depth=0,
+        enhancements=2,
+        enhance_samples=50,
+    ):
+        """
+        Calculate intersections by linearized polyline intersections with enhancements.
+        We calculate probable intersections by linearizing our segment into `sample` polylines
+        we then find those intersecting segments and the range of t where those intersections
+        could have occurred and then subdivide those segments in a series of enhancements to
+        find their intersections with increased precision.
+
+        This code is fast, but it could fail by both finding a rare phantom intersection (if there
+        is a low or no enhancements) or by failing to find a real intersection. Because the polylines
+        approximation did not intersect in the base case.
+
+        At a resolution of about 1e-15 the intersection calculations become unstable and intersection
+        candidates can duplicate or become lost. We terminate at that point and give the last best
+        guess.
+
+        :param segment1:
+        :param segment2:
+        :param samples:
+        :param ta:
+        :param tb:
+        :param depth:
+        :param enhancements:
+        :param enhance_samples:
+        :return:
+        """
+        assert samples >= 2
+        a = np.linspace(ta[0], ta[1], num=samples)
+        b = np.linspace(tb[0], tb[1], num=samples)
+        step_a = a[1] - a[0]
+        step_b = b[1] - b[0]
+        j = fun1(segment1, a)
+        k = fun2(segment2, b)
+
+        p0 = j[:-1]
+        d0 = j[1:] - j[:-1]
+        p1 = k[:-1]
+        d1 = k[1:] - k[:-1]
+
+        ap0, ap1 = np.meshgrid(p0, p1)
+        ad0, ad1 = np.meshgrid(d0, d1)
+        e = ap1 - ap0
+        ex = np.real(e)
+        ey = np.imag(e)
+        d0x = np.real(ad0)
+        d0y = np.imag(ad0)
+        d1x = np.real(ad1)
+        d1y = np.imag(ad1)
+
+        kross = (d0x * d1y) - (d0y * d1x)
+        # sqkross = kross * kross
+        # sqLen0 = np.real(ad0) * np.real(ad0) + np.imag(ad0) * np.imag(ad0)
+        # sqLen1 = np.real(ad1) * np.real(ad1) + np.imag(ad1) * np.imag(ad1)
+        s = ((ex * d1y) - (ey * d1x)) / kross
+        t = ((ex * d0y) - (ey * d0x)) / kross
+        hits = np.dstack(
+            (
+                # sqkross > 0.1 * sqLen0 * sqLen1,
+                s >= 0,
+                s <= 1,
+                t >= 0,
+                t <= 1,
+            )
+        ).all(axis=2)
+        where_hit = np.argwhere(hits)
+        # pos = ap0[hits] + s[hits] * ad0[hits]
+        if len(where_hit) != 1 and step_a < 1e-10:
+            # We're hits are becoming unstable give last best value.
+            if ta[2] is not None and tb[2] is not None:
+                yield ta[2], tb[2]
+            return
+
+        # Calculate the t values for the intersections
+        ta_hit = t[hits]
+        tb_hit = s[hits]
+
+        for i, hit in enumerate(where_hit):
+            at = ta[0] + float(hit[1]) * step_a  # Zoomed min+segment intersected.
+            bt = tb[0] + float(hit[0]) * step_b
+            # Fractional guess within intersected segment
+            a_fractional = ta_hit[i] * step_a
+            b_fractional = tb_hit[i] * step_b
+            if depth == enhancements:
+                # We've enhanced as best as we can, yield the current + segment t-value to our answer
+                yield at + a_fractional, bt + b_fractional
+            else:
+                yield from self._find_intersections_kross(
+                    segment1,
+                    segment2,
+                    fun1,
+                    fun2,
+                    ta=(at, at + step_a, at + a_fractional),
+                    tb=(bt, bt + step_b, bt + b_fractional),
+                    samples=enhance_samples,
+                    depth=depth + 1,
+                    enhancements=enhancements,
+                    enhance_samples=enhance_samples,
+                )
+
 
     def _find_polyline_intersections(self, a, b):
         """
