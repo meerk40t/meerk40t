5410f586c48c9550aff53f1b56468b9a60c918da
jpirnay <jens@pirnay.com>
2023-11-04 18:31:32 +0100

    Redo tats pause button change

--- meerk40t/gui/laserpanel.py
+++ meerk40t/gui/laserpanel.py
@@ -163,7 +163,7 @@ class LaserPanel(wx.Panel):
 
         sizer_control.Add(self.button_start, 1, wx.EXPAND, 0)
 
-        self.button_pause = wx.Button(self, wx.ID_ANY, _("Pause"))
+        self.button_pause = HoverButton(self, wx.ID_ANY, _("Pause"))
         self.button_pause.SetToolTip(_("Pause/Resume the laser"))
         self.button_pause.SetBitmap(
             icons8_pause.GetBitmap(
