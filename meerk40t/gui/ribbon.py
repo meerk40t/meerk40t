81688ddf7cbbc8bfa99a1c35f06fd869f4c4e9e8
jpirnay <jens@pirnay.com>
2023-11-17 17:04:53 +0100

    Dont draw empty panes

--- meerk40t/gui/ribbon.py
+++ meerk40t/gui/ribbon.py
@@ -1344,7 +1344,7 @@ class Art:
                 int(x), int(y), int(x1 - x), int(y1 - y), self.rounded_radius
             )
             for panel in page.panels:
-                if panel is None:
+                if panel is None or panel.visible_button_count == 0:
                     continue
                 self._paint_panel(dc, panel)
                 for button in panel.visible_buttons():
