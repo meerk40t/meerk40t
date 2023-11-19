18eba5b2a2543301d3e0b35dae47b823f2f906f8
tatarize <tatarize@gmail.com>
2023-11-19 04:33:49 -0800

    Implement initial perspective warp

--- meerk40t/core/node/effect_warp.py
+++ meerk40t/core/node/effect_warp.py
@@ -1,25 +1,70 @@
-import math
+import numpy as np
 from copy import copy
-from math import sqrt
 
 from meerk40t.core.node.mixins import FunctionalParameter
 from meerk40t.core.node.node import Node
-from meerk40t.core.units import Angle, Length
-from meerk40t.svgelements import Color, Matrix
-from meerk40t.tools.geomstr import Geomstr  # ,  Scanbeam
+from meerk40t.svgelements import Color
+from meerk40t.tools.geomstr import Geomstr
 
 
 class PMatrix:
     def __init__(self):
-        self.a = 1
-        self.b = 0
-        self.c = 0
-        self.d = 0
-        self.e = 1
-        self.f = 0
-        self.g = 0
-        self.h = 0
-        self.i = 1
+        self.mx = np.zeros((3, 3), dtype=float)
+        self.mx[0, 0] = 1.0
+        self.mx[1, 1] = 1.0
+        self.mx[2, 2] = 1.0
+
+    @classmethod
+    def perspective(cls, p1, p2, p3, p4):
+        """
+        Create a matrix which transforms these four ordered points to the clockwise points of the unit-square.
+
+        If G and H are very close to 0, this is an affine transformation. If they are not, then the perspective
+        transform requires g and h,
+
+        @param p1:
+        @param p2:
+        @param p3:
+        @param p4:
+        @return:
+        """
+        x1, y1 = p1
+        x2, y2 = p2
+        x3, y3 = p3
+        x4, y4 = p4
+
+        i = 1
+        try:
+            j = (y1 - y2 + y3 - y4) / (y2 - y3)
+            k = (x1 - x2 + x3 - x4) / (x4 - x3)
+            m = (y4 - y3) / (y2 - y3)
+            n = (x2 - x3) / (x4 - x3)
+
+            h = i * (j - k * m) / (1 - m * n)
+            g = i * (k - j * n) / (1 - m * n)
+        except ZeroDivisionError:
+            h = 0.0
+            g = 0.0
+
+        c = x1 * i
+        f = y1 * i
+        a = x4 * (g + i) - x1 * i
+        b = x2 * (h + i) - x1 * i
+        d = y4 * (g + i) - y1 * i
+        e = y2 * (h + i) - y1 * i
+        m = cls()
+        m.mx[0, 0] = a
+        m.mx[0, 1] = b
+        m.mx[0, 2] = c
+
+        m.mx[1, 0] = d
+        m.mx[1, 1] = e
+        m.mx[1, 2] = f
+
+        m.mx[2, 0] = g
+        m.mx[2, 1] = h
+        m.mx[2, 2] = i
+        return m
 
 
 class WarpEffectNode(Node, FunctionalParameter):
