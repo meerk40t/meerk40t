e3f197b9b8c815e821e21147c3551acabcf6213d
jpirnay <jens@pirnay.com>
2023-11-23 12:19:14 +0100

    Add images to tips

--- meerk40t/gui/tips.py
+++ meerk40t/gui/tips.py
@@ -6,6 +6,8 @@
 """
 import datetime
 import os
+import requests
+import webbrowser
 
 import wx
 from wx import aui
@@ -38,12 +40,16 @@ def register_panel_tips(window, context):
 
 
 class TipPanel(wx.Panel):
+    """
+    Display MeerK40t usage tips
+    """
     def __init__(self, *args, context=None, **kwds):
         # begin wxGlade: PositionPanel.__init__
         kwds["style"] = kwds.get("style", 0) | wx.TAB_TRAVERSAL
         wx.Panel.__init__(self, *args, **kwds)
         self.context = context
         self.tip_command = ""
+        self.tip_image = ""
         self.tips = list()
         self.setup_tips()
         self.context.setting(bool, "show_tips", True)
@@ -51,11 +57,18 @@ class TipPanel(wx.Panel):
         icon_size = dip_size(self, 25, 25)
         # Main Sizer
         sizer_main = wx.BoxSizer(wx.VERTICAL)
+        self.image_tip = wx.StaticBitmap(self, wx.ID_ANY, style=wx.SB_FLAT)
+        self.image_tip.SetMinSize(wx.Size(250, -1))
+        self.image_tip.SetMaxSize(wx.Size(250, -1))
+        self.image_tip.SetSize(wx.Size(250, -1))
         self.text_tip = wx.TextCtrl(
             self, wx.ID_ANY, "", style=wx.TE_READONLY | wx.TE_MULTILINE
         )
-        sizer_main.Add(self.text_tip, 1, wx.EXPAND, 0)
+        tip_area = wx.BoxSizer(wx.HORIZONTAL)
+        tip_area.Add(self.image_tip, 0, wx.EXPAND, 0)
+        tip_area.Add(self.text_tip, 1, wx.EXPAND, 0)
 
+        sizer_main.Add(tip_area, 1, wx.EXPAND, 0)
         button_sizer = wx.BoxSizer(wx.HORIZONTAL)
         self.button_prev = wx.Button(self, wx.ID_ANY, _("Previous tip"))
         self.button_prev.SetBitmap(icons8_circled_left.GetBitmap(resize=icon_size[0]))
@@ -86,6 +99,7 @@ class TipPanel(wx.Panel):
         self.button_next.Bind(wx.EVT_BUTTON, self.on_tip_next)
         self.button_try.Bind(wx.EVT_BUTTON, self.on_button_try)
 
+        self.cache_dir = self.establish_picture_cache()
         self.context.setting(int, "next_tip", 0)
         self.current_tip = self.context.next_tip
 
@@ -110,11 +124,88 @@ class TipPanel(wx.Panel):
         else:
             self.button_try.Show(False)
             self.tip_command = ""
+        if my_tip[2]:
+            self.image_tip.Show(True)
+            self.set_tip_image(my_tip[2])
+        else:
+            self.set_tip_image("")
         self.Layout()
+
+    def set_tip_image(self, path):
+        self.tip_image = path
+        if isinstance(path, wx.Bitmap):
+            self.image_tip.SetBitmap(path)
+            self.image_tip.Show(True)
+        else:
+            found = False
+            if path and self.cache_dir:
+                parts = path.split("/")
+                if len(parts) > 0:
+                    basename = parts[-1]
+                    local_path = os.path.join(self.cache_dir, basename)
+                    # Is this file already on the disk? If not load it...
+                    if not os.path.exists(local_path):
+                        loaded = False
+                        try:
+                            # We need verify=False, otherwise a lot of certificate
+                            # errors will occur...
+                            page = requests.get(path, verify=False)
+                            if page.status_code == 200:  # okay
+                                try:
+                                    with open(local_path, "wb") as f:
+                                        f.write(page.content)
+                                        loaded = True
+                                except (OSError, PermissionError, RuntimeError) as e:
+                                    # print (f"Error happened during image write: {e}")
+                                    pass
+                            else:
+                                # print (f"url could not be found: {path}")
+                                pass
+
+                        except (requests.ConnectionError, requests.Timeout, requests.HTTPError) as e:
+                            # print (f"Error happened during urlretrieve: {e}")
+                            pass
+
+                        found = loaded
+
+                    if local_path and os.path.exists(local_path):
+                        bmp = wx.Bitmap()
+                        res = bmp.LoadFile(local_path)
+                        if res:
+                            new_x, new_y = bmp.Size
+                            img_size = self.image_tip.GetSize()
+                            if new_x > img_size[0] or new_y > img_size[1]:
+                                if new_x > img_size[0]:
+                                    fact = img_size[0] / new_x
+                                    new_y *= fact
+                                    new_x *= fact
+                                if new_y > img_size[1]:
+                                    fact = img_size[1] / new_y
+                                    new_y *= fact
+                                    new_x *= fact
+                                image = bmp.ConvertToImage()
+                                image.Rescale(int(new_x), int(new_y))
+                                bmp = wx.Bitmap(image)
+
+                            try:
+                                self.image_tip.SetScaleMode(wx.StaticBitmap.Scale_None)
+                            except AttributeError:
+                                # Old version of wxpython
+                                pass
+                            self.image_tip.SetBitmap(bmp)
+                            found = True
+
+            if found:
+                self.image_tip.Show(True)
+            else:
+                self.image_tip.Show(False)
 
     def on_button_try(self, event):
         if self.tip_command:
-            self.context(f"{self.tip_command}\n")
+            if self.tip_command.startswith("http"):
+                webbrowser.open(self.tip_command, new=0, autoraise=True)
+            else:
+                self.context(f"{self.tip_command}\n")
 
     def on_check_startup(self, event):
         state = self.check_startup.GetValue()
@@ -134,6 +225,7 @@ class TipPanel(wx.Panel):
                     + "Just place your mouse over a window or an UI-element and press F11."
                 ),
                 "",
+                "",
             )
         )
         self.tips.append(
@@ -145,6 +237,7 @@ class TipPanel(wx.Panel):
                     + "where you can have different configuration settings (eg regular and rotary)."
                 ),
                 "window open DeviceManager",
+                "",
             ),
         )
         self.tips.append(
@@ -156,8 +249,30 @@ class TipPanel(wx.Panel):
                     + "just set the 'Unsupported feature' option in the preference section to 'Ask at load time'"
                 ),
                 "window open Preferences\n",
+                "",
             ),
         )
+        self.tips.append(
+            (
+                _(
+                    "There are a couple of YouTube-videos that deal with some specific functionalities and explain their usage:\n"
+                    + "https://www.youtube.com/channel/UCsAUV23O2FyKxC0HN7nkAQQ"
+                ),
+                "https://www.youtube.com/channel/UCsAUV23O2FyKxC0HN7nkAQQ",
+                icons8_detective.GetBitmap(resize=50),
+            )
+        )
+        self.tips.append(
+            (
+                _(
+                    "More instructional videos? Sure, here is another channel:\n\n"
+                    + "https://www.youtube.com/channel/UCMN9gGvpacxZINPZCSOecaQ"
+                ),
+                "https://www.youtube.com/channel/UCMN9gGvpacxZINPZCSOecaQ",
+                "https://user-images.githubusercontent.com/2670784/284025481-7c3f06a2-d0fb-46ce-9adc-320566eef310.png",
+            )
+        )
+
         lastdate = None
         lastcall = self.context.setting(int, "last_tip_check", None)
         doit = True
@@ -182,7 +297,6 @@ class TipPanel(wx.Panel):
 
     def load_tips_from_github(self):
         successful = False
-        import requests
 
         content = ""
         url = "https://github.com/meerk40t/meerk40t/raw/main/locale/"
@@ -196,7 +310,7 @@ class TipPanel(wx.Panel):
         # print(self.context.language, locale, languages)
         if locale and locale != "en":
             try:
-                page = requests.get(url + locale + "/tips.txt")
+                page = requests.get(url + locale + "/tips.txt", verify=False)
                 if page.status_code == 200:  # okay
                     content = page.text
                     successful = True
@@ -242,7 +356,7 @@ class TipPanel(wx.Panel):
                 result.append(0)
             return result
 
-        def add_tip(tip, cmd, version, current_version):
+        def add_tip(tip, cmd, img, version, current_version):
             if version:
                 minimal_version = comparable_version(version)
                 # print (f"comparing {current_version} to {minimal_version}")
@@ -253,7 +367,7 @@ class TipPanel(wx.Panel):
                 tip = tip.replace(r"\n", "\n")
                 if cmd:
                     cmd = cmd.replace(r"\n", "\n")
-                self.tips.append((tip, cmd))
+                self.tips.append((tip, cmd, img))
 
         myversion = comparable_version(self.context.kernel.version)
 
@@ -262,25 +376,43 @@ class TipPanel(wx.Panel):
                 ver = ""
                 tip = ""
                 cmd = ""
+                img = ""
                 for line in f:
                     cline = line.strip()
                     if cline.startswith("#") or len(cline) == 0:
                         continue
                     if cline.startswith("tip="):
-                        add_tip(tip, cmd, ver, myversion)
+                        add_tip(tip, cmd, img, ver, myversion)
                         ver = ""
                         tip = ""
                         cmd = ""
+                        img = ""
                         tip = cline[len("tip=") :]
                     elif cline.startswith("version="):
                         ver = cline[len("version=") :]
                     elif cline.startswith("cmd="):
                         cmd = cline[len("cmd=") :]
+                    elif cline.startswith("image="):
+                        img = cline[len("image=") :]
                 # Something pending?
-                add_tip(tip, cmd, ver, myversion)
+                add_tip(tip, cmd, img, ver, myversion)
 
         except (OSError, RuntimeError, PermissionError, FileNotFoundError):
             return
+
+    def establish_picture_cache(self):
+        """
+        Check for existence of a subdirectory to store images
+        and create it if not found
+        """
+        safe_dir = os.path.realpath(get_safe_path(self.context.kernel.name))
+        cache_dir = os.path.join(safe_dir, "tip_images")
+        if not os.path.exists(cache_dir):
+            try:
+                os.mkdir(cache_dir)
+            except (OSError, PermissionError, RuntimeError):
+                cache_dir = ""
+        return cache_dir
 
     def pane_show(self, *args):
         pass
