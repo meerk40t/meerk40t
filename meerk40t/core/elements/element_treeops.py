fd8b29321cc18dc631c4af5582b86c40fd8ba98c
tatarize <tatarize@gmail.com>
2023-12-02 00:50:49 -0800

    Remove older raw image for passthrough

--- meerk40t/core/elements/element_treeops.py
+++ meerk40t/core/elements/element_treeops.py
@@ -2915,16 +2915,6 @@ def init_tree(kernel):
     def image_save_processed(node, **kwargs):
         self("image save output.png --processed\n")
 
-    @tree_conditional(lambda node: not node.lock and is_developer_mode())
-    @tree_submenu(_("Convert"))
-    @tree_operation(_("Raw Image"), node_type="elem image", help="")
-    def image_convert_raw(node, **kwargs):
-        node.replace_node(
-            image=node.image,
-            matrix=node.matrix,
-            type="image raster",
-        )
-
     @tree_conditional(lambda node: len(node.children) > 0)
     @tree_separator_before()
     @tree_operation(
