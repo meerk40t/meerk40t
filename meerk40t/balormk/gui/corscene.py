4e1599f45628e3f6f7f7462ea432d7f440877a55
tatarize <tatarize@gmail.com>
2023-12-11 04:46:49 -0800

    Add cursory geometry value

--- meerk40t/balormk/gui/corscene.py
+++ meerk40t/balormk/gui/corscene.py
@@ -1,18 +1,6 @@
-from meerk40t.gui.icons import (
-            icon_balor_bounds,
-            icon_balor_full,
-            icon_balor_hull,
-            icon_balor_regmarks,
-            icons8_center_of_gravity,
-            icons8_computer_support,
-            icons8_connected,
-            icons8_flash_off,
-            icons8_flash_on,
-            icons8_light_off,
-            icons8_light_on,
-        )
 import wx
 
+from meerk40t.gui.laserrender import LaserRender
 from meerk40t.gui.scene.sceneconst import (
     HITCHAIN_DELEGATE,
     HITCHAIN_HIT,
@@ -20,57 +8,81 @@ from meerk40t.gui.scene.sceneconst import (
 )
 from meerk40t.gui.scene.widget import Widget
 from meerk40t.gui import icons
-from meerk40t.gui.scenewidgets.guidewidget import GuideWidget
+from meerk40t.gui.icons import icons8_center_of_gravity
 from meerk40t.gui.utilitywidgets.buttonwidget import ButtonWidget
+from meerk40t.tools.geomstr import Geomstr
+
+
+def cor_file_geometry(width=None):
+    path = Geomstr()
+    path.line(complex(0x17A5, 0x7FFF), complex(0xE859, 0x7FFF))
+    path.line(complex(0x7FFF, 0x17A5), complex(0x7FFF, 0xE859))
+    path.line(complex(0x8A3C, 0x9EB6), complex(0x8A3C, 0x75C2))
+
+    path.line(complex(0x1999, 0x1999), complex(0xE665, 0x1999))
+    path.line(complex(0xE665, 0x1999), complex(0xE665, 0xE665))
+    path.line(complex(0xE665, 0xE665), complex(0x1999, 0xE665))
+    path.line(complex(0x1999, 0xE665), complex(0x1999, 0x1999))
+
+    path.line(complex(0x1EB7, 0xCCCC), complex(0x3331, 0xCCCC))
+    path.line(complex(0x3331, 0xCCCC), complex(0x3331, 0xE146))
+    path.line(complex(0x3331, 0xE146), complex(0x1EB7, 0xE146))
+    path.line(complex(0x1EB7, 0xE146), complex(0x1EB7, 0xCCCC))
+
+    path.line(complex(0xD709, 0x1EB7), complex(0xE146, 0x28F4))
+    path.line(complex(0xE146, 0x28F4), complex(0xD709, 0x3331))
+    path.line(complex(0xD709, 0x3331), complex(0xCCCC, 0x28F4))
+    path.line(complex(0xCCCC, 0x28F4), complex(0xD709, 0x1EB7))
+    return path
 
 
 def register_scene(service):
-        _ = service.kernel.translation
+    _ = service.kernel.translation
 
-        service.register(
-            "button/control/cor_file",
-            {
-                "label": _("cor_file"),
-                "icon": icons8_center_of_gravity,
-                "tip": _("Create CorFile"),
-                "help": "devicebalor",
-                "action": lambda v: service("widget_corfile\n"),
-            },
-        )
+    g = Geomstr()
 
-        @service.console_command(
-            "widget_corfile",
-            hidden=True,
-            help=_("Update galvo flips for movement"),
-        )
-        def scene_corfile(**kwargs):
-            scene = service.root.opened.get("Scene")
-            from meerk40t.gui.scene.scenespacewidget import SceneSpaceWidget
+    service.register(
+        "button/control/cor_file",
+        {
+            "label": _("cor_file"),
+            "icon": icons8_center_of_gravity,
+            "tip": _("Create CorFile"),
+            "help": "devicebalor",
+            "action": lambda v: service("widget_corfile\n"),
+        },
+    )
 
-            scene.push_stack(SceneSpaceWidget(scene))
-            scene.widget_root.scene_widget.add_widget(-1, CorFileWidget(scene))
+    @service.console_command(
+        "widget_corfile",
+        hidden=True,
+        help=_("Update galvo flips for movement"),
+    )
+    def scene_corfile(**kwargs):
+        scene = service.root.opened.get("Scene")
+        from meerk40t.gui.scene.scenespacewidget import SceneSpaceWidget
 
-            def confirm(**kwargs):
-                scene.pop_stack()
-                scene.request_refresh()
+        scene.push_stack(SceneSpaceWidget(scene))
+        scene.widget_root.scene_widget.add_widget(-1, CorFileWidget(scene))
 
-            size = 100
-            scene.widget_root.interface_widget.add_widget(
-                -1,
-                ButtonWidget(
-                    scene,
-                    0,
-                    0,
-                    size,
-                    size,
-                    icons.icons8_delete.GetBitmap(use_theme=False),
-                    confirm,
-                ),
-            )
-            scene.widget_root.focus_viewport_scene(
-                (0, 0, 0xFFFF, 0xFFFF), scene.gui.Size
-            )
+        def confirm(**kwargs):
+            scene.pop_stack()
             scene.request_refresh()
+
+        size = 100
+        scene.widget_root.interface_widget.add_widget(
+            -1,
+            ButtonWidget(
+                scene,
+                0,
+                0,
+                size,
+                size,
+                icons.icons8_delete.GetBitmap(use_theme=False),
+                confirm,
+            ),
+        )
+        scene.widget_root.focus_viewport_scene((0, 0, 0xFFFF, 0xFFFF), scene.gui.Size)
+        scene.request_refresh()
 
 
 class CorFileWidget(Widget):
@@ -81,6 +93,8 @@ class CorFileWidget(Widget):
     def __init__(self, scene):
         Widget.__init__(self, scene, all=True)
         self.name = "Corfile"
+        self.render = LaserRender(scene.context)
+        self.geometry = cor_file_geometry()
 
     def hit(self):
         return HITCHAIN_HIT
@@ -97,15 +111,18 @@ class CorFileWidget(Widget):
             pass
         return RESPONSE_CHAIN
 
-    def process_draw(self, gc):
+    def process_draw(self, gc: wx.GraphicsContext):
         """
         Draws the background on the scene.
         """
         unit_width = 0xFFFF
         unit_height = 0xFFFF
         # brush = wx.Brush(colour=wx.WHITE, style=wx.BRUSHSTYLE_SOLID)
-        gc.SetBrush(wx.GREEN_BRUSH)
+        gc.SetBrush(wx.WHITE_BRUSH)
         gc.DrawRectangle(0, 0, unit_width, unit_height)
+        gc.SetPen(wx.BLACK_PEN)
+        path = self.render.make_geomstr(gc, self.geometry)
+        gc.DrawPath(path)
 
     def signal(self, signal, *args, **kwargs):
         """
