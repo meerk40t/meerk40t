0f5160759456691bf2022b920d530a1e9ca93c98
jpirnay <jens@pirnay.com>
2023-12-21 15:27:50 +0100

    Add placement_grid command

--- meerk40t/core/elements/placements.py
+++ meerk40t/core/elements/placements.py
@@ -80,4 +80,132 @@ def init_commands(kernel):
         self.set_emphasis(added)
         return "ops", added
 
+
+    @self.console_argument("nx", type=int, help=_("How many additional placements on the X-Axis?\n(0 = as many as fit on the bed)"))
+    @self.console_argument("dx", type=Length, help=_("Gap in x-direction"))
+    @self.console_argument("ny", type=int, help=_("How many additional placements on the Y-Axis?\n(0 = as many as fit on the bed)"))
+    @self.console_argument("dy", type=Length, help=_("Gap in y-direction"))
+    @self.console_command(
+        "placement_grid",
+        help=_("repeat placements in a grid orientation"),
+        input_type=None,
+        output_type="ops",
+        all_arguments_required=True,
+    )
+    def place_grid(command, channel, _, nx=None, dx=None, ny=None, dy=None, **kwargs):
+        data = []
+        for n in list(self.ops(selected=True)):
+            if n.type == "place point":
+                data.append(n)
+        if len(data) == 0:
+            channel(_("No placements selected"))
+            return
+        if dx is None or dx == "":
+            channel(_("No placements selected"))
+            return
+        if dy is None or dy == "":
+            return
+        try:
+            len_dx = Length(dx)
+            dx_val = float(len_dx)
+            len_dy = Length(dy)
+            dy_val = float(len_dy)
+        except ValueError:
+            channel(_("Invalid values for dx and/or dy"))
+            return
+        if nx is None or nx < 0 or ny is None or ny < 0:
+            channel(_("Invalid values for nx/ny provided"))
+            return
+        try:
+            nx = int(nx)
+            ny = int(ny)
+        except ValueError:
+            channel(_("Invalid values for nx/ny provided"))
+            return
+        if nx == 1 and ny == 1:
+            channel(_("Nothing to do."))
+            return
+
+        ops = []
+        max_x = self.device.view.width
+        max_y = self.device.view.height
+        pos = None
+        for pnode in data:
+            corner = pnode.corner
+            rotation = pnode.rotation
+            idx_x = 0
+            idx_y = 0
+            starty = pnode.y
+            if ny == 0:
+                while starty < max_y:
+                    startx = pnode.x
+                    idx_x = 0
+                    if nx == 0:
+                        while startx < max_x:
+                            if idx_x != 0 or idx_y != 0:
+                                op = self.op_branch.add(
+                                    type="place point",
+                                    pos=pos,
+                                    x=startx,
+                                    y=starty,
+                                    rotation=rotation,
+                                    corner=corner,
+                                )
+                                ops.append(op)
+                            startx += dx_val
+                            idx_x += 1
+                    else:
+                        for idx in range(nx):
+                            if idx_x != 0 or idx_y != 0:
+                                op = self.op_branch.add(
+                                    type="place point",
+                                    pos=pos,
+                                    x=startx,
+                                    y=starty,
+                                    rotation=rotation,
+                                    corner=corner,
+                                )
+                                ops.append(op)
+                            startx += dx_val
+                            idx_x += 1
+            else:
+                for idx in range(ny):
+                    startx = pnode.x
+                    idx_x = 0
+                    if nx == 0:
+                        while startx < max_x:
+                            if idx_x != 0 or idx_y != 0:
+                                op = self.op_branch.add(
+                                    type="place point",
+                                    pos=pos,
+                                    x=startx,
+                                    y=starty,
+                                    rotation=rotation,
+                                    corner=corner,
+                                )
+                                ops.append(op)
+                            startx += dx_val
+                            idx_x += 1
+                    else:
+                        for idx in range(nx):
+                            if idx_x != 0 or idx_y != 0:
+                                op = self.op_branch.add(
+                                    type="place point",
+                                    pos=pos,
+                                    x=startx,
+                                    y=starty,
+                                    rotation=rotation,
+                                    corner=corner,
+                                )
+                                ops.append(op)
+                            startx += dx_val
+                            idx_x += 1
+
+                    starty += dy_val
+                    idx_y += 1
+
+        self.signal("updateop_tree")
+        self.signal("refresh_scene", "Scene")
+        return "ops", ops
+
     # --------------------------- END COMMANDS ------------------------------
