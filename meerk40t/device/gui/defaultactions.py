a24474bed832ecaa97df99c6ade515a9f205ff7e
tatarize <tatarize@gmail.com>
2024-01-21 18:35:42 -0800

    Temp rename while discussing accurate names

--- meerk40t/device/gui/defaultactions.py
+++ meerk40t/device/gui/defaultactions.py
@@ -36,7 +36,8 @@ class DefaultActionPanel(wx.Panel):
 
         self.standards = (
             ("Home", "util home", ""),
-            ("Goto Origin", "util goto", "0,0"),
+            ("Goto Origin", "util goto", "0,0,True"),
+            ("Goto Scene 0,0", "util goto", "0,0"),
             ("Beep", "util console", "beep"),
             ("Interrupt", "util console", 'interrupt "Spooling was interrupted"'),
             ("Console", "util console", ""),
