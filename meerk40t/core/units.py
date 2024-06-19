e92aae9bb457dbad076f526104d0637fa483c9b0
jpirnay <jens@pirnay.com>
2024-06-19 14:26:35 +0200

    Add percent to recognised patterns

--- meerk40t/core/units.py
+++ meerk40t/core/units.py
@@ -109,6 +109,10 @@ class Length:
     ):
         self._digits = digits
         self._amount = amount
+        if relative_length:
+            self._relative = Length(relative_length, unitless=unitless).units
+        else:
+            self._relative = None
         if self._amount is None:
             if len(args) == 2:
                 value = str(args[0]) + str(args[1])
@@ -316,6 +320,8 @@ class Length:
             return self.pt
         elif self._preferred_units == "spx":
             return self.spx
+        elif self._preferred_units == "%":
+            return self.percent
         else:
             return self.units
 
@@ -337,6 +343,8 @@ class Length:
             return self.length_um
         elif self._preferred_units == "pt":
             return self.length_pt
+        elif self._preferred_units == "%":
+            return self.length_percent
         else:
             return self.length_units
 
@@ -411,6 +419,15 @@ class Length:
         return amount
 
     @property
+    def percent(self):
+        if self._relative is None or self._relative == 0:
+            raise ValueError("Percent without relative length is meaningless.")
+        amount = 100.0 * self._amount / self._relative
+        if self._digits:
+            amount = round(amount, self._digits)
+        return amount
+
+    @property
     def length_pixels(self):
         amount = self.pixels
         return f"{round(amount, 8)}px"
@@ -454,6 +471,11 @@ class Length:
     def length_spx(self):
         amount = self.spx
         return f"{round(amount, 8)}spx"
+
+    @property
+    def length_percent(self):
+        amount = self.percent
+        return f"{round(amount, 8)}%"
 
     @property
     def length_units(self):
