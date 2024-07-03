f07bde08fdd8d333e89dfa23f0bd64358101c236
jpirnay <jens@pirnay.com>
2024-07-03 10:45:14 +0200

    Local linux build script

---
+++ tools/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo Making sure plugins are loaded
+mv meerk40t/external_plugins.py meerk40t/external_plugins.unused
+mv meerk40t/external_plugins_build.py meerk40t/external_plugins.py
+
+echo Renaming main file for the build
+## pyinstaller struggles with meerk40t.py having the same name as the package meerk40t. Rename for the build
+mv meerk40t.py mk40t.py
+pyinstaller .github/workflows/linux/meerk40t.spec
+echo Moving files back to their original places
+mv mk40t.py meerk40t.py
+mv meerk40t/external_plugins.py meerk40t/external_plugins_build.py
+mv meerk40t/external_plugins.unused meerk40t/external_plugins.py
