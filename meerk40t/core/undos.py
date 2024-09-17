cb0f6ba152bc6da8bb63b8e4e2f564eaf8769705
jpirnay <jens@pirnay.com>
2024-09-17 12:01:24 +0200

    Add label functions

--- meerk40t/core/undos.py
+++ meerk40t/core/undos.py
@@ -113,12 +113,26 @@ class Undo:
             q = "*" if i == self._undo_index else " "
             yield f"{q}{str(i).ljust(5)}: state {str(v)}"
 
+    def undo_string(self, *args):
+        if self._undo_index == 0 or len(self._undo_stack) == 0:
+            # At bottom of stack / empty stack
+            return ""
+        # for idx, s in enumerate(self._undo_stack):
+        #     print (f"[{idx}]{'*' if idx==self._undo_index else " "} {str(s)}")
+        return str(self._undo_stack[self._undo_index])
+
     def has_undo(self, *args):
         if self._undo_index == 0:
             # At bottom of stack.
             return False
         # Stack is entirely empty.
         return len(self._undo_stack) != 0
+
+    def redo_string(self, *args):
+        if self._undo_index >= len(self._undo_stack) - 1:
+            # At top of stack
+            return ""
+        return str(self._undo_stack[self._undo_index + 1])
 
     def has_redo(self, *args):
         return self._undo_index < len(self._undo_stack) - 1
