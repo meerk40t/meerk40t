ee8b49cc7b040156184537959f52185a07ac566d
jpirnay <jens@pirnay.com>
2024-09-22 21:22:24 +0200

    Version push

--- tools/build_icon.cmd
+++ tools/build_icon.cmd
@@ -1,5 +1,5 @@
 @echo off
-set ver=0.9.5.b6
+set ver=0.9.5.b7
 echo Converting master image to a couple of smaller images
 echo This requires imagemagick (https://imagemagick.org)
 echo Superimposing Version information: '%ver%'
