33ea4f5a8e4d967f2ac3909bafbf0ef9b9b7ecff
jpirnay <jens@pirnay.com>
2024-09-26 21:07:17 +0200

    Have another filenode under regmarks

--- meerk40t/core/elements/element_treeops.py
+++ meerk40t/core/elements/element_treeops.py
@@ -219,7 +219,7 @@ def init_tree(kernel):
             type="group",
             keep_children=True,
             label=_("Content of {filenode}").format(filenode=node.name),
-        )        
+        )
         self.signal("rebuild_tree")
 
     @tree_conditional(lambda node: not is_regmark(node))
@@ -1262,8 +1262,16 @@ def init_tree(kernel):
         help="",
     )
     def remove_type_file(node, **kwargs):
+        to_be_removed = [node]
+        for e in self.elem_branch.children:
+            if e.type == "file" and e.filepath == node.filepath and e not in to_be_removed:
+                to_be_removed.append(e)
+        for e in self.reg_branch.children:
+            if e.type == "file" and e.filepath == node.filepath and e not in to_be_removed:
+                to_be_removed.append(e)
+        for e in to_be_removed:
+            e.remove_node()
         self.set_emphasis(None)
-        node.remove_node()
 
     @tree_conditional(lambda node: not is_regmark(node))
     @tree_operation(
@@ -2534,7 +2542,15 @@ def init_tree(kernel):
                 _("File does not exist."),
             )
             return
-        node.remove_node()
+        to_be_removed = [node]
+        for e in self.elem_branch.children:
+            if e.type == "file" and e.filepath == node.filepath and e not in to_be_removed:
+                to_be_removed.append(e)
+        for e in self.reg_branch.children:
+            if e.type == "file" and e.filepath == node.filepath and e not in to_be_removed:
+                to_be_removed.append(e)
+        for e in to_be_removed:
+            e.remove_node()
         self.load(filepath)
 
     @tree_operation(
