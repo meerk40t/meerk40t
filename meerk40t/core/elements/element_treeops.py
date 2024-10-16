b9b0c5bc0d771dc81f547432717933e14909cd78
jpirnay <jens@pirnay.com>
2024-10-16 21:11:22 +0200

    Typo fix

--- meerk40t/core/elements/element_treeops.py
+++ meerk40t/core/elements/element_treeops.py
@@ -166,7 +166,7 @@ def init_tree(kernel):
             activate(node)
 
     ## @tree_separator_after()
-    @tree_operation(_("Edit"), node_type="util console", help=_("Modfiy console command"), grouping="00PROPS")
+    @tree_operation(_("Edit"), node_type="util console", help=_("Modify console command"), grouping="00PROPS")
     def edit_console_command(node, **kwargs):
         activate = self.kernel.lookup("function/open_property_window_for_node")
         if activate is not None:
