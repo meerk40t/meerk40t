f2bea10f3a086b955f4604cf2e43b6a5cb2a67be
jpirnay <jens@pirnay.com>
2024-11-03 15:50:29 +0100

    Add refresh signal at dialog end

--- meerk40t/gui/wxmscene.py
+++ meerk40t/gui/wxmscene.py
@@ -121,6 +121,7 @@ class ContourDetectionDialog(wx.Dialog):
         win_wd, win_ht = self.GetSize()
         self.context.win_bgcontour_width = win_wd
         self.context.win_bgcontour_height = win_ht
+        self.context.signal("refresh_scene", "Scene")
 
 
 class MeerK40tScenePanel(wx.Panel):
