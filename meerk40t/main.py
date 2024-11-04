6914912d8eee5b8b3768235aa03d71a589c02701
jpirnay <jens@pirnay.com>
2024-11-04 21:09:36 +0100

    Version push

--- meerk40t/main.py
+++ meerk40t/main.py
@@ -11,7 +11,7 @@ import os.path
 import sys
 
 APPLICATION_NAME = "MeerK40t"
-APPLICATION_VERSION = "0.9.5202"
+APPLICATION_VERSION = "0.9.5300"
 
 if not getattr(sys, "frozen", False):
     # If .git directory does not exist we are running from a package like pypi
