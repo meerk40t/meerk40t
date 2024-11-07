3fdebcad08af6e35df8e188b86f1c1021fd98f8c
jpirnay <jens@pirnay.com>
2024-11-07 15:33:56 +0100

    Adjuts unittests

--- test/test_drivers_lihuiyu.py
+++ test/test_drivers_lihuiyu.py
@@ -25,7 +25,7 @@ Creator-Software: MeerK40t v0.0.0-testing
 
 %0%0%0%0%
 IB067R091S1P
-IV1552121G002NLTS1EDnU|sB|oD|aU|qT|oD|iU|oB|oD|mU|qT|oD|sU|oB|oD|uU|oT|oD|yU|oB|oD054U|mT|oD054U|oB|oD058U|mT|oD058U|mB|oD058U|oT|oD062U|mB|oD062U|mT|oD062U|mB|oD062U|mT|oD062U|mB|oD062U|mT|oD062U|mB|qD058U|mT|oD058U|mB|oD058U|mT|qD054U|mB|oD054U|mT|qD|yU|mB|qD|uU|mT|qD|qU|mB|qD|mU|mT|qD|iU|mB|sD|aU|mT|uDlFNSE-
+IV1552121G002NLTS1EDnUdBD|aUbTD|iBD|mUbTD|sBD|uTD|yBD052TD054BD056TD056BD058TD060BD060TD060BD060TD060BD060TD060BbD056TD056BD056TbD052BD052TbD|wBbD|sTbD|oBbD|kTbD|gBdDxTfDlFNSE-
 """
 
 egv_blank = """Document type : LHYMICRO-GL file
@@ -188,6 +188,7 @@ class TestDriverLihuiyu(unittest.TestCase):
             kernel()
         with open(file1) as f:
             data = f.read()
+            # print (f'egv_image="""{data}"""')
         self.assertEqual(data, egv_image)
 
 
