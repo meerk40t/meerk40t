8470c836746f4f73715c5c25733f0fae34f4eec1
jpirnay <jens@pirnay.com>
2024-11-19 22:36:28 +0100

    Revert (faulty) algorithm, so that main becomes usable again

--- meerk40t/tools/rasterplotter.py
+++ meerk40t/tools/rasterplotter.py
@@ -37,7 +37,6 @@ class RasterPlotter:
         step_x=1,
         step_y=1,
         filter=None,
-        **kwargs,
     ):
         """
         Initialization for the Raster Plotter function. This should set all the needed parameters for plotting.
@@ -70,8 +69,6 @@ class RasterPlotter:
         self.bidirectional = bidirectional
         self.use_integers = use_integers
         self.skip_pixel = skip_pixel
-        # The overscan value isn't really used here any more
-        # The previous implementation just used the value to force a line end
         if horizontal:
             self.overscan = round(overscan / float(step_x))
         else:
@@ -104,7 +101,7 @@ class RasterPlotter:
 
         if all pixels skipped returns None
         """
-        for x in range(self.width):
+        for x in range(0, self.width):
             pixel = self.px(x, y)
             if pixel != self.skip_pixel:
                 return x
@@ -116,7 +113,7 @@ class RasterPlotter:
 
         if all pixels skipped returns None
         """
-        for y in range(self.height):
+        for y in range(0, self.height):
             pixel = self.px(x, y)
             if pixel != self.skip_pixel:
                 return y
@@ -425,8 +422,6 @@ class RasterPlotter:
         skip_pixel = self.skip_pixel
 
         x, y = self.initial_position()
-        if not self.start_minimum_y:
-            y += 1
         dx = 1 if self.start_minimum_x else -1
         dy = 1 if self.start_minimum_y else -1
 
@@ -446,15 +441,10 @@ class RasterPlotter:
             )
             if next_y is not None:
                 # If we have a next scanline, we must end after the last pixel of that scanline too.
-                upper_bound = max(next_y, upper_bound)
-                lower_bound = min(next_y, lower_bound)
-            if lower_bound == upper_bound:
-                # Me sure we have that pixel covered
-                pixel = self.px(x, lower_bound)
-                yield x, lower_bound, pixel
+                upper_bound = max(next_y, upper_bound) + self.overscan
+                lower_bound = min(next_y, lower_bound) - self.overscan
 
             if traveling_bottom:
-                ly = lower_bound
                 while y < upper_bound:
                     try:
                         pixel = self.px(x, y)
@@ -462,18 +452,11 @@ class RasterPlotter:
                         pixel = 0
                     y = self.nextcolor_bottom(x, y, upper_bound)
                     y = min(y, upper_bound)
-                    # As we are ending at the boundary of a pixel
-                    # we need to add 0 as we are coming from the top
-                    # and need to look at the upper edge
                     if pixel == skip_pixel:
                         yield x, y, 0
                     else:
-                        ly = y
                         yield x, y, pixel
-                # jump to eol
-                y = upper_bound
             else:
-                ly = upper_bound
                 while lower_bound < y:
                     try:
                         pixel = self.px(x, y)
@@ -481,41 +464,19 @@ class RasterPlotter:
                         pixel = 0
                     y = self.nextcolor_top(x, y, lower_bound)
                     y = max(y, lower_bound)
-                    # As we are ending at the boundary of a pixel
-                    # we need to add 1 as we are coming from the bottom
-                    # and need to look at the bottom edge
                     if pixel == skip_pixel:
-                        yield x, y + 1, 0
+                        yield x, y, 0
                     else:
-                        ly = y + 1
-                        yield x, y + 1, pixel
-                # jump to eol
-                y = lower_bound
-            # eol treatment
-            insufficient = ly <= y if traveling_bottom else ly >= y
-            if insufficient:
-                # Didnt we reach the end? Then check the pixel
-                try:
-                    pixel = self.px(x, y)
-                except IndexError:
-                    pixel = 0
-                if pixel != skip_pixel:
-                    delta = 1 if traveling_bottom else 0
-                    yield x, y + delta, pixel
-            cy = y
-            yield x, cy, 0
-            if self.overscan:
-                cy = y + self.overscan * (1 if traveling_bottom else -1)
-                yield x, cy, 0
+                        yield x, y, pixel
 
             if next_y is None:
                 # remaining image is blank, we stop right here.
                 return
-            yield next_x, cy, 0
-            if cy != next_y:
+            yield next_x, y, 0
+            if y != next_y:
                 yield next_x, next_y, 0
             x = next_x
-            y = next_y if next_traveling_bottom else next_y + 1
+            y = next_y
             dy = -dy
 
     def _plot_horizontal(self):
@@ -529,8 +490,6 @@ class RasterPlotter:
         skip_pixel = self.skip_pixel
 
         x, y = self.initial_position()
-        if not self.start_minimum_x:
-            x += 1
         dx = 1 if self.start_minimum_x else -1
         dy = 1 if self.start_minimum_y else -1
         yield x, y, 0
@@ -543,88 +502,46 @@ class RasterPlotter:
             upper_bound = self.rightmost_not_equal(y)
             traveling_right = self.start_minimum_x if unidirectional else dx >= 0
             next_traveling_right = self.start_minimum_x if unidirectional else dx <= 0
-            # print (f"Range {y}: {lower_bound} - {upper_bound} ({self.width})")
+
             next_x, next_y = self.calculate_next_horizontal_pixel(
                 y + dy, dy, leftmost_pixel=next_traveling_right
             )
             if next_x is not None:
                 # If we have a next scanline, we must end after the last pixel of that scanline too.
-                upper_bound = max(next_x, upper_bound)
-                lower_bound = min(next_x, lower_bound)
+                upper_bound = max(next_x, upper_bound) + self.overscan
+                lower_bound = min(next_x, lower_bound) - self.overscan
 
-            if lower_bound == upper_bound:
-                # Make sure we have that pixel covered
-                pixel = self.px(lower_bound, y)
-                yield lower_bound, y, pixel
             if traveling_right:
-                lx = lower_bound
                 while x < upper_bound:
                     try:
                         pixel = self.px(x, y)
                     except IndexError:
-                        # So we are beyond the given boundaries
                         pixel = 0
                     x = self.nextcolor_right(x, y, upper_bound)
                     x = min(x, upper_bound)
-                    # As we are ending at the boundary of a pixel
-                    # we need to add 0 as we are coming from the left
-                    # and need to look at the left edge
                     if pixel == skip_pixel:
                         yield x, y, 0
                     else:
-                        lx = x
                         yield x, y, pixel
-                        # print (f"> line: {x+1}, {y}, {pixel:.2f}")
-                # jump to eol
-                x = upper_bound
             else:
-                lx = upper_bound
                 while lower_bound < x:
-                    lx = x
                     try:
                         pixel = self.px(x, y)
                     except IndexError:
                         pixel = 0
                     x = self.nextcolor_left(x, y, lower_bound)
-                    ox = x
                     x = max(x, lower_bound)
-                    # print (f"< Testing {ox} -> {x}: {pixel:.2f} -> {self.px(x, y):.2f}")
-                    # As we are ending at the boundary of a pixel
-                    # we need to add 1 as we are coming from the right
-                    # and need to look at the right edge
                     if pixel == skip_pixel:
-                        yield x + 1, y, 0
+                        yield x, y, 0
                     else:
-                        lx = x + 1
-                        yield x + 1, y, pixel
-                        # print (f"< line: {x+1}, {y}, {pixel:.2f}")
-                # jump to eol
-                x = lower_bound
-            # eol treatment
-            # print (f"ended at {lx} [{self.px(lx, y):.2f}] ({x} [{self.px(x, y):.2f}])")
-            insufficient = lx <= x if traveling_right else lx >= x
-            if insufficient:
-                # Didnt we reach the end? Then check the pixel
-                try:
-                    pixel = self.px(x, y)
-                except IndexError:
-                    pixel = 0
-                if pixel != skip_pixel:
-                    delta = 1 if traveling_right else 0
-                    yield x + delta, y, pixel
-                    # print (f"{'<' if traveling_right else '>'} final line: {x + delta}, {y}, {pixel:.2f}")
-            cx = x
-            yield cx, y, 0
-            if self.overscan:
-                cx = x + self.overscan * (1 if traveling_right else -1)
-                yield cx, y, 0
+                        yield x, y, pixel
+
             if next_y is None:
                 # remaining image is blank, we stop right here.
                 return
-            # We jump to the next line
-            yield cx, next_y, 0
-            if cx != next_x:
+            yield x, next_y, 0
+            if x != next_x:
                 yield next_x, next_y, 0
-            x = next_x if next_traveling_right else next_x + 1
+            x = next_x
             y = next_y
             dx = -dx
