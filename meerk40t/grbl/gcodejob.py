fd1c8677e7f2556fc91191a4a4b8941326429a67
jpirnay <jens@pirnay.com>
2024-11-22 20:26:20 +0100

    Remove coolant calls as they dont have an effect

--- meerk40t/grbl/gcodejob.py
+++ meerk40t/grbl/gcodejob.py
@@ -363,19 +363,13 @@ class GcodeJob:
                     self.program_mode = False
                 elif v == 7:
                     #  Coolant Control: Mist coolant control.
-                    self._driver.service.kernel.root.coolant.coolant_on(
-                        self._driver.service
-                    )
+                    pass
                 elif v == 8:
                     # Coolant Control: Flood coolant On
-                    self._driver.service.kernel.root.coolant.coolant_on(
-                        self._driver.service
-                    )
+                    pass
                 elif v == 9:
                     # Coolant Control: Flood coolant Off
-                    self._driver.service.kernel.root.coolant.coolant_off(
-                        self._driver.service
-                    )
+                    pass
                 elif v == 56:
                     # Parking motion override control.
                     pass
