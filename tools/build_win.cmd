705592be1ece7e6b6322f77739e6b15324c65f41
jpirnay <jens@pirnay.com>
2024-12-16 21:53:27 +0100

    Amending scripts

--- tools/build_win.cmd
+++ tools/build_win.cmd
@@ -7,9 +7,8 @@ move meerk40t\external_plugins_build.py meerk40t\external_plugins.py
 echo Renaming main file for the build
 rem pyinstaller struggles with meerk40t.py having the same name as the package meerk40t. Rename for the build
 move meerk40t.py mk40t.py
-pyinstaller .github\workflows\win\meerk40t-all.spec
+pyinstaller .github\workflows\win\meerk40t.spec
 echo Moving files back to their original places
 move mk40t.py meerk40t.py
 move meerk40t\external_plugins.py meerk40t\external_plugins_build.py
 move meerk40t\external_plugins.unused meerk40t\external_plugins.py
-move dist\MeerK40t.exe dist\MeerK40t-win-all.exe
