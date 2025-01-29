158a7c2ef74549d14513c2f596c797316428b27f
jpirnay <jens@pirnay.com>
2025-01-29 18:24:56 +0100

    Version bump

--- meerk40t/main.py
+++ meerk40t/main.py
@@ -11,7 +11,7 @@ import os.path
 import sys
 
 APPLICATION_NAME = "MeerK40t"
-APPLICATION_VERSION = "0.9.6301"
+APPLICATION_VERSION = "0.9.6400"
 
 if not getattr(sys, "frozen", False):
     # If .git directory does not exist we are running from a package like pypi
