fe3129bb0edfa003d77ed71959ddbe16cc34cb0b
jpirnay <jens@pirnay.com>
2025-03-01 18:17:39 +0100

    Version push

--- tools/build_icon.sh
+++ tools/build_icon.sh
@@ -1,4 +1,4 @@
-VER=0.9.7
+VER=0.9.72
 echo Converting master image to a couple of smaller images
 echo This requires imagemagick \(https://imagemagick.org\)
 echo Superimposing Version information: "$VER"
