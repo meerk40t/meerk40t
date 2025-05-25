eab12c152c9f9766edad69dd2cc2c329066142dd
jpirnay <jens@pirnay.com>
2025-05-25 17:27:06 +0200

    Couple of refinements

--- meerk40t/extra/vtracer.py
+++ meerk40t/extra/vtracer.py
@@ -1,16 +1,108 @@
-import os
-
-from PIL import ImageOps
-
-from meerk40t.kernel.kernel import get_safe_path
-
-# Unused imports
-# from meerk40t.svgelements import Color, Matrix, Path
-# from meerk40t.tools.geomstr import Geomstr
+"""
+Vtracer https://github.com/visioncortex/vtracer
+visioncortex VTracer is an open source software to convert raster images (like jpg & png)
+into vector graphics (svg). It can vectorize graphics and photographs and trace the curves
+to output compact vector files.
+Comparing to [Potrace](http://potrace.sourceforge.net/) which only accept binarized
+inputs (Black & White pixmap), VTracer has an image processing pipeline which
+can handle colored high resolution scans.
+Comparing to Adobe Illustrator's [Image Trace](https://helpx.adobe.com/illustrator/using/image-trace.html),
+VTracer's output is much more compact (less shapes) as we adopt a stacking strategy
+and avoid producing shapes with holes.
+A technical description of the algorithm is on [visioncortex.org/vtracer-docs](//www.visioncortex.org/vtracer-docs).
+To use it we need to have the python interface in place: pip install vtracer
+"""
 
 """
-vracer routines,
-https://github.com/visioncortex/vtracer
+Not needed left here for reference purposes
+
+def simplified_load(source : str, bbox : tuple) -> list:
+    from meerk40t.tools.geomstr import Geomstr
+    from meerk40t.svgelements import Matrix, Color
+    from meerk40t.core.node.elem_path import PathNode
+    from time import perf_counter
+    t0 = perf_counter()
+    time_geom = 0
+    time_trans = 0
+    time_read = 0
+    time_bbox = 0
+    result_list = []
+    p_pattern = '<path d="'
+    t_pattern = 'transform="'
+    f_pattern = 'fill="'
+    black = Color("black")
+    content = []
+    min_x = float("inf")
+    min_y = float("inf")
+    max_x = -float("inf")
+    max_y = -float("inf")
+    with open(source, "r") as svg_file:
+        tt = perf_counter()
+        all_lines = svg_file.readlines()
+        time_read = perf_counter() - tt
+        for line in all_lines:
+            idx_start = line.find(p_pattern, 0)
+            if idx_start < 0:
+                continue
+            idx_end = line.find('"', idx_start + len(p_pattern))
+            d_str = line[idx_start + len(p_pattern):idx_end]
+            # print (d_str)
+            tt = perf_counter()
+            geom = Geomstr.svg(d_str)
+            time_geom += perf_counter() - tt
+            if geom.index == 0:
+                # print (f"Strange, empty from '{d_str}' ({line})")
+                continue
+            fill_value = None
+            mat_start = line.find(t_pattern, idx_end)
+            if mat_start >= 0:
+                tt = perf_counter()
+                mat_end = line.find('"', mat_start + len(t_pattern))
+                mat_str = line[mat_start + len(t_pattern):mat_end]
+                matrix = Matrix(mat_str)
+                geom.transform(matrix)
+                time_trans += perf_counter() - tt
+            fill_start = line.find(f_pattern, idx_end)
+            if fill_start >= 0:
+                fill_end = line.find('"', fill_start + len(f_pattern))
+                fill_str = line[fill_start + len(f_pattern):fill_end]
+                # if fill_str == "#ffffff":
+                #     continue
+
+            else:
+                fill_str = ""
+            content.append((geom, fill_str))
+            tt = perf_counter()
+            g_bb = geom.bbox()
+            min_x = min(min_x, g_bb[0])
+            min_y = min(min_y, g_bb[1])
+            max_x = max(max_x, g_bb[2])
+            max_y = max(max_y, g_bb[3])
+            time_bbox += perf_counter() - tt
+    t1 = perf_counter()
+    if content:
+        sx = (bbox[2] - bbox[0]) / (max_x - min_x)
+        sy = (bbox[3] - bbox[1]) / (max_y - min_y)
+        tx = bbox[0] - min_x
+        ty = bbox[1] - min_y
+        components = (sx, 0, 0, sy, tx, ty)
+        matrix = Matrix(components)
+    else:
+        matrix = None
+    for geom, fill_str in content:
+        if matrix:
+            tt = perf_counter()
+            geom.transform(matrix)
+            time_trans += perf_counter() - tt
+        node = PathNode(geometry = geom, stroke=black, stroke_width = 500)
+        if fill_str:
+            fill_value = Color(fill_str)
+            node.fill = fill_value
+        result_list.append(node)
+    t2 = perf_counter()
+    # print (f"Loading and geometry creation: {t1-t0:.2f}sec, node creation: {t2-t1:.2f} sec, total: {t2-t0:.2f}sec")
+    # print (f"Pure creation: {time_geom:.2f}sec, transform {time_trans:.2f}sec, reading {time_read:.2f}sec, bbox: {time_bbox:.2f}sec")
+    return result_list
 """
 
 
@@ -19,121 +111,107 @@ def plugin(kernel, lifecycle=None):
         try:
             import vtracer
         except ImportError:
-            # print("Potrace plugin could not load because potracer/pypotrace is not installed.")
+            # print("vtracer plugin could not load because vtracer is not installed.")
             return True
 
     if lifecycle == "register":
         _ = kernel.translation
-        import vtracer
 
-        def make_vector(
-            image,
-            colormode="binary",  # ["color"] or "binary"
-            hierarchical="stacked",  # ["stacked"] or "cutout"
-            mode="spline",  # ["spline"] "polygon", or "none"
-            filter_speckle=4,  # default: 4
-            color_precision=6,  # default: 6
-            layer_difference=16,  # default: 16
-            corner_threshold=60,  # default: 60
-            length_threshold=4.0,  # in [3.5, 10] default: 4.0
-            max_iterations=10,  # default: 10
-            splice_threshold=45,  # default: 45
-            path_precision=8,  # default: 8
-            **kwargs,
-        ):
-            if colormode is None or colormode not in ("color", "binary"):
-                colormode = "binary"
-            if mode is None or mode not in ("spline", "polygon"):
-                mode = "spline"
-            if hierarchical is None or hierarchical not in ("hierarchical", "cutout"):
-                hierarchical = "hierarchical"
-            if filter_speckle is None:
-                filter_speckle = 4
-            if color_precision is None:
-                color_precision = 6
-            if layer_difference is None:
-                layer_difference = 16
-            if corner_threshold is None:
-                corner_threshold = 60
-            if (
-                length_threshold is None
-                or length_threshold < 3.5
-                or length_threshold > 10
-            ):
-                length_threshold = 4
-            if max_iterations is None:
-                max_iterations = 10
-            if splice_threshold is None:
-                splice_threshold = 45
-            if path_precision is None:
-                path_precision = 8
-            image = image.convert("RGBA")
-            pixels: list[tuple[int, int, int, int]] = list(image.getdata())
-            svg = vtracer.convert_pixels_to_svg(
-                size=(image.width, image.height),
-                rgba_pixels=pixels,
-                colormode=colormode,
-                hierarchical=hierarchical,
-                mode=mode,
-                filter_speckle=filter_speckle,  # default: 4
-                color_precision=color_precision,  # default: 6
-                layer_difference=layer_difference,  # default: 16
-                corner_threshold=corner_threshold,  # default: 60
-                length_threshold=length_threshold,  # in [3.5, 10] default: 4.0
-                max_iterations=max_iterations,  # default: 10
-                splice_threshold=splice_threshold,  # default: 45
-                path_precision=path_precision,  # default: 8
-            )
-            return svg
-
-        # Not yet good enough to serve as default vectorization engine
-        kernel.register("render-op/notreadyyet_make_vector", make_vector)
-
-        @kernel.console_option("color", "c", type=bool, action="store_true")
-        @kernel.console_option("original", "o", type=bool, action="store_true")
-        @kernel.console_option("invert", "i", type=bool, action="store_true")
         @kernel.console_command(
-            "vtrace",
+            "vtracer",
             help=_("return paths around image"),
-            input_type=("image", None),
-            output_type=None,
+            input_type=("image", "elements", None),
+            output_type="elements",
         )
-        def do_vtrace(
-            command,
+        def do_vtracer(
             channel,
-            _,
             data=None,
-            color=False,
-            original=False,
-            invert=False,
             **kwargs,
         ):
+            try:
+                import os
+                from time import perf_counter
+
+                from vtracer import convert_image_to_svg_py
+
+                from meerk40t.core.units import Length
+                from meerk40t.kernel import get_safe_path
+            except ImportError:
+                channel("vtracer isn't installed, use 'pip install vtracer'")
+                return None
             elements = kernel.root.elements
             if data is None:
-                data = [
-                    node
-                    for node in elements.flat(emphasized=True)
-                    if hasattr(node, "active_image")
-                ]
-            if len(data) == 0:
-                channel("No image selected")
-            cmode = "color" if color else "binary"
-            for node in data:
-                bb = node.bounds
-                image = node.active_image
-                if original:
+                data = list(elements.elems(emphasized=True))
+            if not data:
+                channel("Nothing selected")
+                return
+            images = [node for node in data if hasattr(node, "image")]
+            if not images:
+                channel("No images selected")
+                return
+
+            safe_dir = os.path.realpath(get_safe_path(kernel.name))
+            input_file = os.path.join(safe_dir, "_vtrace_input.png")
+            output_file = os.path.join(safe_dir, "_vtrace_output.svg")
+            t_start = perf_counter()
+            t_convert = t_load = 0
+            # _("Vectorizing image")
+            with elements.undoscope("Vectorizing image"):
+                for node in images:
+                    # kernel.root.signal("freeze_tree", True)
+                    _start = perf_counter()
+                    bb = node.bounds
+                    im_wd = bb[2] - bb[0]
+                    im_ht = bb[3] - bb[1]
+                    im_x = bb[0]
+                    im_y = bb[1]
+                    flag = node.prevent_crop
+                    if not flag:
+                        node.prevent_crop = True
+                        node.update(None)
+                        bb2 = node.bounds
+                        dx = bb2[0] - bb[0]
+                        dy = bb2[1] - bb[1]
+                        bb = (bb[0] - dx, bb[1] - dy, bb[2] - dx, bb[3] - dy)
+                        node.prevent_crop = flag
+                        node.update(None)
                     image = node.image
-                if invert:
-                    image = ImageOps.invert(image.convert("L"))
-                channel(
-                    f"Processing {'original' if original else 'modified'} image with {image.width}x{image.height} pixels"
-                )
-                svgdata = make_vector(image=image, colormode=cmode)
-                directory = get_safe_path(kernel.name, create=True)
-                filename = os.path.join(directory, "vtrace_temp.svg")
-                with open(filename, "w") as f:
-                    f.write(svgdata)
-                # Now load the data
-                kernel.elements(
-                    f'xload "{filename}" {bb[0]} {bb[1]} {bb[2] - bb[0]} {bb[3] - bb[1]}\n'
-                )
+
+                    image.save(input_file)
+                    convert_image_to_svg_py(
+                        image_path=input_file, out_path=output_file, colormode="binary"
+                    )
+                    t_convert += perf_counter() - _start
+
+                    _start = perf_counter()
+                    # print (f"Vectorization took {t1-t0:.1f}sec, now loading file, executing {cmd}")
+                    elements.suppress_updates = True
+                    cmd = (
+                        f'xload "{output_file}"'
+                        + f" {Length(im_x).length_mm}"
+                        + f" {Length(im_y).length_mm}"
+                        f" {Length(im_wd).length_mm}" + f" {Length(im_ht).length_mm}"
+                    )
+                    kernel.root(f"{cmd}\n")
+                    # elements.suppress_updates = True
+                    # nodes = simplified_load(output_file, bb)
+                    # elem = kernel.elements.elem_branch.add(type="group", label=f"VTrace ({node.display_label()})")
+                    # for e in nodes:
+                    #     elem.add_node(e)
+                    # elements.suppress_updates = False
+                    t_load += perf_counter() - _start
+                    # print (f"Loading took {t2-t1:.1f}sec")
+                    try:
+                        os.remove(input_file)
+                        os.remove(output_file)
+                    except (PermissionError, OSError):
+                        pass
+                    except Exception as e:
+                        channel(f"Could not remove temporary files: {e}")
+                    # kernel.root.signal("freeze_tree", False)
+            t_end = perf_counter()
+            channel(
+                f"Time needed for vectorisation: {t_end - t_start:.1f}sec (analysis: {t_convert:.1f}sec, loading: {t_load:.1f}sec)"
+            )
+            kernel.root.signal("refresh_scene", "Scene")
+            return "elements", None
