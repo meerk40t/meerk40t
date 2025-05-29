4a84d19cad0d0ea9aa21241b05c347c61a9b8d4f
SimLV <github@iamsim.ru>
2025-05-29 15:24:07 +0300

    fix invalid array offsets

--- meerk40t/ruida/rdjob.py
+++ meerk40t/ruida/rdjob.py
@@ -1021,34 +1021,34 @@ class RDJob:
         elif array[0] == 0xD9:
             if array[1] == 0x00:
                 opts = array[2]
-                value = abscoord(array[2:7])
+                value = abscoord(array[3:8])
                 desc = f"Rapid move X ({value}μm)"
             elif array[1] == 0x01:
                 opts = array[2]
-                value = abscoord(array[2:7])
+                value = abscoord(array[3:8])
                 desc = f"Rapid move Y ({value}μm)"
             elif array[1] == 0x02:
                 opts = array[2]
-                value = abscoord(array[2:7])
+                value = abscoord(array[3:8])
                 desc = f"Rapid move Z ({value}μm)"
             elif array[1] == 0x03:
                 opts = array[2]
-                value = abscoord(array[2:7])
+                value = abscoord(array[3:8])
                 desc = f"Rapid move U ({value}μm)"
             elif array[1] == 0x0F:
                 opts = array[2]
-                value = abscoord(array[2:7])
+                value = abscoord(array[3:8])
                 desc = f"Rapid move Feed ({value}μm)"
             elif array[1] == 0x10:
                 opts = array[2]
-                x = abscoord(array[2:7])
-                y = abscoord(array[7:12])
+                x = abscoord(array[3:8])
+                y = abscoord(array[8:13])
                 desc = f"Rapid move XY ({x}μm, {y}μm)"
             elif array[1] == 0x30:
                 opts = array[2]
-                x = abscoord(array[2:7])
-                y = abscoord(array[7:12])
-                u = abscoord(array[12:17])
+                x = abscoord(array[3:7])
+                y = abscoord(array[8:13])
+                u = abscoord(array[13:18])
                 desc = f"Rapid move XYU ({x}μm, {y}μm, {u}μm)"
         elif array[0] == 0xDA:
             mem = parse_mem(array[2:4])
