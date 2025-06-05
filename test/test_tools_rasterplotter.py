f701a3dc50690785fd7c49ec9007f134cd2b4e0f
jpirnay <jens@pirnay.com>
2025-06-05 20:37:16 +0200

    Unify testcases

--- test/test_tools_rasterplotter.py
+++ test/test_tools_rasterplotter.py
@@ -8,16 +8,13 @@ from meerk40t.tools.rasterplotter import RasterPlotter
 
 
 class TestRasterPlotter(unittest.TestCase):
-    def test_onepixel_black_image(self):
-        """
-        Tests the speed of rasterplotter for a one pixel image.
-
-        :return:
-        """
-        image = Image.new("RGBA", (1, 1), "black") # This is an empty image
+    
+    def _test_rasterplotter(self, image, testcase: str):
         image = image.convert("L")
+        width = image.width
+        height = image.height   
         img = image.load()
-        plotter = RasterPlotter(img, 1, 1)
+        plotter = RasterPlotter(img, width, height)
         t = time.time()
         i = 0
         ipos = plotter.initial_position_in_scene()
@@ -29,8 +26,17 @@ class TestRasterPlotter(unittest.TestCase):
             if on:
                 pixels += (abs(x - lastx) + 1) * (abs(y - lasty) + 1)
             lastx, lasty = x, y
-        print(f"\nOne pixel empty found: {i} lines, {pixels} pixels, ranging from ({ipos[0]}, {ipos[1]}) to ({lpos[0]}, {lpos[1]})") 
+        print(f"\n{testcase} found: {i} lines, {pixels} pixels, ranging from ({ipos[0]}, {ipos[1]}) to ({lpos[0]}, {lpos[1]})") 
         print(f"Time taken to finish process {time.time() - t:.3f}s\n")
+
+    def test_onepixel_black_image(self):
+        """
+        Tests the speed of rasterplotter for a one pixel image.
+
+        :return:
+        """
+        image = Image.new("RGBA", (1, 1), "black") # This is an empty image
+        self._test_rasterplotter(image, "One pixel empty image")
 
     def test_onepixel_white_image(self):
         """
@@ -39,24 +45,19 @@ class TestRasterPlotter(unittest.TestCase):
         :return:
         """
         image = Image.new("RGBA", (1, 1), "white") # This is a one-pixel image
-        # draw = ImageDraw.Draw(image)
-        # draw.point((0, 0), "black")
-        image = image.convert("L")
-        img = image.load()
-        plotter = RasterPlotter(img, 1, 1)
-        t = time.time()
-        i = 0
-        ipos = plotter.initial_position_in_scene()
-        lpos = plotter.final_position_in_scene()
-        lastx, lasty = ipos 
-        pixels = 0
-        for x, y, on in plotter.plot():
-            i += 1
-            if on:
-                pixels += (abs(x - lastx) + 1) * (abs(y - lasty) + 1)
-            lastx, lasty = x, y
-        print(f"\nOne pixel full found: {i} lines, {pixels} pixels, ranging from ({ipos[0]}, {ipos[1]}) to ({lpos[0]}, {lpos[1]})") 
-        print(f"Time taken to finish process {time.time() - t:.3f}s\n")
+        self._test_rasterplotter(image, "One pixel full image")
+
+    def test_line_image(self):
+        """
+        Tests the speed of rasterplotter for a one pixel image.
+
+        :return:
+        """
+        image = Image.new("RGBA", (10, 1), "black") 
+        draw = ImageDraw.Draw(image)
+        draw.line((1, 0, 8, 0), "white")
+        
+        self._test_rasterplotter(image, "8 pixel line")
 
     def test_rasterplotter_largecircle(self):
         """
@@ -67,21 +68,5 @@ class TestRasterPlotter(unittest.TestCase):
         image = Image.new("RGBA", (2560, 2560), "white")
         draw = ImageDraw.Draw(image)
         draw.ellipse((0, 0, 2560, 2560), "black")
-        image = image.convert("L")
-        img = image.load()
-        plotter = RasterPlotter(img, 2560, 2560)
-        t = time.time()
-        i = 0
-        ipos = plotter.initial_position_in_scene()
-        lpos = plotter.final_position_in_scene()
-        lastx, lasty = ipos 
-        pixels = 0
-        for x, y, on in plotter.plot():
-            i += 1
-            if on:
-                pixels += (abs(x - lastx) + 1) * (abs(y - lasty) + 1)
-            lastx, lasty = x, y
-        ipos = plotter.initial_position_in_scene()
-        lpos = plotter.final_position_in_scene()
-        print(f"\nLarge circle found: {i} lines, {pixels} pixels,ranging from ({ipos[0]}, {ipos[1]}) to ({lpos[0]}, {lpos[1]})") 
-        print(f"Time taken to finish process {time.time() - t:.3f}s\n")
+        self._test_rasterplotter(image, "Large ellipse")
+
