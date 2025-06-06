0d2861f2879c30aa27c98c4ae3818224fb7d5fc4
jpirnay <jens@pirnay.com>
2025-06-06 10:57:39 +0200

    Extend raster test cases

--- test/test_tools_rasterplotter.py
+++ test/test_tools_rasterplotter.py
@@ -1,7 +1,7 @@
 import time
 import unittest
 
-from copy import copy
+# from copy import copy
 from PIL import Image, ImageDraw
 
 from meerk40t.constants import (
@@ -20,19 +20,40 @@ from meerk40t.tools.rasterplotter import RasterPlotter
 
 class TestRasterPlotter(unittest.TestCase):
     def _test_rasterplotter(
-        self, 
-        image, 
-        testcase: str="", 
-        method:int=0, 
-        horizontal:bool=True,
-        bidirectional:bool=False,
-        sx:bool=True,
-        sy:bool=True,
+        self,
+        msgstr: str,
+        image: Image.Image,
     ):
+        t = time.time()
+        method_names = {
+            RASTER_T2B: "Top to Bottom",
+            RASTER_B2T: "Bottom to Top",
+            RASTER_L2R: "Left to Right",
+            RASTER_R2L: "Right to Left",
+            RASTER_HATCH: "Hatching",
+            RASTER_GREEDY_H: "Greedy Horizontal",
+            RASTER_GREEDY_V: "Greedy Vertical",
+            RASTER_CROSSOVER: "Crossover",
+            RASTER_SPIRAL: "Spiral",
+        }
+        cases = []
+        for case_method in method_names:
+            for case_bidir in (False, True):
+                for case_horizontal in (False, True):
+                    for case_edge in (False, True):
+                        testcase = method_names[case_method]
+                        cases.append(
+                            (
+                                testcase,
+                                case_method,
+                                case_horizontal,
+                                case_bidir,
+                                case_edge,
+                            )
+                        )
         image = image.convert("L")
         width = image.width
         height = image.height
-        img = image.load()
         parameters = {
             # Provide an override for the minimumx / minimumy / horizontal / bidirectional
             RASTER_T2B: (None, True, True, None),  # top to bottom
@@ -44,16 +65,100 @@ class TestRasterPlotter(unittest.TestCase):
             RASTER_GREEDY_V: (None, None, None, True),  # greedy neighbour
             RASTER_CROSSOVER: (None, None, None, True),  # true crossover
         }
-        def_x, def_y, def_hor, def_bidir = parameters.get(
-            method, (None, None, None, None)
+        special_cases = [
+            {},  # default case
+            {"legacy": True},  # legacy case for lihuiyu/moshi
+        ]
+        casecount = 0
+        for special_case in special_cases:
+            for case, method, horizontal, bidirectional, edge in cases:
+                img = image.load()
+                def_x, def_y, def_hor, def_bidir = parameters.get(
+                    method, (None, None, None, None)
+                )
+                start_minimum_x = edge if def_x is None else def_x
+                start_minimum_y = edge if def_y is None else def_y
+                horizontal = horizontal if def_hor is None else def_hor
+                bidirectional = bidirectional if def_bidir is None else def_bidir
+                plotter = RasterPlotter(
+                    img,
+                    width,
+                    height,
+                    direction=method,
+                    bidirectional=bidirectional,
+                    start_minimum_x=start_minimum_x,
+                    start_minimum_y=start_minimum_y,
+                    special=special_case,
+                )
+                i = 0
+                ipos = plotter.initial_position_in_scene()
+                lpos = plotter.final_position_in_scene()
+                lastx, lasty = ipos
+                pixels = 0
+                for x, y, on in plotter.plot():
+                    i += 1
+                    if on:
+                        pixels += (abs(x - lastx) + 1) * (abs(y - lasty) + 1)
+                    lastx, lasty = x, y
+                print(
+                    f"{msgstr}.{case} (hor={horizontal}, bidir={bidirectional}, special={special_case}) found: {i} lines, {pixels} px, from ({ipos[0]}, {ipos[1]}) to ({lpos[0]}, {lpos[1]})"
+                )
+                casecount += 1
+        print(
+            f"Time taken to finish process for {msgstr} and {casecount} cases: {time.time() - t:.3f}s\n"
         )
-        start_minimum_x = def_x if sx is None else sx
-        start_minimum_y = def_y if sy is None else sy
-        horizontal = horizontal if def_hor is None else def_hor
-        bidirectional = bidirectional if def_bidir is None else def_bidir
+
+    def test_onepixel_black_image(self):
+        """
+        Tests the speed of rasterplotter for a one pixel image.
+
+        :return:
+        """
+        image = Image.new("RGBA", (1, 1), "black")  # This is an empty image
+        self._test_rasterplotter("Empty 1x1", image)
+
+    def test_onepixel_white_image(self):
+        """
+        Tests the speed of rasterplotter for a one pixel image.
+
+        :return:
+        """
+        image = Image.new("RGBA", (1, 1), "white")  # This is a one-pixel image
+        self._test_rasterplotter("Black 1x1", image)
+
+    def test_line_image(self):
+        """
+        Tests the speed of rasterplotter for a one pixel image.
+
+        :return:
+        """
+        image = Image.new("RGBA", (10, 1), "black")
+        draw = ImageDraw.Draw(image)
+        draw.line((1, 0, 8, 0), "white")
+        self._test_rasterplotter("8 pixel line", image)
+
+    def test_rasterplotter_largecircle(self):
+        """
+        Tests the speed of rasterplotter for large circle.
+
+        :return:
+        """
+        width = 2560
+        height = 2560
+        image = Image.new("RGBA", (width, height), "white")
+        draw = ImageDraw.Draw(image)
+        draw.ellipse((0, 0, width, height), "black")
+        testcase = "Large circle"
+        horizontal = True
+        method = RASTER_L2R
+        bidirectional = False
+        start_minimum_x = True
+        start_minimum_y = True
         plotter = RasterPlotter(
-            img, width, height, 
-            direction=method, 
+            image.load(),
+            width,
+            height,
+            direction=method,
             bidirectional=bidirectional,
             start_minimum_x=start_minimum_x,
             start_minimum_y=start_minimum_y,
@@ -73,68 +178,3 @@ class TestRasterPlotter(unittest.TestCase):
             f"\n{testcase} (horiz={horizontal}, bidir={bidirectional}) found: {i} lines, {pixels} pixels, ranging from ({ipos[0]}, {ipos[1]}) to ({lpos[0]}, {lpos[1]})"
         )
         print(f"Time taken to finish process {time.time() - t:.3f}s\n")
-
-    def test_onepixel_black_image(self):
-        """
-        Tests the speed of rasterplotter for a one pixel image.
-
-        :return:
-        """
-        image = Image.new("RGBA", (1, 1), "black")  # This is an empty image
-        cases = [
-            ("One pixel empty image - Top to Bottom", RASTER_T2B, False, False),
-            ("One pixel empty image - Bottom to Top", RASTER_B2T, False, False),
-            ("One pixel empty image - Left to Right", RASTER_L2R, True,  False),
-            ("One pixel empty image - Right to Left", RASTER_R2L, True, False ),
-            ("One pixel empty image - Top to Bottom", RASTER_T2B, False, True),
-            ("One pixel empty image - Bottom to Top", RASTER_B2T, False, True),
-            ("One pixel empty image - Left to Right", RASTER_L2R, True,  True),
-            ("One pixel empty image - Right to Left", RASTER_R2L, True, True ),
-        ]
-        for msg, method, horizontal, bidir in cases:
-            img = copy(image)
-            self._test_rasterplotter(img, msg, method=method, horizontal=horizontal, bidirectional=bidir)   
-
-    def test_onepixel_white_image(self):
-        """
-        Tests the speed of rasterplotter for a one pixel image.
-
-        :return:
-        """
-        image = Image.new("RGBA", (1, 1), "white")  # This is a one-pixel image
-        cases = [
-            ("One pixel empty image - Top to Bottom", RASTER_T2B, False, False),
-            ("One pixel full image - Bottom to Top", RASTER_B2T, False, False),
-            ("One pixel full image - Left to Right", RASTER_L2R, True, False ),
-            ("One pixel full image - Right to Left", RASTER_R2L, True, False ),
-            ("One pixel empty image - Top to Bottom", RASTER_T2B, False,True),
-            ("One pixel full image - Bottom to Top", RASTER_B2T, False,True),
-            ("One pixel full image - Left to Right", RASTER_L2R, True, True ),
-            ("One pixel full image - Right to Left", RASTER_R2L, True, True ),
-        ]
-        for msg, method, horizontal, bidir in cases:
-            img = copy(image)
-            self._test_rasterplotter(img, msg, method=method, horizontal=horizontal, bidirectional=bidir)
-
-    def test_line_image(self):
-        """
-        Tests the speed of rasterplotter for a one pixel image.
-
-        :return:
-        """
-        image = Image.new("RGBA", (10, 1), "black")
-        draw = ImageDraw.Draw(image)
-        draw.line((1, 0, 8, 0), "white")
-
-        self._test_rasterplotter(image, "8 pixel line")
-
-    def test_rasterplotter_largecircle(self):
-        """
-        Tests the speed of rasterplotter for large circle.
-
-        :return:
-        """
-        image = Image.new("RGBA", (2560, 2560), "white")
-        draw = ImageDraw.Draw(image)
-        draw.ellipse((0, 0, 2560, 2560), "black")
-        self._test_rasterplotter(image, "Large ellipse")
