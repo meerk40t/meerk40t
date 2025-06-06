3bb0fa25e34eece366d3b85cc5804f2c383ab89c
jpirnay <jens@pirnay.com>
2025-06-06 18:44:56 +0200

    Small updates

--- install-run.sh
+++ install-run.sh
@@ -12,14 +12,14 @@ if [ $? -ne 0 ]; then
     echo "sudo apt-get install python3-pip"
     echo "2) For development libraries:"
     echo "sudo apt-get install libgtk-3-dev libpython3-dev"
-    echo "3) For more/faster functionality you may need to install additional libraries:"
+    echo "3) For more/faster functionality:"
     echo "sudo apt install libagg-dev libpotrace-dev cmake libeigen3-dev"
     exit 1
 fi
 read -p "Do you want to install optional (but helpful) packages (Y/N)? [N]: " choice
 choice=${choice:0:1}
 if [[ "$choice" == "Y" || "$choice" == "y" ]]; then
-    pip3 install -r requirements-optional-linux.txt
+    python3 -m pip install -r requirements-optional-linux.txt
     if [[ $? -ne 0 ]]; then
         echo "Warning: Optional package installation failed. Some features may not be available."
     fi
