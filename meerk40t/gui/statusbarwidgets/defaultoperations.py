993ee7b311adf8bedbcb9e0db3958e5e477f2daa
jpirnay <jens@pirnay.com>
2025-08-04 16:50:26 -0500

    No tooltip crash

--- meerk40t/gui/statusbarwidgets/defaultoperations.py
+++ meerk40t/gui/statusbarwidgets/defaultoperations.py
@@ -475,7 +475,7 @@ class DefaultOperationWidget(StatusBarWidget):
         # First reset all
         for idx, node in enumerate(self.context.elements.default_operations):
             slabel = self.node_label(node)
-            if slabel:
+            if slabel and idx < len(self.assign_buttons):
                 self.assign_buttons[idx].SetToolTip(slabel)
         oplist = list(self.context.elements.ops())
         for node in oplist:
@@ -490,7 +490,7 @@ class DefaultOperationWidget(StatusBarWidget):
                         slabel = self.node_label(node)
                         if slabel:
                             for idx, op in enumerate(self.assign_operations):
-                                if op.id == opid:
+                                if op.id == opid and idx < len(self.assign_buttons):
                                     self.assign_buttons[idx].SetToolTip(slabel)
                                     break
                         break
