33f17e8403ca3e75be784a13478bf737c15a72be
jpirnay <jens@pirnay.com>
2025-08-17 10:35:21 +0200

    Add unidirectional to persisted properties

--- meerk40t/core/node/effect_hatch.py
+++ meerk40t/core/node/effect_hatch.py
@@ -84,7 +84,7 @@ class HatchEffectNode(Node, Suppressable):
         Returns:
             str: A descriptor string in the format "<type>|<hatch_type>|<hatch_distance>|<hatch_angle>|<hatch_angle_delta>|<loops>".
         """
-        return f"{self.type}|{self.hatch_type}|{self.hatch_distance}|{self.hatch_angle}|{self.hatch_angle_delta}|{self.loops}"
+        return f"{self.type}|{self.hatch_type}|{self.hatch_distance}|{self.hatch_angle}|{self.hatch_angle_delta}|{self.loops}|{'1' if self.unidirectional else '0'}"
 
     def set_effect_descriptor(self, descriptor):
         """
@@ -103,6 +103,9 @@ class HatchEffectNode(Node, Suppressable):
             ValueError: Silently ignored if the descriptor cannot be split into five parts.
         """
         try:
+            pattern = descriptor.split("|")
+            if len(pattern) == 6:  # Old format
+                pattern.append("0")  # Default unidirectional to False
             (
                 typeinfo,
                 hatchtype,
@@ -110,13 +113,15 @@ class HatchEffectNode(Node, Suppressable):
                 hatchangle,
                 hatchangledelta,
                 loops,
-            ) = descriptor.split("|")
+                unidirectional,
+            ) = pattern
             if typeinfo == self.type:
                 self.hatch_type = hatchtype
                 self.hatch_distance = hatchdistance
                 self.hatch_angle = hatchangle
                 self.hatch_angle_delta = hatchangledelta
                 self.loops = loops
+                self.unidirectional = True if unidirectional == "1" else False
                 self.recalculate()
         except ValueError:
             pass
