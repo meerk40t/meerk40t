4be3d651b87c31f6019b735ff1c12643e1694256
jpirnay <jens@pirnay.com>
2025-10-17 11:37:38 +0200

    Remove unintentionally doubled block

--- meerk40t/gui/gui_optypes.py
+++ meerk40t/gui/gui_optypes.py
@@ -42,22 +42,4 @@ def get_operation_info() -> Dict[str, Tuple[str, Any, int]]:
         "util console": (_("Command"), icons8_console, 0),
         "generic": (_("Generic"), icons8_console, 0),
     }
-    return {
-        "op cut": (_("Cut"), icons8_laser_beam, 0),
-        "op raster": (_("Raster"), icons8_direction, 0),
-        "op image": (_("Image"), icons8_image, 0),
-        "op engrave": (_("Engrave"), icons8_laserbeam_weak, 0),
-        "op dots": (_("Dots"), icon_points, 0),
-        "effect hatch": (_("Hatch"), icon_effect_hatch, 0),
-        "effect wobble": (_("Wobble"), icon_effect_wobble, 0),
-        "effect warp": (_("Warp"), icon_distort, 0),
-        "place current": (_("Jobstart current"), icons8_home_filled, 0),
-        "place point": (_("Jobstart at point"), icons8_home_filled, 0),
-        "util wait": (_("Wait"), icon_timer, 0),
-        "util home": (_("Home"), icons8_home_filled, 0),
-        "util goto": (_("Goto"), icon_return, 0),
-        "util output": (_("Output"), icon_external, 0),
-        "util input": (_("Input"), icon_internal, 0),
-        "util console": (_("Command"), icons8_console, 0),
-        "generic": (_("Generic"), icons8_console, 0),
-    }
+
