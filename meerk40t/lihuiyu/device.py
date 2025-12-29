84bbc0fed18df6ee07cc339992eb1f20011310c3
jpirnay <jens@pirnay.com>
2025-12-29 21:30:31 +0100

    Format code

--- meerk40t/lihuiyu/device.py
+++ meerk40t/lihuiyu/device.py
@@ -697,7 +697,8 @@ class LihuiyuDevice(Service, Status):
         @self.console_argument("dy", type=Length, help=_("change in y"))
         @self.console_command(
             "move_at_speed",
-            help="move_at_speed <speed> <dx> <dy> : " + _("move the laser at a specific speed and distance"),
+            help="move_at_speed <speed> <dx> <dy> : "
+            + _("move the laser at a specific speed and distance"),
             all_arguments_required=True,
         )
         def move_speed(channel, _, speed, dx, dy, **kwgs):
@@ -854,7 +855,7 @@ class LihuiyuDevice(Service, Status):
             "resume",
             help=_("realtime resume of the machine"),
         )
-        def realtime_resume(channel, _,**kwgs):
+        def realtime_resume(channel, _, **kwgs):
             if self.driver.paused:
                 self.driver.resume()
                 self.signal("pause")
