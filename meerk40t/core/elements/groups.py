dd7912b4d481c996ada9d6f741d1bb0273bcf332
jpirnay <jens@pirnay.com>
2026-01-28 17:40:29 +0100

    Use bulk methods

--- meerk40t/core/elements/groups.py
+++ meerk40t/core/elements/groups.py
@@ -225,8 +225,7 @@ def init_commands(kernel):
                 parent_node = minimal_parent(to_treat)
                 label = label if label is not None else "Group"
                 group_node = parent_node.add(type="group", label=label, expanded=True)
-                for e in to_treat:
-                    group_node.append_child(e)
+                group_node.append_children(to_treat)
                 channel(_("Grouped {count} elements.").format(count=len(data)))
                 classify_new(group_node)
         self.signal("rebuild_tree", "elements")
@@ -264,8 +263,7 @@ def init_commands(kernel):
         with self.undoscope("Ungroup elements"):
             with self.node_lock:
                 for gnode in to_treat:
-                    for n in list(gnode.children):
-                        gnode.insert_sibling(n, below=False)
+                    gnode.insert_siblings(list(gnode.children), below=False)
                     gnode.remove_node()  # Removing group/file node.
         self.signal("rebuild_tree", "elements")
 
